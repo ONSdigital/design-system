34156bab3b4008e32ae869dcf6273f820d71c25a
Ben Meyrick <bameyrick@gmail.com>
2018-11-22 15:06:14 +0000

    Generate es2015+ and es5 outputs for browsers that support them

--- webpack.prod.babel.js
+++ webpack.prod.babel.js
@@ -3,7 +3,10 @@ import merge from 'webpack-merge';
 import commonConfig from './webpack.common';
 
 const mode = 'production';
+const common = commonConfig(mode);
 
-export default merge(commonConfig(mode), {
-  mode
-});
+export default [
+  merge(common.nonJs, {}),
+  merge(common.es2015plus, {}),
+  merge(common.es5, {})
+];
