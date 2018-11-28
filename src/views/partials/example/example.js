0f069bd6ba5b85c9d17dc674f6aba5ce23af8f97
Ben Meyrick <bameyrick@gmail.com>
2018-11-28 12:50:08 +0000

    Add ability to close example tabs

---
+++ src/views/partials/example/example.js
@@ -0,0 +1,19 @@
+class Example {
+  constructor(context) {
+    this.tabs = [...context.querySelectorAll('.js-example-tab')];
+    this.radios = [...context.querySelectorAll('.js-example-radio')];
+
+    this.tabs.forEach((tab, index) => tab.addEventListener('click', (event) => this.handleTabClick(event, index)));
+  }
+
+  handleTabClick(event, index) {
+    event.preventDefault();
+    const radio = this.radios[index];
+
+    radio.checked = !radio.checked;
+  }
+}
+
+[...document.querySelectorAll('.js-example')].map(example => new Example(example));
+
+
