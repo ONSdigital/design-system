7f7e844b701a95fa90187cf48d146ad36abe167f
Ben Meyrick <bameyrick@gmail.com>
2018-11-30 17:14:58 +0000

    Added password input

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 import './api/_load';
 import './dialog';
 import './submitter';
-import '../components/textarea/src/character-limit.js';
-// import '../components/02-form-elements/input/input--password';
+import 'components/textarea/src/character-limit';
+import 'components/password/src/password';
 // import '../components/collapsible/collapsible.js';
 // import '../components/04-navigation/main-nav.js';
 // import '../components/04-navigation/section-nav.js';
