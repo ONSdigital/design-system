46936ebebb67e0ff865e9d0ea6a9561a12ab470e
Ben Meyrick <bameyrick@gmail.com>
2018-12-14 13:59:44 +0000

    Ensure tests are run in production mode

--- src/tests/config/karma.conf.es5.js
+++ src/tests/config/karma.conf.es5.js
@@ -2,6 +2,6 @@ import karmaConfigGenerator from './karma.conf.common';
 import commonConfig from '../../../webpack.common';
 import browserstackLaunchersConfig from './karma.conf.browserstack-es5-launchers.js';
 
-const webpackConfig = commonConfig('development').es5;
+const webpackConfig = commonConfig('production').es5;
 
 export default karmaConfigGenerator(webpackConfig, browserstackLaunchersConfig);
