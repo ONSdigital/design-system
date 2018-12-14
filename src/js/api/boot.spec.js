8df53214c1355c640e133db66ade5e3c5bbb77ff
Ben Meyrick <bameyrick@gmail.com>
2018-12-14 15:48:49 +0000

    Added js prettier and linting

--- src/js/api/boot.spec.js
+++ src/js/api/boot.spec.js
@@ -1,22 +1,26 @@
-import {subscribe} from "./_sdcModules";
+import { subscribe } from './_sdcModules';
 
 describe('boot (API method)', () => {
   it('should run all module methods subscribed with a boot option', async function() {
     const method1 = chai.spy();
     const method2 = chai.spy();
 
-    subscribe('test-module-boot1', [{
-      method: method1,
-      methodName: 'testMethodBoot1'
-    }]);
+    subscribe('test-module-boot1', [
+      {
+        method: method1,
+        methodName: 'testMethodBoot1'
+      }
+    ]);
 
-    subscribe('test-module-boot2', [{
-      method: method2,
-      methodName: 'testMethodBoot2',
-      boot: true
-    }]);
+    subscribe('test-module-boot2', [
+      {
+        method: method2,
+        methodName: 'testMethodBoot2',
+        boot: true
+      }
+    ]);
 
-    document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('DOMContentLoaded', function() {
       window.sdcAPI.boot();
 
       expect(method1).to.not.have.been.called();
