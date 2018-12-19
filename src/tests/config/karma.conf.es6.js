727b520e3817ef7ee9622d35905cbdf787bc9618
Ben Meyrick <bameyrick@gmail.com>
2018-12-19 16:29:12 +0000

    Update nunjucks template configs to have keys in quotes to closer match jinja

--- src/tests/config/karma.conf.es6.js
+++ src/tests/config/karma.conf.es6.js
@@ -2,6 +2,6 @@ import karmaConfigGenerator from './karma.conf.common';
 import commonConfig from '../../../webpack.common';
 import browserstackLaunchersConfig from './karma.conf.browserstack-es6-launchers.js';
 
-const webpackConfig = commonConfig('production').es2015plus;
+const webpackConfig = commonConfig(process.env.NODE_ENV || 'production').es2015plus;
 
 export default karmaConfigGenerator(webpackConfig, browserstackLaunchersConfig);
