c39ffdd0d33db7d465b4ea1cedd511c62784f0cd
Ben Meyrick <bameyrick@gmail.com>
2019-01-16 10:32:07 +0000

    Revert "Added more unit tests"
    This reverts commit 7e8b057aeb333ba1d7d5f59e62bec08bacfe7e2b.

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,6 +1,6 @@
 import 'whatwg-fetch';
 
-export function checkStatus(response) {
+function checkStatus(response) {
   if (response.status >= 200 && response.status < 300) {
     return response;
   } else {
