c40d3b573c8a2ed65c4a3206b449d90afa7fa48e
Matt Simon <info@boxadesign.co.uk>
2019-02-01 13:04:14 +0000

    Updated  unit tests for sortable table

--- src/tests/spec/table/table-sortable.spec.js
+++ src/tests/spec/table/table-sortable.spec.js
@@ -176,7 +176,7 @@ describe('Component: Sortable table', function() {
       it('should sort the column into descending order', function() {
         originalValues.reverse();
         const trs = this.tbody.querySelectorAll('tr');
-        for (var i = 0; i < trs.length; i++) {
+        for (let i = 0; i < trs.length; i++) {
           const tdVal = trs[i].childNodes[1].textContent;
           sortedValues.push(tdVal);
         }
