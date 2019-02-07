d747b8e5e0c8a67a500ab2272efd56eacaa63659
Ben Meyrick <bameyrick@gmail.com>
2019-02-07 10:30:22 +0000

    Travis please

--- .travis/pu.sh
+++ .travis/pu.sh
@@ -33,9 +33,6 @@ make_version() {
 upload_files() {
   # This make sure the current work area is pushed to the tip of the current branch
   git push origin HEAD:$TRAVIS_BRANCH
-  
-  # This pushes the new tag
-  git push --tags
 }
 
 setup_git
