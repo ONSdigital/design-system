4919ec861fdf1b53c06858f796c2c97563443de6
Ben Meyrick <bameyrick@gmail.com>
2019-02-07 13:03:16 +0000

    Remove unused function

--- .travis/pu.sh
+++ .travis/pu.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 
 setup_git() {
-    # Set the user name and email to match the API token holder
-    # This will make sure the git commits will have the correct photo
-    # and the user gets the credit for a checkin
-    git config --global user.email "bameyrick@gmail.com"
-    git config --global user.name "Ben Meyrick"
-    git config --global push.default matching
-    
-    # Get the credentials from a file
-    git config credential.helper "store --file=.git/credentials"
-    
-    # This associates the API Key with the account
-    echo "https://${GITHUB_API_KEY}:@github.com" > .git/credentials
+  # Set the user name and email to match the API token holder
+  # This will make sure the git commits will have the correct photo
+  # and the user gets the credit for a checkin
+  git config --global user.email "bameyrick@gmail.com"
+  git config --global user.name "Ben Meyrick"
+  git config --global push.default matching
+  
+  # Get the credentials from a file
+  git config credential.helper "store --file=.git/credentials"
+  
+  # This associates the API Key with the account
+  echo "https://${GITHUB_API_KEY}:@github.com" > .git/credentials
 }
 
 make_version() {
   git branch
+
   git checkout master
+
   git branch
   # Echo the status to the log so that we can see it is OK
   git status
@@ -27,10 +29,10 @@ make_version() {
   # npm version $TRAVIS_BRANCH -m "chore: release version %s [skip ci]"
 }
 
-upload_files() {
-  # git push origin
-}
+# upload_files() {
+#   # git push origin
+# }
 
 setup_git
 make_version
-upload_files
+# upload_files
