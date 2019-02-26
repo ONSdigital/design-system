ea945db1117cbd421949c1879e0876d25c39f810
Ben Meyrick <bameyrick@gmail.com>
2019-02-26 11:12:06 +0000

    Remove timeout component and all of its dependecies

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,8 @@
-import './dialog';
-import './submitter';
 import 'components/mutually-exclusive/mutually-exclusive';
 import './inpagelink';
 import 'components/textarea/character-limit';
 import 'components/password/password';
 import 'components/tabs/tabs';
-import 'components/timeout/timeout.dom';
 import 'components/details/collapsible.dom';
 import 'components/table/scrollable-table';
 import 'components/table/sortable-table';
