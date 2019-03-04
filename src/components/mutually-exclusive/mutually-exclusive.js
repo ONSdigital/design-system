653ac8065d862e4dd719418f26ca58edbf057e6d
Ben Meyrick <bameyrick@gmail.com>
2019-03-04 16:27:13 +0000

    Fix mutually exlusive for inputs with prefix/suffix and no label

--- src/components/mutually-exclusive/mutually-exclusive.js
+++ src/components/mutually-exclusive/mutually-exclusive.js
@@ -89,9 +89,19 @@ class MutuallyExclusive {
   }
 
   getElementLabelText(element) {
-    const label = this.context.querySelector(`label[for=${element.id}]`) || this.context.querySelector('legend');
+    const label =
+      this.context.querySelector(`label[for=${element.id}]`) ||
+      element.parentNode.querySelector('abbr') ||
+      this.context.querySelector('legend');
+
     // This filter is used to strip out any text that is in 'u-vh' elements for accessibility
-    const labelText = [...label.childNodes].filter(node => node.nodeType === 3 && node.textContent.trim())[0].textContent.trim();
+    let labelText;
+
+    if (label.tagName === 'abbr') {
+      labelText = label.getAttribute('title');
+    } else {
+      labelText = [...label.childNodes].filter(node => node.nodeType === 3 && node.textContent.trim())[0].textContent.trim();
+    }
 
     return labelText;
   }
