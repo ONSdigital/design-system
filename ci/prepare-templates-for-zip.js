31c2e407ac2f4c6aa6bbd9de28f95aa944282251
Ben Meyrick <bameyrick@gmail.com>
2019-04-24 15:54:11 +0100

    Create script to copy templates into folder for zipping and applying to the release

---
+++ ci/prepare-templates-for-zip.js
@@ -0,0 +1,41 @@
+import * as fs from 'fs';
+import prependFile from 'prepend-file';
+import { removeFolder, createFolder, asyncForEach } from './helpers';
+import { copyComponents, copyTemplates } from './common';
+
+const cwd = process.cwd();
+const sourcePath = `${cwd}/src`;
+const destPath = `${cwd}/templates`;
+const componentsPath = `${sourcePath}/components`;
+const newComponentsPath = `${destPath}/components`;
+const templatesPath = `${sourcePath}/styles/page-template`;
+const newTemplatesPath = `${destPath}/layout`;
+const copiedPageTemplatePath = `${newTemplatesPath}/_template.njk`;
+
+async function removeExistingFolders() {
+  const folders = [newComponentsPath, newTemplatesPath];
+
+  await asyncForEach(folders, removeFolder);
+}
+
+async function addCDNVersionToPageTemplate() {
+  const version = process.env.RELEASE_VERSION;
+
+  if (version) {
+    const insert = `{% set release_version = '${version}' %}\n`;
+
+    prependFile.sync(copiedPageTemplatePath, insert);
+  } else {
+    throw new Error('RELEASE_VERSION not specified');
+  }
+}
+
+async function run() {
+  await removeExistingFolders();
+  await createFolder(destPath);
+  await copyComponents(componentsPath, newComponentsPath);
+  await copyTemplates(templatesPath, newTemplatesPath);
+  await addCDNVersionToPageTemplate();
+}
+
+run();
