ac3d2d73b3a7577762e817d3243c44598d4442fe
boxadesign <info@boxadesign.co.uk>
2019-08-07 10:46:37 +0100

    travis updated

--- src/tests/config/karma.conf.browserstack-es6-launchers.js
+++ src/tests/config/karma.conf.browserstack-es6-launchers.js
@@ -6,11 +6,11 @@ export default function() {
       // 'bs_iphone_xs',
       // 'bs_nexus_5',
       'bs_mac_chrome',
-      // 'bs_mac_firefox',
-      // 'bs_mac_safari',
-      // 'bs_windows_10_IE_edge',
-      // 'bs_windows_10_chrome',
-      // 'bs_windows_10_firefox',
+      'bs_mac_firefox',
+      'bs_mac_safari',
+      'bs_windows_10_IE_edge',
+      'bs_windows_10_chrome',
+      'bs_windows_10_firefox',
     ],
 
     customLaunchers: {
@@ -42,40 +42,40 @@ export default function() {
         os: 'OS X',
         os_version: 'Mojave',
       }),
-      // bs_mac_firefox: inheritBase({
-      //   browser: 'firefox',
-      //   browser_version: '66.0',
-      //   os: 'OS X',
-      //   os_version: 'Mojave',
-      // }),
-      // bs_mac_safari: inheritBase({
-      //   browser: 'safari',
-      //   browser_version: '12',
-      //   os: 'OS X',
-      //   os_version: 'Mojave',
-      // }),
+      bs_mac_firefox: inheritBase({
+        browser: 'firefox',
+        browser_version: '66.0',
+        os: 'OS X',
+        os_version: 'Mojave',
+      }),
+      bs_mac_safari: inheritBase({
+        browser: 'safari',
+        browser_version: '12',
+        os: 'OS X',
+        os_version: 'Mojave',
+      }),
 
       /**
        * Windows
        */
-      // bs_windows_10_IE_edge: inheritBase({
-      //   os: 'Windows',
-      //   os_version: '10',
-      //   browser: 'Edge',
-      //   browser_version: '18',
-      // }),
-      // bs_windows_10_chrome: inheritBase({
-      //   os: 'Windows',
-      //   os_version: '10',
-      //   browser: 'Chrome',
-      //   browser_version: '75.0',
-      // }),
-      // bs_windows_10_firefox: inheritBase({
-      //   os: 'Windows',
-      //   os_version: '10',
-      //   browser: 'firefox',
-      //   browser_version: '67.0',
-      // }),
+      bs_windows_10_IE_edge: inheritBase({
+        os: 'Windows',
+        os_version: '10',
+        browser: 'Edge',
+        browser_version: '18',
+      }),
+      bs_windows_10_chrome: inheritBase({
+        os: 'Windows',
+        os_version: '10',
+        browser: 'Chrome',
+        browser_version: '75.0',
+      }),
+      bs_windows_10_firefox: inheritBase({
+        os: 'Windows',
+        os_version: '10',
+        browser: 'firefox',
+        browser_version: '67.0',
+      }),
     },
   };
 }
