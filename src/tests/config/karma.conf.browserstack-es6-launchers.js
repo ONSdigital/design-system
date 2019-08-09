4dff861d4c05c2ff8c03707cc372e42edd3e0690
Matt Simon <info@boxadesign.co.uk>
2019-08-09 16:01:55 +0100

    Added spacing for headings within page content (#570)
    * Added spacing for headings within page content
    
    * updated components for spacing improvements
    
    * Updated card component to maintain grid alignment
    
    * Added data attributes to language, footer, navigation
    
    * Removed example
    
    * Removed tests from Travis
    
    * Formatting
    
    * Updated card list example with data attributes
    
    * Restored travis test config
    
    * Removed travis test config
    
    * Updated travis
    
    * Updated browserstack config
    
    * Updated travis
    
    * travis updated
    
    * Updated travis
    
    * Kick off build
    
    * Travis change
    
    * Updated browsers for browstack and karma

--- src/tests/config/karma.conf.browserstack-es6-launchers.js
+++ src/tests/config/karma.conf.browserstack-es6-launchers.js
@@ -4,7 +4,7 @@ export default function() {
   return {
     browsers: [
       // 'bs_iphone_xs',
-      // 'bs_nexus_5',
+      'bs_samsung_galaxy_s9',
       'bs_mac_chrome',
       'bs_mac_firefox',
       'bs_mac_safari',
@@ -20,18 +20,19 @@ export default function() {
       // bs_iphone_xs: inheritBase({
       //   device: 'iPhone XS Max',
       //   os: 'ios',
-      //   os_version: '12.0'
+      //   os_version: '12.1',
+      //   real_mobile: true,
       // }),
 
       /**
        * Android
        */
-      // bs_nexus_5: inheritBase({
-      //   device: 'Google Nexus 5',
-      //   platform: 'ANDROID',
-      //   os: 'android',
-      //   os_version: '5.0'
-      // }),
+      bs_samsung_galaxy_s9: inheritBase({
+        device: 'Samsung Galaxy S9 Plus',
+        os: 'android',
+        os_version: '8.0',
+        real_mobile: true,
+      }),
 
       /**
        * OS X
