2f515422de88e7c178b0b38cd9347e67655226a3
boxadesign <info@boxadesign.co.uk>
2019-08-09 12:44:35 +0100

    Updated karma tests

--- src/tests/config/karma.conf.browserstack-es5-launchers.js
+++ src/tests/config/karma.conf.browserstack-es5-launchers.js
@@ -3,9 +3,9 @@
 export default function() {
   return {
     browsers: [
-      'bs_ipad_air_2',
+      // 'bs_ipad_air_2',
       // 'bs_android_galaxy_S5_mini',
-      // 'bs_mac_safari',
+      'bs_mac_safari',
       'bs_windows_10_IE_11',
     ],
 
@@ -13,13 +13,13 @@ export default function() {
       /**
        * iOS
        */
-      bs_ipad_air_2: inheritBase({
-        device: 'iPad Air 2',
-        browserName: 'iPad',
-        platform: 'MAC',
-        os: 'ios',
-        os_version: '9.1',
-      }),
+      // bs_ipad_air_2: inheritBase({
+      //   device: 'iPad Air 2',
+      //   browserName: 'iPad',
+      //   platform: 'MAC',
+      //   os: 'ios',
+      //   os_version: '9.1',
+      // }),
 
       /**
        * Android
@@ -35,12 +35,12 @@ export default function() {
       /**
        * OS X
        */
-      // bs_mac_safari: inheritBase({
-      //   browser: 'safari',
-      //   browser_version: '9.1',
-      //   os: 'OS X',
-      //   os_version: 'El Capitan',
-      // }),
+      bs_mac_safari: inheritBase({
+        browser: 'safari',
+        browser_version: '9.1',
+        os: 'OS X',
+        os_version: 'El Capitan',
+      }),
 
       /**
        * Windows
