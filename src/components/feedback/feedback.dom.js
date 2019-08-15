8eadc4f2c8efacfb54a839f72deddf3a3da7cc18
Matt Simon <info@boxadesign.co.uk>
2019-08-15 15:17:13 +0100

    Fixed conditional issue with feedback (#582)

--- src/components/feedback/feedback.dom.js
+++ src/components/feedback/feedback.dom.js
@@ -2,10 +2,7 @@ import domready from 'js/domready';
 
 async function initialise() {
   const buttons = [...document.querySelectorAll('.js-feedback-button')];
-
-  if (buttons.length) {
-    const Feedback = (await import('./feedback')).default;
-    new Feedback(buttons);
-  }
+  const Feedback = (await import('./feedback')).default;
+  new Feedback(buttons);
 }
 domready(initialise);
