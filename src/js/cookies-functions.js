ddaeb7d893dcd1eb8d8acfc000441fbbff385a72
boxadesign <info@boxadesign.co.uk>
2019-09-17 23:49:44 +0100

    Updated domain settings for cookies

--- src/js/cookies-functions.js
+++ src/js/cookies-functions.js
@@ -17,9 +17,6 @@ export const COOKIE_CATEGORIES = {
   VISITOR_INFO1_LIVE: 'campaigns',
 };
 
-const domain = getDomain(document.domain);
-const setDomain = !domain.indexOf('locahost') ? ';domain=' + domain : '';
-
 export function cookie(name, value, options) {
   if (typeof value !== 'undefined') {
     if (value === false || value === null) {
@@ -61,6 +58,8 @@ export function getConsentCookie() {
 }
 
 export function setConsentCookie(options) {
+  const domain = getDomain(document.domain);
+
   let cookieConsent = getConsentCookie();
   if (!cookieConsent) {
     cookieConsent = JSON.parse(JSON.stringify(DEFAULT_COOKIE_CONSENT).replace(/'/g, '"'));
@@ -72,8 +71,9 @@ export function setConsentCookie(options) {
         if (COOKIE_CATEGORIES[cookies] === cookieType) {
           cookie(cookies, null);
           if (cookie(cookies)) {
-            const cookieString = cookies + '=;expires=' + new Date() + setDomain + ';path=/';
+            const cookieString = cookies + '=; expires=' + new Date() + '; domain=' + domain + '; path=/';
             document.cookie = cookieString;
+            console.log('remove cookie:', cookieString);
           }
         }
       }
@@ -111,6 +111,9 @@ export function checkConsentCookie(cookieName, cookieValue) {
 }
 
 export function setCookie(name, value, options) {
+  const domain = getDomain(document.domain);
+  const setDomain = !domain.indexOf('locahost') ? '; domain=' + domain : '';
+
   if (checkConsentCookie(name, value)) {
     if (typeof options === 'undefined') {
       options = {};
@@ -120,11 +123,13 @@ export function setCookie(name, value, options) {
     if (options.days) {
       const date = new Date();
       date.setTime(date.getTime() + options.days * 24 * 60 * 60 * 1000);
-      cookieString = cookieString + '; expires=' + date.toGMTString() + setDomain;
+      cookieString = cookieString + '; expires=' + date.toGMTString();
     }
     if (document.location.protocol === 'https:') {
       cookieString = cookieString + '; Secure';
     }
+    cookieString = cookieString + setDomain;
+    console.log('set cookie:', cookieString);
     document.cookie = cookieString;
   }
 }
