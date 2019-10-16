74e0ba7459043d04b30933b007dd56a1106ae85b
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2019-10-16 14:55:06 +0100

    refactor to now change button on submission of form

--- src/components/button/button.dom.js
+++ src/components/button/button.dom.js
@@ -2,11 +2,12 @@ import domready from 'js/domready';
 
 async function submitButton() {
   const buttons = [...document.querySelectorAll('.js-button')];
+  const form = [...document.getElementsByTagName('form')];
 
-  if (buttons.length) {
+  if (buttons.length && form.length) {
     const SubmitButton = (await import('./button')).default;
     buttons.forEach(button => {
-      new SubmitButton(button);
+      new SubmitButton(button, form[0]);
     });
   }
 }
