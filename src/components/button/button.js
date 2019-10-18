82aa2ba64d8eff13abefbb028f94b992ab8fb105
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2019-10-18 15:34:29 +0100

    add timed version of loader button

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,12 +1,28 @@
-export default class LoaderButton {
-  constructor(button, form) {
+export default class SubmitButton {
+  constructor(button, form, submitType) {
     this.button = button;
     this.form = form;
-    this.form.addEventListener('submit', this.loaderButton.bind(this));
+    this.submitType = submitType;
+    if (this.submitType == 'loader') {
+      this.form.addEventListener('submit', this.loaderButton.bind(this));
+    } else if (this.submitType == 'timer') {
+      this.form.addEventListener('submit', this.timerButton.bind(this));
+    }
   }
 
   loaderButton() {
     this.button.classList.add('is-loading');
     this.button.setAttribute('disabled', true);
+  }
+
+  timerButton() {
+    this.button.setAttribute('disabled', true);
+    setTimeout(
+      function(button) {
+        button.removeAttribute('disabled');
+      },
+      1000,
+      this.button,
+    );
   }
 }
--- src/tests/spec/loader-button/loader-button.spec.js
+++ src/tests/spec/loader-button/loader-button.spec.js
@@ -1,14 +1,74 @@
 import { awaitPolyfills } from 'js/polyfills/await-polyfills';
 import template from 'components/button/_test-template.njk';
-import LoaderButton from 'components/button/button';
+import SubmitButton from 'components/button/button';
 
-const params = {
+let params = {
   id: 'button',
   text: 'Submit',
-  loader: true,
+  submitType: 'loader',
 };
 
 describe('Function: Loader Button ', function() {
+  let wrapper, buttonElement, formElement;
+
+  before(() => awaitPolyfills);
+
+  beforeEach(() => {
+    const html = template.render({ params });
+
+    wrapper = document.createElement('div');
+    wrapper.innerHTML = html;
+    document.body.appendChild(wrapper);
+    buttonElement = document.getElementById(params.id);
+    formElement = document.getElementsByTagName('form');
+  });
+
+  afterEach(() => {
+    if (wrapper) {
+      wrapper.remove();
+    }
+  });
+
+  describe('Before the button is initialised', () => {
+    it('Button should have relevant classes', () => {
+      expect(buttonElement.classList.contains('btn--loader')).to.be.true;
+      expect(buttonElement.classList.contains('js-loader')).to.be.true;
+      expect(buttonElement.classList.contains('js-submit-btn')).to.be.true;
+    });
+  });
+
+  describe('Once the button is initialised', () => {
+    beforeEach(() => {
+      new SubmitButton(buttonElement, formElement[0], params.submitType);
+    });
+
+    it('Button disabled attribute should not be set', () => {
+      expect(buttonElement.getAttribute('disabled')).to.not.exist;
+    });
+
+    describe('and the button is clicked', () => {
+      beforeEach(() => {
+        buttonElement.click();
+      });
+
+      it('Button should have loading style applied', () => {
+        expect(buttonElement.classList.contains('is-loading')).to.be.true;
+      });
+
+      it('Button should be disabled', () => {
+        expect(buttonElement.getAttribute('disabled')).to.equal('true');
+      });
+    });
+  });
+});
+
+params = {
+  id: 'button',
+  text: 'Submit',
+  submitType: 'timer',
+};
+
+describe.only('Function: Timer Button ', function() {
   let wrapper, buttonElement, formElement;
 
   before(() => awaitPolyfills);
@@ -32,14 +92,15 @@ describe('Function: Loader Button ', function() {
 
   describe('Before the button is initialised', () => {
     it('Button should have relevant classes', () => {
-      expect(buttonElement.classList.contains('btn--loader')).to.be.true;
-      expect(buttonElement.classList.contains('js-loader-btn')).to.be.true;
+      expect(buttonElement.classList.contains('btn--loader')).to.be.false;
+      expect(buttonElement.classList.contains('js-timer')).to.be.true;
+      expect(buttonElement.classList.contains('js-submit-btn')).to.be.true;
     });
   });
 
   describe('Once the button is initialised', () => {
     beforeEach(() => {
-      new LoaderButton(buttonElement, formElement[0]);
+      new SubmitButton(buttonElement, formElement[0], params.submitType);
     });
 
     it('Button disabled attribute should not be set', () => {
@@ -51,12 +112,22 @@ describe('Function: Loader Button ', function() {
         buttonElement.click();
       });
 
-      it('Button should have loading style applied', () => {
-        expect(buttonElement.classList.contains('is-loading')).to.be.true;
+      it('Button should not have loading style applied', () => {
+        expect(buttonElement.classList.contains('is-loading')).to.be.false;
       });
 
       it('Button should be disabled', () => {
+        console.log(buttonElement);
         expect(buttonElement.getAttribute('disabled')).to.equal('true');
+      });
+
+      it('After timer button disabled attribute should not be set', () => {
+        setTimeout(function() {
+          console.log('waiting over.');
+          done();
+        }, 30000000000);
+        console.log(buttonElement);
+        expect(buttonElement.getAttribute('disabled')).to.not.exist;
       });
     });
   });
