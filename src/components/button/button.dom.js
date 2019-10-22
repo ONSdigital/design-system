2d95fde3c8d916749d16c1d67497a28e99e9e2b1
rmccar <42928680+rmccar@users.noreply.github.com>
2019-10-22 08:58:58 +0100

    Add second version of loader button (#644)
    * changed submit button to loader button
    
    * add timed version of loader button
    
    * add timed version of loader button
    
    * remove second version from example
    
    * remove test changes
    
    * pr comment
    
    * pr comment
    
    * update example button text
    
    * fixed tests
    
    * remove second button from example
    
    * remove log line

--- src/components/button/button.dom.js
+++ src/components/button/button.dom.js
@@ -1,13 +1,19 @@
 import domready from 'js/domready';
 
 async function submitButton() {
-  const buttons = [...document.querySelectorAll('.js-button')];
+  const buttons = [...document.querySelectorAll('.js-submit-btn')];
   const form = [...document.getElementsByTagName('form')];
+  let submitType;
 
   if (buttons.length && form.length) {
     const SubmitButton = (await import('./button')).default;
     buttons.forEach(button => {
-      new SubmitButton(button, form[0]);
+      if (button.classList.contains('js-timer')) {
+        submitType = 'timer';
+      } else if (button.classList.contains('js-loader')) {
+        submitType = 'loader';
+      }
+      new SubmitButton(button, form[0], submitType);
     });
   }
 }
