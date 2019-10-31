c1a3b2c0af8f781dc71dc18001b6f62fcda65bad
Matt Simon <info@boxadesign.co.uk>
2019-10-31 23:04:15 +0000

    Added search component with character check

---
+++ src/components/search/character-check.js
@@ -0,0 +1,69 @@
+const inputClassLimitReached = 'input--limit-reached';
+const remainingClassLimitReached = 'input__limit--reached';
+const attrCharCheckRef = 'data-char-check-ref';
+const attrCharCheckVal = 'data-char-check-num';
+
+export default class CharLimit {
+  constructor(context) {
+    this.context = context;
+    this.input = this.context.querySelector('input');
+    this.button = this.context.querySelector('button');
+    this.checkElement = document.getElementById(this.input.getAttribute(attrCharCheckRef));
+    this.checkVal = this.input.getAttribute(attrCharCheckVal);
+    this.singularMessage = this.checkElement.getAttribute('data-charcount-singular');
+    this.pluralMessage = this.checkElement.getAttribute('data-charcount-plural');
+    this.charLimitSingularMessage = this.checkElement.getAttribute('data-charcount-limit-singular');
+    this.charLimitPluralMessage = this.checkElement.getAttribute('data-charcount-limit-plural');
+
+    this.updateCheckReadout(null, true);
+    this.setButtonState(this.checkVal);
+    this.context.addEventListener('input', this.updateCheckReadout.bind(this));
+  }
+
+  updateCheckReadout(event, firstRun) {
+    const value = this.input.value;
+    const remaining = this.checkVal - value.length;
+
+    // Prevent aria live announcement when component initialises
+    if (!firstRun && event.inputType) {
+      this.checkElement.setAttribute('aria-live', 'polite');
+    } else {
+      this.checkElement.removeAttribute('aria-live');
+    }
+
+    this.checkRemaining(remaining);
+    this.setCheckClass(remaining, this.input, inputClassLimitReached);
+    this.setCheckClass(remaining, this.checkElement, remainingClassLimitReached);
+  }
+
+  checkRemaining(remaining) {
+    let message;
+    if (remaining === 1) {
+      message = this.singularMessage;
+    } else if (remaining === -1) {
+      message = this.charLimitSingularMessage;
+      remaining = Math.abs(remaining);
+    } else if (remaining < -1) {
+      message = this.charLimitPluralMessage;
+      remaining = Math.abs(remaining);
+    } else {
+      message = this.pluralMessage;
+    }
+    this.setShowMessage(remaining);
+    this.setButtonState(remaining);
+    this.checkElement.innerText = message.replace('{x}', remaining);
+  }
+
+  setButtonState(remaining) {
+    this.button.classList[remaining === 0 ? 'remove' : 'add']('btn--disabled');
+    this.button.disabled = remaining === 0 ? null : 'true';
+  }
+
+  setShowMessage(remaining) {
+    this.checkElement.classList[(remaining < this.checkVal && remaining > 0) || remaining < 0 ? 'remove' : 'add']('u-d-no');
+  }
+
+  setCheckClass(remaining, element, setClass) {
+    element.classList[remaining < 0 ? 'add' : 'remove'](setClass);
+  }
+}
