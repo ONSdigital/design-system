ba61105208ab9bbcb8e128359deaf713e4bef32c
Matt Simon <info@boxadesign.co.uk>
2019-11-01 13:11:48 +0000

    Added js for placeholder

---
+++ src/components/search/search-focus.js
@@ -0,0 +1,13 @@
+export default class FocusInput {
+  constructor(input) {
+    this.input = input;
+    this.input.addEventListener('focus', this.setFocusState.bind(this));
+    this.input.addEventListener('input', this.setFocusState.bind(this));
+  }
+
+  setFocusState() {
+    const value = this.input.value.length;
+    console.log(value);
+    this.input.classList[value > 0 ? 'add' : 'remove']('input--has-content');
+  }
+}
