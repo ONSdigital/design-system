f02eadee684b12a7f76ec8fb89395688be355c74
Matt Simon <info@boxadesign.co.uk>
2019-12-10 15:04:04 +0000

    Added clear radios functionality and visible ins (#660)
    * Added clear radios functionality and visible ins
    * Added unit tests and documentation for new examples and params
    * Added param check for password on input
    * clear button focus state and no-js reset in example
    * show clear button on focus of visible other input
    * Formatted for compatibility with jinja templates
    * reset button type for no js solution
    * amended typo in documentation
    * added limitations of reset button note to documentation

--- src/components/radios/radios.dom.js
+++ src/components/radios/radios.dom.js
@@ -7,5 +7,22 @@ domready(async () => {
     const Radios = (await import('components/checkboxes/checkboxes')).default;
 
     new Radios(radios);
+
+    const button = document.querySelector('.js-clear-btn');
+    const otherInput = document.querySelector('.radio__other');
+    if (button) {
+      const ClearRadios = (await import('./clear-radios')).default;
+
+      new ClearRadios(radios, button, otherInput);
+    }
+
+    const openOther = document.querySelector('.radio__other--open');
+    if (openOther) {
+      const parent = openOther.parentNode;
+      const radioInput = parent.querySelector('.radio__input');
+      const CheckRadios = (await import('./check-radios')).default;
+
+      new CheckRadios(radioInput, openOther);
+    }
   }
 });
