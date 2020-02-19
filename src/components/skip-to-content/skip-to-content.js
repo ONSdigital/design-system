20bfaa9ee83693b1ed39f7281e838669342c7aaa
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2020-02-19 15:07:10 +0000

    removed log

--- src/components/skip-to-content/skip-to-content.js
+++ src/components/skip-to-content/skip-to-content.js
@@ -2,7 +2,6 @@ export default function skipToMain(link) {
   const id = link.getAttribute('href').replace('#', '');
   link.addEventListener('click', event => {
     event.preventDefault();
-    console.log(id);
     document.getElementById(id).scrollIntoView();
   });
 }
