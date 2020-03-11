df260c4993e4098c3655fecdea3d58875a99607b
Matthew Palmer <matthewandrewpalmer@outlook.com>
2020-03-11 16:54:04 +0000

    charcheckMinOnly parameter to only check that input is exact or above the check-num value

--- src/components/search/character-check.js
+++ src/components/search/character-check.js
@@ -2,6 +2,7 @@ const inputClassLimitReached = 'input--limit-reached';
 const remainingClassLimitReached = 'input__limit--reached';
 const attrCharCheckRef = 'data-char-check-ref';
 const attrCharCheckVal = 'data-char-check-num';
+const attrCharCheckMin = 'data-char-check-min-only';
 
 export default class CharCheck {
   constructor(context) {
@@ -10,6 +11,7 @@ export default class CharCheck {
     this.button = this.context.querySelector('button');
     this.checkElement = document.getElementById(this.input.getAttribute(attrCharCheckRef));
     this.checkVal = this.input.getAttribute(attrCharCheckVal);
+    this.checkMinOnly = this.input.getAttribute(attrCharCheckMin);
 
     this.singularMessage = this.checkElement.getAttribute('data-charcount-singular');
     this.pluralMessage = this.checkElement.getAttribute('data-charcount-plural');
@@ -24,6 +26,7 @@ export default class CharCheck {
   updateCheckReadout(event, firstRun) {
     const value = this.input.value;
     const remaining = this.checkVal - value.length;
+    const noMaxCheck = this.checkMinOnly;
     // Prevent aria live announcement when component initialises
     if (!firstRun && event.inputType) {
       this.checkElement.setAttribute('aria-live', 'polite');
@@ -31,36 +34,36 @@ export default class CharCheck {
       this.checkElement.removeAttribute('aria-live');
     }
 
-    this.checkRemaining(remaining);
+    this.checkRemaining(remaining, noMaxCheck);
     this.setCheckClass(remaining, this.input, inputClassLimitReached);
     this.setCheckClass(remaining, this.checkElement, remainingClassLimitReached);
   }
 
-  checkRemaining(remaining) {
+  checkRemaining(remaining, noMaxCheck) {
     let message;
     if (remaining === 1) {
       message = this.singularMessage;
-    } else if (remaining === -1) {
+    } else if (remaining > 1) {
+      message = this.pluralMessage;
+    } else if (remaining === -1 && !noMaxCheck) {
       message = this.charLimitSingularMessage;
       remaining = Math.abs(remaining);
-    } else if (remaining < -1) {
+    } else if (remaining < -1 && !noMaxCheck) {
       message = this.charLimitPluralMessage;
       remaining = Math.abs(remaining);
-    } else {
-      message = this.pluralMessage;
     }
-    this.setShowMessage(remaining);
-    this.setButtonState(remaining);
+    this.setShowMessage(remaining, noMaxCheck);
+    this.setButtonState(remaining, noMaxCheck);
     this.checkElement.innerText = message.replace('{x}', remaining);
   }
 
-  setButtonState(remaining) {
-    this.button.classList[remaining === 0 ? 'remove' : 'add']('btn--disabled');
-    this.button.disabled = remaining === 0 ? null : 'true';
+  setButtonState(remaining, noMaxCheck) {
+    this.button.classList[(remaining === 0 || remaining < 0 && noMaxCheck) ? 'remove' : 'add']('btn--disabled');
+    this.button.disabled = (remaining === 0 || remaining < 0 && noMaxCheck) ? null : 'true';
   }
 
-  setShowMessage(remaining) {
-    this.checkElement.classList[(remaining < this.checkVal && remaining > 0) || remaining < 0 ? 'remove' : 'add']('u-d-no');
+  setShowMessage(remaining, noMaxCheck) {
+    this.checkElement.classList[(remaining < this.checkVal && remaining > 0) || (remaining < 0 && !noMaxCheck) ? 'remove' : 'add']('u-d-no');
   }
 
   setCheckClass(remaining, element, setClass) {
