3053bd7245e46f92d8afe75d9f951ab1fdd11d3c
rmccar <42928680+rmccar@users.noreply.github.com>
2020-07-31 15:32:18 +0100

    bump Codecov to 3.7.2 (#861)
    * add alignment css (#845)
    
    * Bump codecov from 3.7.0 to 3.7.1 (#846)
    
    Bumps [codecov](https://github.com/codecov/codecov-node) from 3.7.0 to 3.7.1.
    - [Release notes](https://github.com/codecov/codecov-node/releases)
    - [Commits](https://github.com/codecov/codecov-node/compare/v3.7.0...v3.7.1)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * add is defined to label if (#836)
    
    * Conform css to use font weight and colour variables (#840)
    
    * confirm css to use font weight variable and use variables for colour codes
    
    * added background style to all headings containing emphasis tag (#849)
    
    * fix bug with ghost button example being transparent (#848)
    
    * update minimist and all packages related to it (#843)
    
    * Change field macro (#856)
    
    * Added check for char limit on load
    
    * Swapped out p for div in onsField macro
    
    * Updated docs
    
    * Bump elliptic from 6.5.2 to 6.5.3 (#860)
    
    Bumps [elliptic](https://github.com/indutny/elliptic) from 6.5.2 to 6.5.3.
    - [Release notes](https://github.com/indutny/elliptic/releases)
    - [Commits](https://github.com/indutny/elliptic/compare/v6.5.2...v6.5.3)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Change warning panel padding (#842)
    
    * add warning panel example to collapsible
    
    * remove left padding for warning panels
    
    * adjust warning panel padding for in collapseables
    
    * Update src/components/collapsible/examples/collapsible-with-warning/index.njk
    
    Co-authored-by: Jon Barnes <43346934+jrbarnes9@users.noreply.github.com>
    
    * Update src/components/collapsible/examples/collapsible-with-warning/index.njk
    
    Co-authored-by: Jon Barnes <43346934+jrbarnes9@users.noreply.github.com>
    
    * Update src/components/collapsible/examples/collapsible-with-warning/index.njk
    
    Co-authored-by: Jon Barnes <43346934+jrbarnes9@users.noreply.github.com>
    
    Co-authored-by: Jon Barnes <43346934+jrbarnes9@users.noreply.github.com>
    
    * Allow mutually exclusive input with charCheck  (#857)
    
    * change text area to use charlimit
    
    * fix to allow mutual exclsuive and char check fields together
    
    * remove whitespace
    
    * remove whitespace
    
    * remove duplicate check in ifs
    
    * refactor char-check-limit
    
    * removed redundant if
    
    * change textarea to work with charcheck object
    
    * rename charchecklimit object
    
    * fix tests
    
    * update docs
    
    * bump codecov to 3.7.2
    
    * Add 'is defined' to all components (#839)
    
    * add is defined to all components
    
    * fix tests
    
    * Update src/components/field/_macro.njk
    
    Co-authored-by: Jon Barnes <43346934+jrbarnes9@users.noreply.github.com>
    
    * Update src/components/collapsible/_macro.njk
    
    Co-authored-by: Jon Barnes <43346934+jrbarnes9@users.noreply.github.com>
    
    * redo changes to pass tests
    
    Co-authored-by: Jon Barnes <43346934+jrbarnes9@users.noreply.github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Jon Barnes <43346934+jrbarnes9@users.noreply.github.com>
    Co-authored-by: Matt Simon <info@boxadesign.co.uk>

--- src/components/textarea/textarea.dom.js
+++ src/components/textarea/textarea.dom.js
@@ -2,11 +2,11 @@ import domready from 'js/domready';
 import autosize from 'autosize';
 
 async function initialise() {
-  const limitedInputs = [...document.querySelectorAll('.js-charlimit-input')];
+  const limitedInputs = [...document.querySelectorAll('.js-char-limit-input')];
   autosize(document.querySelectorAll('textarea'));
 
   if (limitedInputs.length) {
-    const CharLimit = (await import('./character-limit')).default;
+    const CharLimit = (await import('../char-check-limit/character-limit')).default;
 
     limitedInputs.forEach(input => new CharLimit(input));
   }
