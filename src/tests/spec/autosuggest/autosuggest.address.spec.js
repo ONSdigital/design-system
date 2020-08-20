2df14be8893a08476ad76c370ee5956beba376d7
boxadesign <info@boxadesign.co.uk>
2020-08-20 15:42:10 +0100

    More test coverage

--- src/tests/spec/autosuggest/autosuggest.address.spec.js
+++ src/tests/spec/autosuggest/autosuggest.address.spec.js
@@ -1,13 +1,8 @@
 import { awaitPolyfills } from 'js/polyfills/await-polyfills';
 import template from 'components/address/_test-template.njk';
 import '../../../scss/main.scss';
-import AutosuggestAddress, {
-  classInputContainer,
-  classNotEditable,
-  classSearch,
-  classInput,
-  classInputUPRN,
-} from '../../../components/input/autosuggest/autosuggest.address';
+import AutosuggestAddress from '../../../components/input/autosuggest/autosuggest.address';
+import AutosuggestUI from '../../../components/input/autosuggest/autosuggest.ui';
 import eventMock from 'stubs/event.stub.spec';
 import fetchMock from 'stubs/window.fetch.stub.spec';
 
@@ -63,7 +58,7 @@ const params = {
   manualButton: 'Manually enter address',
 };
 
-describe('Autosuggest.address component', function() {
+describe.only('Autosuggest.address component', function() {
   before(function(done) {
     awaitPolyfills.then(() => {
       this.rewiremock = require('rewiremock/webpack').default;
@@ -165,7 +160,8 @@ describe('Autosuggest.address component', function() {
         this.findAddressSpy = chai.spy.on(this.autosuggestAddress, 'findAddress');
         this.testPostcodeSpy = chai.spy.on(this.autosuggestAddress, 'testFullPostcodeQuery');
       });
-      describe('and the value does not match an existing query', function() {
+
+      describe('when the value does not match an existing query', function() {
         beforeEach(function(done) {
           this.autosuggestAddress.suggestAddresses('195 colle', [], false);
           setTimeout(done);
@@ -174,7 +170,8 @@ describe('Autosuggest.address component', function() {
           expect(this.findAddressSpy).to.have.been.called();
         });
       });
-      describe('and the value is a full postcode', function() {
+
+      describe('when the value is a full postcode', function() {
         const postcode = 'CF14 2NT';
         beforeEach(function(done) {
           this.autosuggestAddress.findAddress(postcode);
@@ -183,6 +180,158 @@ describe('Autosuggest.address component', function() {
         it('then the testFullPostcodeQuery function should return true', function() {
           expect(this.testPostcodeSpy).to.have.been.called();
           expect(this.autosuggestAddress.testFullPostcodeQuery(postcode)).to.equal(true);
+        });
+      });
+
+      describe('when addresses are retrieved', function() {
+        describe('when the query is not a part postcode', function() {
+          beforeEach(function(done) {
+            this.results = {
+              response: {
+                input: '196 coll',
+                addresses: [
+                  {
+                    uprn: '100070332099',
+                    bestMatchAddress: '196 College Road, Birmingham, B44 8HF',
+                    bestMatchAddressType: 'PAF',
+                  },
+                  {
+                    uprn: '100100119969',
+                    bestMatchAddress: '196 College Road, Whitchurch, Cardiff, CF14 2NZ',
+                    bestMatchAddressType: 'PAF',
+                  },
+                ],
+              },
+            };
+            this.mapAddressesSpy = chai.spy.on(this.autosuggestAddress, 'addressMapping');
+            this.autosuggestAddress.mapFindResults(this.results.response, 10, 200);
+            setTimeout(done);
+          });
+
+          it('then the addressMapping function will be called', function() {
+            expect(this.mapAddressesSpy).to.have.been.called();
+          });
+        });
+        describe('when the query is a part postcode', function() {
+          beforeEach(function(done) {
+            this.results = {
+              response: {
+                partpostcode: 'cf14 2',
+                postcodes: [
+                  {
+                    postcode: 'CF14 2AA',
+                    streetName: 'Penlline Road',
+                    townName: 'Whitchurch',
+                    addressCount: 41,
+                    firstUprn: 10002526869,
+                    postTown: 'Cardiff',
+                  },
+                  {
+                    postcode: 'CF14 2AB',
+                    streetName: 'Penlline Road',
+                    townName: 'Whitchurch',
+                    addressCount: 1,
+                    firstUprn: 10002511038,
+                    postTown: 'Cardiff',
+                  },
+                ],
+              },
+            };
+            this.postcodeGroupsMappingSpy = chai.spy.on(this.autosuggestAddress, 'postcodeGroupsMapping');
+            this.replaceSingleCountAddressesSpy = chai.spy.on(this.autosuggestAddress, 'replaceSingleCountAddresses');
+            this.createAddressObjectSpy = chai.spy.on(this.autosuggestAddress, 'createAddressObject');
+            this.autosuggestAddress.mapFindResults(this.results.response, 10, 200);
+            setTimeout(done);
+          });
+
+          it('then the postcodeGroupsMapping function will be called', function() {
+            expect(this.postcodeGroupsMappingSpy).to.have.been.called();
+          });
+
+          it('then the replaceSingleCountAddresses function will be called', function() {
+            expect(this.replaceSingleCountAddressesSpy).to.have.been.called();
+          });
+
+          describe('when the results contain an address with an address count of 1', function() {
+            it('then the createAddressObject function will be called', function() {
+              expect(this.createAddressObjectSpy).to.have.been.called();
+            });
+          });
+        });
+
+        describe('when a non-grouped address is selected', function() {
+          beforeEach(function(done) {
+            this.selectedResult = {
+              lang: '195 College Road, Whitchurch, Cardiff, CF14 2NT',
+              sanitisedText: '195 college road whitchurch cardiff cf14 2nt',
+              uprn: '100100119968',
+              displayText: '195 College Road, Whitchurch, Cardiff, CF14 2NT',
+            };
+            this.retrieveAddressSpy = chai.spy.on(this.autosuggestAddress, 'retrieveAddress');
+            this.autosuggestAddress.onAddressSelect(this.selectedResult);
+            setTimeout(done);
+          });
+
+          it('then the retrieveAddress function will be called', function() {
+            expect(this.retrieveAddressSpy).to.have.been.called();
+          });
+
+          describe('when the address is retrieved', function() {
+            beforeEach(function() {
+              this.address = {
+                response: {
+                  address: {
+                    uprn: '100100119968',
+                    formattedAddress: '195 College Road, Whitchurch, Cardiff, CF14 2NT',
+                    addressLine1: '195 College Road',
+                    addressLine2: 'Whitchurch',
+                    addressLine3: '',
+                    townName: 'Cardiff',
+                    postcode: 'CF14 2NT',
+                    foundAddressType: 'PAF',
+                  },
+                },
+              };
+            });
+
+            it('then the address should be formatted correctly', function(done) {
+              const formattedAddress = {
+                addressLine1: '195 College Road',
+                addressLine2: 'Whitchurch',
+                addressLine3: '',
+                townName: 'Cardiff',
+                postcode: 'CF14 2NT',
+              };
+              expect(this.autosuggestAddress.createAddressLines(this.address, done)).to.equal(formattedAddress);
+            });
+          });
+        });
+
+        describe('when a grouped address is selected', function() {
+          beforeEach(function(done) {
+            this.selectedResult = {
+              lang: 'Penlline Road, Whitchurch, Cardiff, CF14 2AA',
+              postcode: 'CF14 2AA',
+              streetName: 'Penlline Road',
+              townName: 'Whitchurch',
+              postTown: 'Cardiff',
+            };
+
+            this.handleChangeSpy = chai.spy.on(this.autosuggestAddress.autosuggest, 'handleChange');
+
+            setTimeout(() => {
+              this.autosuggestAddress.onAddressSelect(this.selectedResult);
+              done();
+            });
+          });
+
+          it('then the handleChange function will be called', function() {
+            expect(this.handleChangeSpy).to.have.been.called();
+          });
+
+          it('then input value should be the same as the selected results', function() {
+            expect(this.autosuggestAddress.input.value).to.equal(this.selectedResult.lang);
+          });
         });
       });
     });
