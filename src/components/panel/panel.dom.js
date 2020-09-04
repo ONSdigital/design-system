3f3cb4dc50c3a5be9eddb690153f0ee26040b0fe
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2020-09-04 14:28:58 +0100

    remove console.log

--- src/components/panel/panel.dom.js
+++ src/components/panel/panel.dom.js
@@ -3,7 +3,6 @@ import errorPanel from './panel';
 
 async function initaliseErrorPanel() {
   const panel = document.querySelector('.js-error-panel');
-  console.log('hello');
   if (panel) {
     errorPanel(panel);
   }
