205301232f47cfdd74b1c519bd7d5a7dcc469773
boxadesign <info@boxadesign.co.uk>
2020-10-07 14:13:32 +0100

    Progress with percy

--- src/tests/visual/snapshot.js
+++
@@ -1,7 +0,0 @@
-const PercyScript = require('@percy/script');
-
-PercyScript.run(async (page, percySnapshot) => {
-  await page.goto('http://localhost:3030');
-  await page.waitFor('.patternlib-page');
-  await percySnapshot('homepage');
-});
