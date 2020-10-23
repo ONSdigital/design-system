c1ffe57c611c3ced4464017fd3020b7c3fef399f
boxadesign <info@boxadesign.co.uk>
2020-10-23 11:46:13 +0100

    Updated tests

--- src/tests/spec/radios/radios.spec.js
+++ src/tests/spec/radios/radios.spec.js
@@ -208,10 +208,10 @@ describe('Component: Radios', function() {
       });
     });
 
-    describe('and there is a visible input and the radio is focused', function() {
+    describe('and there is a visible input and the radio is checked', function() {
       beforeEach(function() {
         this.input = this.openOther.querySelector('.input');
-        this.radioInput.focus();
+        this.radioInput.click();
       });
 
       it('then the input should have a tab index of 0', function(done) {
