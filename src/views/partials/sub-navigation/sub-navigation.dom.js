80420dc23070f60fb73735f09f9a6ff4eb522ca0
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2020-11-23 16:06:38 +0000

    sub navigation scroll current page into view

---
+++ src/views/partials/sub-navigation/sub-navigation.dom.js
@@ -0,0 +1,11 @@
+import domready from 'js/domready';
+import subNavigation from './sub-navigation';
+
+async function initaliseSubNavigation() {
+  const subNavCurrentPage = document.querySelector('.js-patternlib-subnav__item--active');
+  if (subNavCurrentPage) {
+    subNavigation(subNavCurrentPage);
+  }
+}
+
+domready(initaliseSubNavigation);
---
+++ src/views/partials/sub-navigation/sub-navigation.js
@@ -0,0 +1,3 @@
+export default function subNavigation(subNavCurrentPage) {
+  subNavCurrentPage.scrollIntoView();
+}
