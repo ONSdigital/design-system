5cd07e7eca52d6a0449d661708c89508ac8efc4f
boxadesign <info@boxadesign.co.uk>
2020-12-08 12:42:14 +0000

    fix typo in test

--- src/tests/spec/autosuggest/autosuggest.address.spec.js
+++ src/tests/spec/autosuggest/autosuggest.address.spec.js
@@ -922,7 +922,7 @@ describe('Autosuggest.address component', function() {
       it('then the fetch url should contain the correct parameters', function() {
         this.limit = 10;
         expect(this.autosuggestAddress.fetch.url).to.equal(
-          '/addresses/eq?input=195 colle&limit=10&classificationfilter=workplace&=wboost=10&favourwelsh=true',
+          '/addresses/eq?input=195 colle&limit=10&classificationfilter=workplace&wboost=10&favourwelsh=true',
         );
       });
     });
