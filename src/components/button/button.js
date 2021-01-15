4d7cf1c73b091f26efa126f5b6262f507faf245a
boxadesign <info@boxadesign.co.uk>
2021-01-15 15:00:51 +0000

    Revert timer js to see if random tests still fail

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,32 +1,31 @@
 let i = 0;
-export const classPatternLibForm = 'js-patternlib-form';
 export default class SubmitButton {
   constructor(button, submitType) {
     this.button = button;
     this.form = [...document.getElementsByTagName('form')][0];
-    this.patternLibForm = this.form.classList.contains(classPatternLibForm) ? true : false;
     this.submitType = submitType;
 
     if (this.submitType == 'loader') {
-      this.button.addEventListener('click', this.loaderButton.bind(this));
+      if (this.form.length) {
+        this.form.addEventListener('submit', this.loaderButton.bind(this));
+      } else {
+        this.button.addEventListener('click', this.loaderButton.bind(this));
+      }
     } else if (this.submitType == 'timer') {
-      this.button.addEventListener('click', this.timerButton.bind(this));
+      if (this.form.length) {
+        this.form.addEventListener('submit', this.timerButton.bind(this));
+      } else {
+        this.button.addEventListener('click', this.timerButton.bind(this));
+      }
     }
   }
 
   loaderButton() {
-    if (this.form && this.form.length && !this.patternLibForm) {
-      console.log(this.form);
-      this.form.submit();
-    }
     this.button.classList.add('is-loading');
     this.button.setAttribute('disabled', true);
   }
 
   timerButton(event) {
-    if (this.form && this.form.length && !this.patternLibForm) {
-      this.form.submit();
-    }
     if (this.button.tagName === 'A') {
       i++;
       if (i > 1) {
