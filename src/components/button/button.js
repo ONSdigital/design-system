016531e0859c988a614624b42cd7a047da425f36
Matt Simon <info@boxadesign.co.uk>
2021-01-19 15:32:12 +0000

    Rework form submission for timer/loader button (#1285)
    * Rework form submission for timer/loader button
    
    * Added condition to ignore pattern lib forms
    
    * Revert timer js to see if random tests still fail
    
    * bring back new js
    
    * Update js
    
    * bring back form submission on loader button
    
    * trying to fix tests
    
    * trying to fix tests
    
    * trying to fix tests
    
    * trying to fix tests
    
    * trying to fix tests
    
    * trying to fix tests
    
    * trying to fix tests
    
    * trying to fix tests
    
    * trying to fix tests
    
    * trying to fix tests
    
    * trying to fix tests
    
    * trying to fix tests
    
    * trying to fix tests
    
    * Remove test
    
    * Remove test
    
    * introduce test
    
    * remove form from test
    
    * remove form from test
    
    * Added only to single test
    
    * Added only to single test
    
    * Added only to single test
    
    * Added only to single test
    
    * Added only to single test
    
    * Added only to single test
    
    * Added only to single test
    
    * Added only to single test
    
    * Added only to single test
    
    * Added only to single test
    
    * Added only to single test
    
    * Added only to single test
    
    * Added only to single test
    
    * Added only to single test
    
    * Remove single test
    
    * Remove form submit
    
    * Update form submit
    
    * Remove form submit
    
    * Remove form submit
    
    * Remove form submit
    
    * Finally fix ie11 tests
    
    * Fixed name issue on button
    
    * Updated docs
    
    Co-authored-by: rmccar <42928680+rmccar@users.noreply.github.com>

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,46 +1,44 @@
 let i = 0;
+export const classPatternLibForm = 'js-patternlib-form';
 export default class SubmitButton {
   constructor(button, submitType) {
-    this.button = button;
-    this.form = [...document.getElementsByTagName('form')][0];
+    this.submitButton = button;
+    this.formEl = [...document.getElementsByTagName('form')][0];
+    this.patternLibForm = this.formEl.classList.contains(classPatternLibForm) ? true : false;
     this.submitType = submitType;
 
     if (this.submitType == 'loader') {
-      if (this.form.length) {
-        this.form.addEventListener('submit', this.loaderButton.bind(this));
-      } else {
-        this.button.addEventListener('click', this.loaderButton.bind(this));
-      }
+      this.submitButton.addEventListener('click', this.loaderButton.bind(this));
     } else if (this.submitType == 'timer') {
-      if (this.form.length) {
-        this.form.addEventListener('submit', this.timerButton.bind(this));
-      } else {
-        this.button.addEventListener('click', this.timerButton.bind(this));
-      }
+      this.submitButton.addEventListener('click', this.timerButton.bind(this));
     }
   }
 
   loaderButton() {
-    this.button.classList.add('is-loading');
-    this.button.setAttribute('disabled', true);
+    this.submitForm();
+    this.submitButton.classList.add('is-loading');
+    this.submitButton.setAttribute('disabled', true);
   }
 
   timerButton(event) {
-    if (this.button.tagName === 'A') {
+    this.submitForm();
+    if (this.submitButton.tagName === 'A') {
       i++;
       if (i > 1) {
         event.preventDefault();
       }
     } else {
-      this.button.setAttribute('disabled', true);
+      this.submitButton.setAttribute('disabled', true);
     }
-    setTimeout(
-      button => {
-        button.removeAttribute('disabled');
-        i = 0;
-      },
-      1000,
-      this.button,
-    );
+    setTimeout(() => {
+      this.submitButton.removeAttribute('disabled');
+      i = 0;
+    }, 1000);
+  }
+
+  submitForm() {
+    if (this.formEl && this.formEl.length && !this.patternLibForm) {
+      this.formEl.submit();
+    }
   }
 }
