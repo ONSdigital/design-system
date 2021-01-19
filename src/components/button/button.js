9fd60d6e531e36bad49bcf1171a16291938428c8
boxadesign <info@boxadesign.co.uk>
2021-01-19 13:05:33 +0000

    Remove form submit

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -29,7 +29,6 @@ export default class SubmitButton {
     } else {
       this.submitButton.setAttribute('disabled', true);
     }
-    this.submitForm();
     setTimeout(() => {
       this.submitButton.removeAttribute('disabled');
       i = 0;
@@ -39,8 +38,6 @@ export default class SubmitButton {
   submitForm() {
     if (this.formEl && this.formEl.length && !this.patternLibForm) {
       this.formEl.submit();
-    } else {
-      return;
     }
   }
 }
