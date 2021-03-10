7e6d172b0ad9cc0d0e590726739880633d6f2651
boxadesign <info@boxadesign.co.uk>
2021-03-10 21:50:00 +0000

    Update tests

--- src/tests/spec/button/link-button.spec.js
+++ src/tests/spec/button/link-button.spec.js
@@ -1,10 +1,9 @@
 import { awaitPolyfills } from 'js/polyfills/await-polyfills';
-import eventMock from 'stubs/event.stub.spec';
 
 import template from 'components/button/_test-template.njk';
 import SubmitButton from 'components/button/button';
 
-describe('Function: Timer Button ', function() {
+describe('Function: Link Button ', function() {
   let wrapper, buttonElement;
 
   let params = {
@@ -39,13 +38,17 @@ describe('Function: Timer Button ', function() {
   });
 
   describe('When the button is clicked with the spacebar', () => {
-    beforeEach(() => {
-      new SubmitButton(buttonElement, params.submitType);
-      this.mockedEvent = eventMock({ keyCode: 32 });
+    beforeEach(done => {
+      this.submitButton = new SubmitButton(buttonElement, params.submitType);
+      this.linkButtonSpy = chai.spy.on(this.submitButton, 'linkButton');
+
+      buttonElement.focus();
+      const event = new KeyboardEvent('keydown', { keyCode: 32 });
+      document.dispatchEvent(event);
     });
 
-    it('then preventDefault should have been called on the event', function() {
-      expect(this.mockedEvent.preventDefault).to.have.been.called();
+    it('then the linkButton function should be called', function() {
+      expect(this.linkButtonSpy).to.have.been.called();
     });
   });
 });
