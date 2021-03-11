78fadcdc001a2d218be624fb1ee59affacae767e
boxadesign <info@boxadesign.co.uk>
2021-03-11 11:10:07 +0000

    Fixing tests

--- src/tests/spec/button/link-button.spec.js
+++ src/tests/spec/button/link-button.spec.js
@@ -1,7 +1,16 @@
 import { awaitPolyfills } from 'js/polyfills/await-polyfills';
+import eventMock from 'stubs/event.stub.spec';
 
 import template from 'components/button/_test-template.njk';
 import SubmitButton from 'components/button/button';
+
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import chaiSpies from 'chai-spies';
+
+chai.should();
+chai.use(chaiSpies);
+chai.use(chaiAsPromised);
 
 describe('Function: Link Button ', function() {
   let wrapper, buttonElement;
@@ -38,15 +47,17 @@ describe('Function: Link Button ', function() {
   });
 
   describe('When the button is clicked with the spacebar', () => {
-    beforeEach(done => {
+    beforeEach(function(done) {
       this.submitButton = new SubmitButton(buttonElement, params.submitType);
-      this.linkButtonSpy = chai.spy.on(this.submitButton, 'linkButton');
-
+      this.mockedEvent = eventMock({ keyCode: 32 });
+      this.submitButton.linkButton(this.mockedEvent);
       setTimeout(done);
+
+      done();
     });
 
-    it('then the linkButton function should be called', function() {
-      expect(this.linkButtonSpy).to.have.been.called();
+    it('then preventDefault should be called on the event', function() {
+      expect(this.mockedEvent.preventDefault).to.have.been.called();
     });
   });
 });
