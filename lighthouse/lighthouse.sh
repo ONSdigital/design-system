6bf49c5f92da4927d90ef057f14b1ac6c3e9ce93
boxadesign <info@boxadesign.co.uk>
2021-05-20 13:24:59 +0100

    Add config path to each lhci command

--- lighthouse/lighthouse.sh
+++ lighthouse/lighthouse.sh
@@ -6,8 +6,8 @@ lhci healthcheck --fatal
 for url in $(jq '.urls[]' ./lighthouse/urls.json); do
     lhci collect "--url=$url" --additive --config=./lighthouse/lighthouserc.js
 done
-lhci assert
-lhci upload --target temporary-public-storage
+lhci assert --config=./lighthouse/lighthouserc.js
+lhci upload --config=./lighthouse/lighthouserc.js
 
 EXIT_CODE=$?
 
