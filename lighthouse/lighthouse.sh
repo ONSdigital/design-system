01d5b188bc5b3203c80e2a1c5f8dfd8501d2d361
boxadesign <info@boxadesign.co.uk>
2021-05-21 11:07:16 +0100

    Attempt to run in parallel 2

--- lighthouse/lighthouse.sh
+++ lighthouse/lighthouse.sh
@@ -4,9 +4,10 @@ npx http-server -p 9000 ./build &
 npm install -g @lhci/cli@0.7.x
 lhci healthcheck --fatal
 
-for url in $(jq '.urls[]' ./lighthouse/urls.json); do
+for url in $(jq '.urls[]' ./lighthouse/urls.json); do sem
     lhci collect "--url=$url" --additive --config=./lighthouse/lighthouserc.js &
 done
+sem --wait
 lhci assert --config=./lighthouse/lighthouserc.js
 EXIT_CODE=$?
 lhci upload --config=./lighthouse/lighthouserc.js
