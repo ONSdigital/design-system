90eabf1e026ae25d44a301cb3d786cfe4bc8615a
rmccar <42928680+rmccar@users.noreply.github.com>
2021-07-12 09:11:04 +0100

    Disable send button on messages (#1582)

---
+++ src/components/reply/reply.dom.js
@@ -0,0 +1,13 @@
+import domready from 'js/domready';
+
+async function initialise() {
+  const replyInputs = [...document.querySelectorAll('.js-reply')];
+
+  if (replyInputs.length) {
+    const ReplyInput = (await import('./reply-input')).default;
+
+    replyInputs.forEach(input => new ReplyInput(input));
+  }
+}
+
+domready(initialise);
