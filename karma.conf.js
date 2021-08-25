05ae759f8802bb87f26caa1437b343b17a7ca352
boxadesign <info@boxadesign.co.uk>
2021-08-25 15:25:49 +0100

    Add localIdentifier to BS config

--- karma.conf.js
+++ karma.conf.js
@@ -97,6 +97,7 @@ export default function(config) {
       accessKey: process.env.BROWSER_STACK_ACCESS_KEY,
       timeout: 1800,
       console: true,
+      localIdentifier: 'ONS',
     },
 
     port: 9876,
