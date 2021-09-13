0d4575ecb77782f2e332ee39941ae3a3af1f3e2c
Lea Hayes <lea.hayes@ext.ons.gov.uk>
2021-09-13 12:24:57 +0100

    Disable BrowserStack tunnel in testem config.

--- stop-BrowserStackLocal.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-
-process.kill(process.argv[2], 'SIGINT');
--- testem.esm.js
+++ testem.esm.js
@@ -12,36 +12,30 @@ module.exports = {
   launch_in_ci: ['bs_mac_safari', 'bs_windows_10_IE_edge', 'bs_windows_10_chrome'],
 
   parallel: 5,
-  on_start: {
-    command: "kill -9 $(ps -A | grep BrowserStackLocal | grep -v grep | cut -d ' ' -f2); node start-BrowserStackLocal.js &",
-    wait_for_text: 'Tunnel started',
-    wait_for_text_timeout: 300000,
-  },
-  on_exit: 'node stop-BrowserStackLocal.js `cat browserStackLocal.pid`; rm browserStackLocal.pid',
 
   launchers: {
     bs_iphone_11_pro: {
-      command: "node run_on_browserstack.js ios 13 nil nil 'iPhone 11 Pro' <url>",
+      command: "node run_on_browserstack.js ios 13 nil nil 'iPhone 11 Pro' http://localhost:7357/",
       protocol: 'browser',
     },
 
     bs_google_pixel_4: {
-      command: "node run_on_browserstack.js android 11.0 nil nil 'Google Pixel 4' <url>",
+      command: "node run_on_browserstack.js android 11.0 nil nil 'Google Pixel 4' http://localhost:7357/",
       protocol: 'browser',
     },
 
     bs_mac_safari: {
-      command: "node run_on_browserstack.js 'OS X' 'Catalina' safari 13.0 nil <url>",
+      command: "node run_on_browserstack.js 'OS X' 'Catalina' safari 13.0 nil http://localhost:7357/",
       protocol: 'browser',
     },
 
     bs_windows_10_IE_edge: {
-      command: 'node run_on_browserstack.js Windows 10 Edge latest nil <url>',
+      command: 'node run_on_browserstack.js Windows 10 Edge latest nil http://localhost:7357/',
       protocol: 'browser',
     },
 
     bs_windows_10_chrome: {
-      command: 'node run_on_browserstack.js Windows 10 Chrome latest nil <url>',
+      command: 'node run_on_browserstack.js Windows 10 Chrome latest nil http://localhost:7357/',
       protocol: 'browser',
     },
   },
