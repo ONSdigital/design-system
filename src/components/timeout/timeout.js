a6fc77252f5d84acdc0e78a0bafc10412c79da6a
boxadesign <info@boxadesign.co.uk>
2021-10-27 18:43:37 +0100

    Updates to timeout module

--- src/components/timeout/timeout.js
+++ src/components/timeout/timeout.js
@@ -4,20 +4,35 @@ export default class Timeout {
   constructor(context) {
     this.context = context;
     this.continueButton = context.querySelector('.ons-js-modal-btn');
-    this.timers = [];
     this.countdown = context.querySelector('.ons-js-timeout-timer');
     this.accessibleCountdown = context.querySelector('.ons-js-timeout-timer-acc');
-
-    this.idleMinutesBeforeTimeOut = context.getAttribute('data-timeout-time') ? context.getAttribute('data-timeout-time') : 1;
-    this.secondsTimeOutDialogVisible = context.getAttribute('data-show-modal-time') ? context.getAttribute('data-show-modal-time') : 20;
     this.timeOutRedirectUrl = context.getAttribute('data-redirect-url');
     this.text = context.getAttribute('data-text');
+    this.sessionExpiryEnpoint = context.getAttribute('data-session-expiry-enpoint') || '/session/expiry';
+
+    // Timing convertions
+    this.secondsTimeOutDialogVisible = context.getAttribute('data-show-modal-time');
+    this.milliSecondsTimeOutDialogVisible = this.secondsTimeOutDialogVisible * 1000;
+    this.minutesTimeOutDialogVisible = this.secondsTimeOutDialogVisible / 60;
+
+    this.idleMilliSecondsBeforeTimeOut = this.convertTimeToMilliSeconds(
+      context.getAttribute('data-timeout-time') || new Date().toISOString(),
+    ); // Temporary timestamp added by default (to do - remove ||)
+    this.idleSecondsBeforeTimeOut = this.idleMilliSecondsBeforeTimeOut / 1000;
+
+    // Language dependent text strings
+    this.minutesTextSingular = 'minute';
+    this.minutesTextPlural = 'minutes';
+    this.secondsTextSingular = 'second';
+    this.secondsTextPlural = 'seconds';
+
+    // States
     this.timeUserLastInteractedWithPage = '';
     this.idleTime = null;
-    this.secondsOfIdleTime = this.idleMinutesBeforeTimeOut * 60;
-    this.milliSecondsBeforeTimeOut = this.idleMinutesBeforeTimeOut * 60000;
-    this.milliSecondsTimeOutDialogVisible = this.secondsTimeOutDialogVisible * 1000;
+    this.timers = [];
+    this.timerRunOnce = false;
 
+    // Start module
     this.modal = new Modal(context);
     this.initialise();
   }
@@ -41,15 +56,16 @@ export default class Timeout {
 
   resetIdleTime() {
     clearTimeout(this.idleTime);
-    this.idleTime = setTimeout(this.openModal.bind(this), this.milliSecondsBeforeTimeOut - this.milliSecondsTimeOutDialogVisible);
+    this.idleTime = setTimeout(this.openModal.bind(this), this.idleMilliSecondsBeforeTimeOut - this.milliSecondsTimeOutDialogVisible);
 
+    // Run every x seconds
     if (!this.modal.isDialogOpen()) {
-      this.setLastInteractiveTime();
+      this.resetLastInteractiveTime();
     }
   }
 
   openModal() {
-    const shouldDialogOpen = this.getLastInteractiveTime(true) >= this.idleMinutesBeforeTimeOut * 60 - this.secondsTimeOutDialogVisible;
+    const shouldDialogOpen = this.getLastInteractiveTime() >= this.idleSecondsBeforeTimeOut - this.secondsTimeOutDialogVisible;
     if (shouldDialogOpen && !this.modal.isDialogOpen()) {
       this.modal.openDialog();
       this.startUiCountdown();
@@ -64,73 +80,54 @@ export default class Timeout {
 
   startUiCountdown() {
     this.clearTimers();
-    const module = this;
-    const countdown = this.countdown;
-    const accessibleCountdown = this.accessibleCountdown;
-    const minutes = this.secondsTimeOutDialogVisible / 60;
+    this.countdown.innerHTML =
+      this.minutesTimeOutDialogVisible >= 1
+        ? this.minutesTimeOutDialogVisible + ' minute' + (this.minutesTimeOutDialogVisible > 1 ? 's ' : ' ')
+        : this.secondsTimeOutDialogVisible + ' second' + (this.secondsTimeOutDialogVisible > 1 ? 's' : '');
 
-    let seconds = this.secondsTimeOutDialogVisible;
-    let timerRunOnce = false;
-    let timers = this.timers;
-    countdown.innerHTML = minutes >= 1 ? minutes + ' minute' + (minutes > 1 ? 's ' : ' ') : seconds + ' second' + (seconds > 1 ? 's' : '');
+    this.runTimer();
+  }
 
-    (function runTimer() {
-      const minutesLeft = parseInt(seconds / 60, 10);
-      const secondsLeft = parseInt(seconds % 60, 10);
-      const timerExpired = minutesLeft < 1 && secondsLeft < 1;
+  runTimer() {
+    const minutesLeft = parseInt(this.secondsTimeOutDialogVisible / 60, 10);
+    const secondsLeft = parseInt(this.secondsTimeOutDialogVisible % 60, 10);
+    const timerExpired = minutesLeft < 1 && secondsLeft < 1;
 
-      const minutesText = minutesLeft > 0 ? minutesLeft + ' minute' + (minutesLeft > 1 ? 's ' : ' ') : '';
-      const secondsText = secondsLeft >= 1 ? secondsLeft + ' second' + (secondsLeft > 1 ? 's' : '') : '';
-      let atMinutesNumberAsText = '';
-      let atSecondsNumberAsText = '';
+    const minutesText = minutesLeft + ' ' + (minutesLeft >= 2 ? this.minutesTextPlural : minutesLeft === 1 ? this.minutesTextSingular : '');
+    const secondsText = secondsLeft + ' ' + (secondsLeft >= 2 ? this.secondsTextPlural : secondsLeft === 1 ? this.secondsTextSingular : '');
 
-      try {
-        atMinutesNumberAsText = this.numberToWords(minutesLeft);
-        atSecondsNumberAsText = this.numberToWords(secondsLeft);
-      } catch (e) {
-        atMinutesNumberAsText = minutesLeft;
-        atSecondsNumberAsText = secondsLeft;
+    let timeLeftText =
+      this.text +
+      ' <span class="ons-u-fw-b">' +
+      (minutesLeft > 0 ? minutesText : '') +
+      (minutesLeft > 0 && secondsLeft > 0 ? ' ' : '') +
+      (secondsLeft > 0 ? secondsText : '') +
+      '</span>.';
+
+    if (timerExpired && this.getLastInteractiveTime() > this.idleSecondsBeforeTimeOut) {
+      this.redirect();
+    } else {
+      this.secondsTimeOutDialogVisible--;
+      this.countdown.innerHTML = timeLeftText;
+
+      if (minutesLeft < 1 && secondsLeft < this.secondsTimeOutDialogVisible) {
+        this.accessibleCountdown.setAttribute('aria-live', 'assertive');
       }
 
-      const atMinutesText = minutesLeft > 0 ? atMinutesNumberAsText + ' minute' + (minutesLeft > 1 ? 's' : '') + '' : '';
-      const atSecondsText = secondsLeft >= 1 ? ' ' + atSecondsNumberAsText + ' second' + (secondsLeft > 1 ? 's' : '') + '' : '';
-
-      let text = module.text + ' <span class="u-fw-b">' + minutesText + secondsText + '</span>.';
-      let atText = module.text + ' ' + atMinutesText;
-      if (atSecondsText) {
-        if (minutesLeft > 0) {
-          atText += ' and';
-        }
-        atText += atSecondsText + '.';
-      } else {
-        atText += '.';
+      if (!this.timerRunOnce) {
+        this.accessibleCountdown.innerHTML = timeLeftText;
+        this.timerRunOnce = true;
+      } else if (secondsLeft % 15 === 0) {
+        this.accessibleCountdown.innerHTML = timeLeftText;
       }
-
-      if (timerExpired && module.getLastInteractiveTime(module.convertToSeconds) > module.secondsOfIdleTime) {
-        module.redirect();
-      } else {
-        seconds--;
-        countdown.innerHTML = text;
-
-        if (minutesLeft < 1 && secondsLeft < 20) {
-          accessibleCountdown.setAttribute('aria-live', 'assertive');
-        }
-
-        if (!timerRunOnce) {
-          accessibleCountdown.innerHTML = atText;
-          timerRunOnce = true;
-        } else if (secondsLeft % 15 === 0) {
-          accessibleCountdown.innerHTML = atText;
-        }
-
-        timers.push(setTimeout(runTimer, 1000));
-      }
-    })();
+      this.timers.push(setTimeout(this.runTimer.bind(this), 1000));
+    }
   }
 
   stopTimeout() {
+    // Modal will close via Modal component
     this.clearTimers();
-    this.setLastInteractiveTime();
+    this.resetLastInteractiveTime();
   }
 
   clearTimers() {
@@ -140,8 +137,8 @@ export default class Timeout {
   }
 
   shouldModuleCloseOrRedirect() {
-    const shouldDialogClose = this.getLastInteractiveTime(true) < this.idleMinutesBeforeTimeOut * 60 - this.secondsTimeOutDialogVisible;
-    const shouldRedirect = this.getLastInteractiveTime(true) > this.secondsOfIdleTime;
+    const shouldDialogClose = this.getLastInteractiveTime() < this.idleSecondsBeforeTimeOut - this.secondsTimeOutDialogVisible;
+    const shouldRedirect = this.getLastInteractiveTime() > this.idleSecondsBeforeTimeOut;
 
     if (shouldRedirect) {
       this.redirect();
@@ -151,148 +148,30 @@ export default class Timeout {
     }
   }
 
-  getLastInteractiveTime(convertToSeconds) {
-    let timeUserLastInteractedWithPage = new Date(window.localStorage.getItem('timeUserLastInteractedWithPage'));
-    let time = timeUserLastInteractedWithPage;
-    if (convertToSeconds) {
-      time = Math.abs((timeUserLastInteractedWithPage - new Date()) / 1000);
-    }
-    return time;
+  getLastInteractiveTime() {
+    return this.idleSecondsBeforeTimeOut;
   }
 
-  setLastInteractiveTime() {
-    window.localStorage.setItem('timeUserLastInteractedWithPage', new Date());
+  resetLastInteractiveTime() {
+    // To do - Convert below with get request to 'session/expiry'. Response will be a json object containing a new time stamp
+    // Update variable 'timeUserLastInteractedWithPage'
+    this.idleMilliSecondsBeforeTimeOut = this.convertTimeToMilliSeconds(new Date().toISOString());
+    this.idleSecondsBeforeTimeOut = this.idleMilliSecondsBeforeTimeOut / 1000;
+  }
+
+  convertTimeToMilliSeconds(time) {
+    let timeoutTime = new Date(time);
+
+    // Temporary - add one minute 30 to fake time (to do - remove lines 52 + 53)
+    const timeoutTimePlusOneMinute30 = timeoutTime.setTime(timeoutTime.getTime() + 1500 * 60);
+    timeoutTime = new Date(timeoutTimePlusOneMinute30);
+
+    const timeInMilliSeconds = Math.abs(timeoutTime - new Date());
+
+    return timeInMilliSeconds;
   }
 
   redirect() {
     window.location.replace(this.timeOutRedirectUrl);
-  }
-
-  numberToWords() {
-    let string = n.toString();
-    let units;
-    let tens;
-    let scales;
-    let start;
-    let end;
-    let chunks;
-    let chunksLen;
-    let chunk;
-    let ints;
-    let i;
-    let word;
-    let words = 'and';
-
-    if (parseInt(string) === 0) {
-      return 'zero';
-    }
-
-    /* Array of units as words */
-    units = [
-      '',
-      'one',
-      'two',
-      'three',
-      'four',
-      'five',
-      'six',
-      'seven',
-      'eight',
-      'nine',
-      'ten',
-      'eleven',
-      'twelve',
-      'thirteen',
-      'fourteen',
-      'fifteen',
-      'sixteen',
-      'seventeen',
-      'eighteen',
-      'nineteen',
-    ];
-
-    /* Array of tens as words */
-    tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-
-    /* Array of scales as words */
-    scales = [
-      '',
-      'thousand',
-      'million',
-      'billion',
-      'trillion',
-      'quadrillion',
-      'quintillion',
-      'sextillion',
-      'septillion',
-      'octillion',
-      'nonillion',
-      'decillion',
-      'undecillion',
-      'duodecillion',
-      'tredecillion',
-      'quatttuor-decillion',
-      'quindecillion',
-      'sexdecillion',
-      'septen-decillion',
-      'octodecillion',
-      'novemdecillion',
-      'vigintillion',
-      'centillion',
-    ];
-
-    /* Split user arguemnt into 3 digit chunks from right to left */
-    start = string.length;
-    chunks = [];
-    while (start > 0) {
-      end = start;
-      chunks.push(string.slice((start = Math.max(0, start - 3)), end));
-    }
-
-    /* Check if function has enough scale words to be able to stringify the user argument */
-    chunksLen = chunks.length;
-    if (chunksLen > scales.length) {
-      return '';
-    }
-
-    /* Stringify each integer in each chunk */
-    words = [];
-    for (i = 0; i < chunksLen; i++) {
-      chunk = parseInt(chunks[i]);
-
-      if (chunk) {
-        /* Split chunk into array of individual integers */
-        ints = chunks[i]
-          .split('')
-          .reverse()
-          .map(parseFloat);
-
-        /* If tens integer is 1, i.e. 10, then add 10 to units integer */
-        if (ints[1] === 1) {
-          ints[0] += 10;
-        }
-
-        /* Add scale word if chunk is not zero and array item exists */
-        if ((word = scales[i])) {
-          words.push(word);
-        }
-
-        /* Add unit word if array item exists */
-        if ((word = units[ints[0]])) {
-          words.push(word);
-        }
-
-        /* Add tens word if array item exists */
-        if ((word = tens[ints[1]])) {
-          words.push(word);
-        }
-
-        /* Add hundreds word if array item exists */
-        if ((word = units[ints[2]])) {
-          words.push(word + ' hundred');
-        }
-      }
-    }
-    return words.reverse().join(' ');
   }
 }
