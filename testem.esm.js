0e9b954db19d075d17b22d1f649223ce90292afc
Matthew Simon <info@boxadesign.co.uk>
2021-11-25 16:53:00 +0000

    Change launcher config

--- testem.esm.js
+++ testem.esm.js
@@ -1,6 +1,6 @@
 module.exports = {
   framework: 'mocha+chai',
-  parallel: 4,
+  parallel: 5,
   port: 1337,
   timeout: 1200,
   browser_start_timeout: 2000,
@@ -13,7 +13,7 @@ module.exports = {
   test_page: 'src/tests/tests.mustache',
 
   launch_in_dev: ['chrome'],
-  launch_in_ci: ['bs_mac_safari', 'bs_windows_10_IE_edge', 'bs_windows_10_chrome'],
+  launch_in_ci: ['bs_chrome'],
 
   on_start: {
     command: "kill -9 $(ps -A | grep BrowserStackLocal | grep -v grep | cut -d ' ' -f2); node start-BrowserStackLocal.js &",
@@ -24,21 +24,8 @@ module.exports = {
   on_exit: 'node stop-BrowserStackLocal.js',
 
   launchers: {
-    bs_mac_safari: {
-      exe: './ci/tests/browserstack.js',
-      args: ['OSX', 'Catalina', 'safari', '13.0', ''],
-      protocol: 'browser',
-    },
-
-    bs_windows_10_IE_edge: {
-      exe: './ci/tests/browserstack.js',
-      args: ['Windows', '10', 'edge', 'latest', ''],
-      protocol: 'browser',
-    },
-
-    bs_windows_10_chrome: {
-      exe: './ci/tests/browserstack.js',
-      args: ['Windows', '10', 'chrome', 'latest', ''],
+    bs_chrome: {
+      command: "node ./ci/tests/browserstack.js 'OS X' 'El Capitan' chrome latest nil <url>",
       protocol: 'browser',
     },
   },
