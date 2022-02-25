836dc3674e873cd40edf9ccb1bcc2266dad931e8
Matthew Simon <info@boxadesign.co.uk>
2022-02-25 13:45:43 +0000

    Update unit tests

--- src/tests/spec/cookies/cookies-banner.spec.js
+++ src/tests/spec/cookies/cookies-banner.spec.js
@@ -1,5 +1,5 @@
 import CookiesBanner from '../../../components/cookies-banner/cookies-banner';
-import { approveAllCookieTypes, cookie, setCookie } from '../../../js/cookies-functions';
+import { approveAllCookieTypes, cookie, setCookie, setDefaultConsentCookie } from '../../../js/cookies-functions';
 
 describe('Component: Cookie banner', function() {
   beforeEach(function() {
@@ -37,11 +37,34 @@ describe('Component: Cookie banner', function() {
     expect(cookieJSON).to.contain({ essential: true, settings: true, usage: true, campaigns: true });
   });
 
+  it('sets all cookies to false when rejecting cookies', function() {
+    new CookiesBanner(this.banner);
+    const rejectAllCookieTypesSpy = chai.spy(setDefaultConsentCookie);
+
+    const button = this.banner.querySelector('.ons-js-reject-cookies');
+    button.click();
+
+    expect(rejectAllCookieTypesSpy).to.have.been.called;
+    const cookieJSON = JSON.parse(cookie('ons_cookie_policy').replace(/'/g, '"'));
+    expect(cookieJSON).to.contain({ essential: true, settings: false, usage: false, campaigns: false });
+  });
+
   it('sets seen cookie message when accepting cookies', function() {
     new CookiesBanner(this.banner);
     const setConsentCookieSpy = chai.spy(CookiesBanner.setCookiesConsent);
 
     const button = this.banner.querySelector('.ons-js-accept-cookies');
+    button.click();
+
+    expect(cookie('ons_cookie_message_displayed')).to.equal('true');
+    expect(setConsentCookieSpy).to.have.been.called;
+  });
+
+  it('sets seen cookie message when rejecting cookies', function() {
+    new CookiesBanner(this.banner);
+    const setConsentCookieSpy = chai.spy(CookiesBanner.setCookiesConsent);
+
+    const button = this.banner.querySelector('.ons-js-reject-cookies');
     button.click();
 
     expect(cookie('ons_cookie_message_displayed')).to.equal('true');
@@ -70,13 +93,25 @@ describe('Component: Cookie banner', function() {
     expect(secondaryBanner.classList.contains('ons-u-d-no')).to.equal(false);
   });
 
-  it('sets all radios to on from off', function() {
+  it('sets all radios to on when accepting cookies', function() {
     new CookiesBanner(this.banner);
 
     const button = this.banner.querySelector('.ons-js-accept-cookies');
     button.click();
 
     const radios = [...this.form.querySelectorAll('input[value=on]')];
+    radios.forEach(radio => {
+      expect(radio.checked).to.be.true;
+    });
+  });
+
+  it('sets all radios to off when rejecting cookies', function() {
+    new CookiesBanner(this.banner);
+
+    const button = this.banner.querySelector('.ons-js-reject-cookies');
+    button.click();
+
+    const radios = [...this.form.querySelectorAll('input[value=off]')];
     radios.forEach(radio => {
       expect(radio.checked).to.be.true;
     });
@@ -102,29 +137,42 @@ describe('Component: Cookie banner', function() {
 
 function renderComponent() {
   const formHTML =
-    '<div class="ons-cookies-banner">' +
+    '<div class="ons-cookies-banner" role="region" aria-label="cookie banner">' +
     '<div class="ons-container ons-cookies-banner__primary">' +
     '<div class="ons-grid">' +
     '<div class="ons-grid__col ons-col-8@m">' +
-    '<h2 class="ons-cookies-banner__title ons-u-mb-xs">Tell us whether you accept cookies</h2>' +
-    '<p class="ons-cookies-banner__desc">We use cookies to collect information about how you use census.gov.uk. We use this information to make the website work as well as possible and improve our services.</p>' +
-    '<button type="button" class="ons-btn ons-btn--small ons-js-accept-cookies ons-u-mb-xs@xxs@s ons-cookies-banner__btn">' +
-    '<span class="ons-btn__inner">Accept all cookies</span>' +
+    '<h2 class="ons-cookies-banner__title ons-u-mb-xs">Cookies on census.gov.uk</h2>' +
+    '<div class="ons-cookies-banner__statement">' +
+    '<p>Cookies are small files that we put on your device. Some cookies are essential to make the site work.</p>' +
+    '<p>You can allow additional cookies that remember your settings and help us improve our services by understanding how you use the site.</p>' +
+    '</div>' +
+    '<div class="ons-grid ons-grid--flex ons-grid--gutterless ons-u-mt-s">' +
+    '<div class="ons-grid__col ons-u-mr-s">' +
+    '<button type="button" class="ons-btn ons-btn--small ons-js-accept-cookies ons-cookies-banner__btn" data-button="accept">' +
+    '<span class="ons-btn__inner">Accept additional cookies</span>' +
     '</button>' +
-    '<a href="#0" role="button" class="ons-btn ons-btn--secondary ons-btn--small ons-u-ml-no@xxs@s ons-cookies-banner__btn ons-btn--link">' +
-    '<span class="ons-btn__inner">Cookie settings</span>' +
-    '</a>' +
+    '</div>' +
+    '<div class="ons-grid__col ons-u-mr-s">' +
+    '<button type="button" class="ons-btn ons-btn--small ons-js-reject-cookies ons-cookies-banner__btn" data-button="reject">' +
+    '<span class="ons-btn__inner">Reject additional cookies</span>' +
+    '</button>' +
+    '</div>' +
+    '<div class="ons-grid__col">' +
+    '<a class="ons-cookies-banner__link" href="">Cookie settings</a>' +
+    '</div>' +
+    '</div>' +
     '</div>' +
     '</div>' +
     '</div>' +
     '<div class="ons-container ons-cookies-banner__confirmation ons-u-d-no">' +
     '<div class="ons-grid ons-grid--flex ons-grid--between ons-grid--gutterless ons-grid--no-wrap@s ons-grid--vertical-center">' +
     '<div class="ons-grid__col ons-grid__col--flex ons-col-auto ons-u-flex-shrink@s">' +
-    '<p class="ons-cookies-banner__desc ons-u-mb-no@s ons-u-mr-s@s">You’ve accepted all cookies. You can <a href="#0">change your cookie settings</a> at any time.</p>' +
+    '<p class="ons-cookies-banner__desc ons-u-mb-no@s ons-u-mr-s@s"><span class="ons-js-accepted-text ons-u-d-no">You’ve accepted all additional cookies. </span><span class="ons-js-rejected-text ons-u-d-no">You’ve rejected all additional cookies. </span>You can <a href="#0">change your cookie preferences</a> at any time.</p>' +
     '</div>' +
     '<div class="ons-grid__col">' +
     '<button type="button" class="ons-btn ons-btn--secondary ons-btn--small ons-js-hide-button">' +
     '<span class="ons-btn__inner">Hide</span>' +
+    '<span class="ons-btn__context ons-u-vh">the cookie message</span>' +
     '</button>' +
     '</div>' +
     '</div>' +
