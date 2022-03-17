3b8da3cb05ff1fd84694ff5fbab536c3f1b47877
Lea Hayes <lea.hayes@ext.ons.gov.uk>
2022-03-17 00:51:59 +0000

    Add some tests for 'access-code' component.

---
+++ src/components/access-code/_macro.spec.js
@@ -0,0 +1,143 @@
+import * as cheerio from 'cheerio';
+
+import { renderComponent } from '../../tests/helpers/rendering';
+
+describe('macro: access-code', () => {
+  it('has the provided `id` attribute', () => {
+    const $ = cheerio.load(
+      renderComponent('access-code', {
+        id: 'example-id',
+      }),
+    );
+
+    expect($('#example-id').length).toBe(1);
+  });
+
+  it('has the provided `name` attribute', () => {
+    const $ = cheerio.load(
+      renderComponent('access-code', {
+        name: 'special-name',
+      }),
+    );
+
+    expect($('input').attr('name')).toBe('special-name');
+  });
+
+  it('has a default `type` of "text"', () => {
+    const $ = cheerio.load(renderComponent('access-code'));
+
+    expect($('input').attr('type')).toBe('text');
+  });
+
+  it('has the provided `type` attribute', () => {
+    const $ = cheerio.load(
+      renderComponent('access-code', {
+        type: 'number',
+      }),
+    );
+
+    expect($('input').attr('inputmode')).toBe('numeric');
+  });
+
+  it('has additionally provided style classes', () => {
+    const $ = cheerio.load(
+      renderComponent('access-code', {
+        classes: 'extra-class another-extra-class',
+      }),
+    );
+
+    expect($('.extra-class').length).toBe(1);
+    expect($('.another-extra-class').length).toBe(1);
+  });
+
+  it('has provided label text and description', () => {
+    const $ = cheerio.load(
+      renderComponent('access-code', {
+        label: {
+          text: 'Enter your 16-character access code',
+          description: 'Keep this code safe. You will need to enter it every time you access your study',
+        },
+      }),
+    );
+
+    expect($('.ons-label--with-description').text()).toBe('Enter your 16-character access code');
+    expect($('.ons-input--with-description').text()).toBe(
+      'Keep this code safe. You will need to enter it every time you access your study',
+    );
+  });
+
+  it('has provided maximum length attribute', () => {
+    const $ = cheerio.load(
+      renderComponent('access-code', {
+        maxlength: 3,
+      }),
+    );
+
+    expect($('input').attr('maxlength')).toBe('10');
+  });
+
+  it('has provided group size attribute', () => {
+    const $ = cheerio.load(
+      renderComponent('access-code', {
+        groupSize: 2,
+      }),
+    );
+
+    expect($('input').attr('data-group-size')).toBe('2');
+  });
+
+  it('has autocomplete disabled on its text input', () => {
+    const $ = cheerio.load(renderComponent('access-code'));
+
+    expect($('input').attr('autocomplete')).toBe('off');
+  });
+
+  it('has automatic capitalization on its text input', () => {
+    const $ = cheerio.load(renderComponent('access-code'));
+
+    expect($('input').attr('autocapitalize')).toBe('characters');
+  });
+
+  it('has provided security message text', () => {
+    const $ = cheerio.load(
+      renderComponent('access-code', {
+        securityMessage: 'Example security message.',
+      }),
+    );
+
+    expect(
+      $('.ons-panel__body')
+        .text()
+        .trim(),
+    ).toBe('Example security message.');
+  });
+
+  it('has provided `postTextBoxLinkText` and `postTextBoxLinkUrl`', () => {
+    const $ = cheerio.load(
+      renderComponent('access-code', {
+        postTextboxLinkText: 'Example link text',
+        postTextboxLinkUrl: '#3',
+      }),
+    );
+
+    expect(
+      $('a[href="#3"]')
+        .text()
+        .trim(),
+    ).toBe('Example link text');
+  });
+
+  it('has provided `error` output', () => {
+    const $ = cheerio.load(
+      renderComponent('access-code', {
+        error: {
+          id: 'uac-error',
+          text: 'Enter an access code',
+        },
+      }),
+    );
+
+    expect($('#uac-error').length).toBe(1);
+    expect($('.ons-panel__error > strong').text()).toBe('Enter an access code');
+  });
+});
