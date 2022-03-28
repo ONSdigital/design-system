7a959148202dd52166683dae0e270987abe20f35
Lea Hayes <lea.hayes@ext.ons.gov.uk>
2022-03-28 10:26:04 +0100

    Disable GPU in headless Chrome when running puppeteer tests to workaround 'Failed to send GpuControl.CreateCommandBuffer'.

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   browserContext: 'incognito',
   launch: {
-    // we use --no-sandbox --disable-setuid-sandbox as a workaround for the
-    // 'No usable sandbox! Update your kernel' error
-    // see more https://github.com/Googlechrome/puppeteer/issues/290
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    args: [
+      // Workaround for the 'No usable sandbox! Update your kernel' error
+      // see more https://github.com/Googlechrome/puppeteer/issues/290
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      // Workaround for 'ContextResult::kTransientFailure: Failed to send GpuControl.CreateCommandBuffer'
+      '--disable-gpu',
+    ],
     dumpio: true,
   },
   server: {
