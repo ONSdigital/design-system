cd9d5afcfdf066eb1d425de4edcaee80941631dc
Lea Hayes <lea.hayes@ext.ons.gov.uk>
2022-04-01 18:01:46 +0100

    Since all `tabs` tests use the same input parameters, refactor to render the same test page before each test.

--- src/components/tabs/tabs.spec.js
+++ src/components/tabs/tabs.spec.js
@@ -30,58 +30,46 @@ describe('script: tabs', () => {
   describe('when the viewport is large', () => {
     beforeEach(async () => {
       await page.setViewport({ width: 1650, height: 1050 });
+
+      await setTestPage('/test', renderComponent('tabs', EXAMPLE_TABS));
     });
 
     it('has the "presentation" role assigned to tab list items', async () => {
-      await setTestPage('/test', renderComponent('tabs', EXAMPLE_TABS));
-
       const role = await page.$eval('.ons-tab__list-item', node => node.getAttribute('role'));
       expect(role).toBe('presentation');
     });
 
     it('has the "tab" role assigned to each tab', async () => {
-      await setTestPage('/test', renderComponent('tabs', EXAMPLE_TABS));
-
       const tabRoleValues = await page.$$eval('.ons-tab', nodes => nodes.map(node => node.getAttribute('role')));
 
       expect(tabRoleValues).toEqual(['tab', 'tab', 'tab']);
     });
 
     it('has "aria-controls" assigned to each tab with the corresponding panel id', async () => {
-      await setTestPage('/test', renderComponent('tabs', EXAMPLE_TABS));
-
       const ariaControlsValues = await page.$$eval('.ons-tab', nodes => nodes.map(node => node.getAttribute('aria-controls')));
 
       expect(ariaControlsValues).toEqual(['tabId1', 'tabId2', 'tabId3']);
     });
 
     it('has "aria-selected" assigned to the first tab', async () => {
-      await setTestPage('/test', renderComponent('tabs', EXAMPLE_TABS));
-
       const ariaSelectedValue = await page.$eval('.ons-tab', node => node.getAttribute('aria-selected'));
 
       expect(ariaSelectedValue).toBe('true');
     });
 
     it('has the "ons-tab--selected" class assigned to the first tab', async () => {
-      await setTestPage('/test', renderComponent('tabs', EXAMPLE_TABS));
-
       const hasClass = await page.$eval('.ons-tab', node => node.classList.contains('ons-tab--selected'));
 
       expect(hasClass).toBe(true);
     });
 
     it('has "tabindex" assigned to each tab', async () => {
-      await setTestPage('/test', renderComponent('tabs', EXAMPLE_TABS));
-
       const tabIndexValues = await page.$$eval('.ons-tab', nodes => nodes.map(node => node.getAttribute('tabindex')));
 
       expect(tabIndexValues).toEqual(['0', '-1', '-1']);
     });
 
     it('has only one visible tab panel', async () => {
-      await setTestPage('/test', renderComponent('tabs', EXAMPLE_TABS));
-
       const panelHiddenStates = await page.$$eval('.ons-tabs__panel', nodes =>
         nodes.map(node => node.classList.contains('ons-tabs__panel--hidden')),
       );
@@ -90,45 +78,30 @@ describe('script: tabs', () => {
     });
 
     describe('when a tab is clicked', () => {
-      it('is assigned a "tabindex" value', async () => {
-        await setTestPage('/test', renderComponent('tabs', EXAMPLE_TABS));
-
+      beforeEach(async () => {
         await page.focus('#tabId2Item a');
         await page.keyboard.press('Enter');
+      });
 
+      it('is assigned a "tabindex" value', async () => {
         const tabIndexValues = await page.$$eval('.ons-tab', nodes => nodes.map(node => node.getAttribute('tabindex')));
 
         expect(tabIndexValues).toEqual(['-1', '0', '-1']);
       });
 
       it('has the "aria-selected" attribute', async () => {
-        await setTestPage('/test', renderComponent('tabs', EXAMPLE_TABS));
-
-        await page.focus('#tabId2Item a');
-        await page.keyboard.press('Enter');
-
         const ariaSelectedValue = await page.$eval('#tabId2Item a', node => node.getAttribute('aria-selected'));
 
         expect(ariaSelectedValue).toBe('true');
       });
 
       it('has the "ons-tab--selected" class assigned', async () => {
-        await setTestPage('/test', renderComponent('tabs', EXAMPLE_TABS));
-
-        await page.focus('#tabId2Item a');
-        await page.keyboard.press('Enter');
-
         const hasClass = await page.$eval('#tabId2Item a', node => node.classList.contains('ons-tab--selected'));
 
         expect(hasClass).toBe(true);
       });
 
       it('shows the corresponding panel', async () => {
-        await setTestPage('/test', renderComponent('tabs', EXAMPLE_TABS));
-
-        await page.focus('#tabId2Item a');
-        await page.keyboard.press('Enter');
-
         const panelHiddenStates = await page.$$eval('.ons-tabs__panel', nodes =>
           nodes.map(node => node.classList.contains('ons-tabs__panel--hidden')),
         );
@@ -139,8 +112,6 @@ describe('script: tabs', () => {
 
     describe('when the right arrow key is pressed', () => {
       it('focuses the next tab', async () => {
-        await setTestPage('/test', renderComponent('tabs', EXAMPLE_TABS));
-
         await page.focus('#tabId2Item a');
         await page.keyboard.press('ArrowRight');
 
@@ -151,8 +122,6 @@ describe('script: tabs', () => {
 
     describe('when the left arrow key is pressed', () => {
       it('focuses the previous tab', async () => {
-        await setTestPage('/test', renderComponent('tabs', EXAMPLE_TABS));
-
         await page.focus('#tabId2Item a');
         await page.keyboard.press('ArrowLeft');
 
@@ -165,11 +134,11 @@ describe('script: tabs', () => {
   describe('when the viewport is small', () => {
     beforeEach(async () => {
       await page.emulate(puppeteer.devices['iPhone X']);
+
+      await setTestPage('/test', renderComponent('tabs', EXAMPLE_TABS));
     });
 
     it('has no aria attributes on tabs', async () => {
-      await setTestPage('/test', renderComponent('tabs', EXAMPLE_TABS));
-
       const tabElements = await page.$$('.ons-tab');
       for (let i = 0; i < 3; ++i) {
         const hasRoleAttribute = await tabElements[i].evaluate(node => node.getAttribute('role') !== null);
@@ -184,8 +153,6 @@ describe('script: tabs', () => {
     });
 
     it('has no hidden tab panels', async () => {
-      await setTestPage('/test', renderComponent('tabs', EXAMPLE_TABS));
-
       const panelCount = await page.$$eval('.ons-tabs__panel', nodes => nodes.length);
       expect(panelCount).toBe(3);
 
