337d19d67b0b6057bf75e416e52c07e3ea6e52cf
Lea Hayes <lea.hayes@ext.ons.gov.uk>
2022-06-07 16:40:58 +0100

    Add puppeteer helper functionality to intercept and fake API requests for testing.

--- src/tests/helpers/puppeteer.js
+++ src/tests/helpers/puppeteer.js
@@ -1,3 +1,5 @@
+import * as url from 'url';
+
 export async function getNodeAttributes(page, selector) {
   return await page.$eval(selector, node => {
     const reducer = (map, attr) => {
@@ -6,4 +8,85 @@ export async function getNodeAttributes(page, selector) {
     };
     return [...node.attributes].reduce(reducer, {});
   });
+}
+
+function sanitizeHref(href) {
+  return url.parse(href, false).href;
+}
+
+export class PuppeteerEndpointFaker {
+  #overrides = new Map();
+  #temporaryOverrides = new Map();
+  #requestHistory = null;
+
+  constructor(basePath) {
+    this.domain = `http://localhost:${process.env.TEST_PORT}`;
+    this.baseUrl = `${this.domain}${basePath}`;
+    this.suppressWarnings = false;
+  }
+
+  get requestHistory() {
+    return this.#requestHistory;
+  }
+
+  async setup(page) {
+    await page.setRequestInterception(true);
+    page.on('request', this.#handleRequest.bind(this));
+  }
+
+  reset() {
+    this.#requestHistory = [];
+    this.#temporaryOverrides.clear();
+  }
+
+  setOverride(path, response) {
+    this.#overrides.set(sanitizeHref(this.baseUrl + path), response);
+  }
+
+  setTemporaryOverride(path, response) {
+    this.#temporaryOverrides.set(sanitizeHref(this.baseUrl + path), response);
+  }
+
+  getRequestCount(path) {
+    console.log(this.requestHistory.map(n => n.path));
+    path = sanitizeHref(path);
+    return this.requestHistory.filter(entry => entry.path === path).length;
+  }
+
+  #resolveOverride(href) {
+    href = sanitizeHref(href);
+    return this.#temporaryOverrides.get(href) ?? this.#overrides.get(href);
+  }
+
+  #handleRequest(interceptedRequest) {
+    if (interceptedRequest.isInterceptResolutionHandled()) {
+      return;
+    }
+
+    if (interceptedRequest.url().startsWith(this.baseUrl)) {
+      const requestInfo = {
+        url: url.parse(interceptedRequest.url(), true),
+        path: interceptedRequest.url().replace(this.baseUrl, ''),
+        headers: interceptedRequest.headers(),
+      };
+
+      this.requestHistory.push(requestInfo);
+
+      const override = this.#resolveOverride(requestInfo.url.href);
+      if (!!override) {
+        console.log(`Faked API request for ${requestInfo.url.href}`);
+        interceptedRequest.respond({
+          status: override.status ?? 200,
+          headers: override.headers ?? {},
+          contentType: override.contentType ?? 'application/json',
+          body: override.body ?? JSON.stringify(override.data),
+        });
+        return;
+      } else if (!this.suppressWarnings) {
+        console.warn(`Fake response not defined for ${requestInfo.url.href}`);
+      }
+    }
+
+    interceptedRequest.continue();
+  }
 }
