392562a58d5925986ffab8bc145f3b4f565f73f1
Lea Hayes <lea.hayes@ext.ons.gov.uk>
2022-06-17 14:19:48 +0100

    Improve re-routing of browser errors to the test console.

--- src/tests/helpers/debug.js
+++ src/tests/helpers/debug.js
@@ -7,6 +7,27 @@ export function quietLog(text, type = 'info') {
   process.stdout.write(`${colorize(text)}\n`);
 }
 
+async function extractTextFromMessage(message) {
+  // Workaround for situation where `message.text()` returns "JSHandle:error" rather than
+  // the actual error text. This is some sort of serialization thing between Chromium and
+  // Puppeteer: https://github.com/puppeteer/puppeteer/issues/3397#issuecomment-434970058
+  const messageParts = await Promise.all(
+    message.args().map(arg =>
+      arg.executionContext().evaluate(arg => {
+        if (arg instanceof Error) {
+          return arg.message;
+        } else {
+          return arg;
+        }
+      }, arg),
+    ),
+  );
+
+  // In most situations `message.text()` will be used...
+  const extractedText = messageParts.join(' ').trim();
+  return extractedText !== '' ? extractedText : message.text();
+}
+
 export function verifyConsoleSubscription(page) {
   if (consoleSubscriptionPage === page) {
     return;
@@ -14,8 +35,9 @@ export function verifyConsoleSubscription(page) {
 
   consoleSubscriptionPage = page;
 
-  page.on('console', message => {
-    quietLog(`browser ${message.type()}: ${message.text()}`, message.type());
+  page.on('console', async message => {
+    const text = await extractTextFromMessage(message);
+    quietLog(`browser ${message.type()}: ${text}`, message.type());
 
     const { url, lineNumber, columnNumber } = message.location();
     if (!!url || !!lineNumber || !!columnNumber) {
