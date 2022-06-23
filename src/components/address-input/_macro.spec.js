0cd851d754d51c31529a48ff0dd899a05e83bb8e
Matthew Simon <info@boxadesign.co.uk>
2022-06-23 10:57:55 +0100

    Fix tests for address-input and update macro option label requirement

--- src/components/address-input/_macro.spec.js
+++ src/components/address-input/_macro.spec.js
@@ -7,6 +7,7 @@ import { renderComponent, templateFaker } from '../../tests/helpers/rendering';
 
 const EXAMPLE_AUTOSUGGEST_ADDRESS_MINIMAL = {
   id: 'address-input-example-id',
+  legend: 'What is the address?',
   label: {
     text: 'Enter address or postcode and select from results',
     id: 'address-input-example-label-id',
