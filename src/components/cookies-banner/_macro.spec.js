66f2f60752010adbb3df190657b4034576c021c7
Matthew Simon <info@boxadesign.co.uk>
2022-07-14 22:34:52 +0100

    Add defaults for en and cy and update tests

--- src/components/cookies-banner/_macro.spec.js
+++ src/components/cookies-banner/_macro.spec.js
@@ -5,167 +5,133 @@ import * as cheerio from 'cheerio';
 import axe from '../../tests/helpers/axe';
 import { renderComponent, templateFaker } from '../../tests/helpers/rendering';
 
-const EXAMPLE_COOKIES_BANNER_MINIMAL = {
-  statementTitle: 'Tell us whether you accept cookies',
+const EXAMPLE_COOKIES_BANNER_PARAMS = {
+  ariaLabel: 'Cookies banner',
+  serviceName: 'ons.gov.uk',
+  statementTitle: 'Cookies on',
+  settingsLinkText: 'Cookie settings',
+  settingsLinkURL: '/cookies',
   statementText:
-    'We use <a href="#0">cookies to collect information</a> about how you use census.gov.uk. We use this information to make the website work as well as possible and improve our services.',
-  confirmationText: 'You’ve accepted all cookies. You can <a href="#0">change your cookie preferences</a> at any time.',
-};
-
-const EXAMPLE_COOKIES_BANNER = {
-  ...EXAMPLE_COOKIES_BANNER_MINIMAL,
-  ariaLabel: 'the cookies banner',
-  primaryButtonText: 'Accept cookies',
-  secondaryButtonText: 'Set preferences',
-  secondaryButtonUrl: 'https://example.com/cookies/set-preferences',
-  confirmationButtonText: 'Close',
-  confirmationButtonTextAria: 'the cookie banner',
+    '<p>Cookies are small files stored on your device when you visit a website. We store some cookies that are essential to make the site work.</p><p>We would like to set <a href="/cookies">additional cookies</a> to remember your settings and understand how you use the site. This helps us to improve our services. </p>',
+  acceptButtonText: 'Accept additional cookies',
+  rejectButtonText: 'Reject additional cookies',
+  preferencesText: 'You can <a href="/cookies">change your cookie preferences</a> at any time.',
+  confirmationButtonText: 'Hide',
+  confirmationButtonTextAria: 'the cookie message',
 };
 
 describe('macro: cookies-banner', () => {
-  it.each([
-    ['minimal parameters', EXAMPLE_COOKIES_BANNER_MINIMAL],
-    ['all parameters', EXAMPLE_COOKIES_BANNER],
-  ])('passes jest-axe checks with %s', async (_, params) => {
-    const $ = cheerio.load(renderComponent('cookies-banner', params));
+  describe.each([
+    ['no parameters provided', {}],
+    ['all parameters provided', EXAMPLE_COOKIES_BANNER_PARAMS],
+  ])('mode: %s', (_, params) => {
+    it('passes jest-axe checks', async () => {
+      const $ = cheerio.load(renderComponent('cookies-banner', params));
 
-    const results = await axe($.html());
-    expect(results).toHaveNoViolations();
+      const results = await axe($.html());
+      expect(results).toHaveNoViolations();
+    });
+
+    it('has `aria-label` of "Cookies banner"', () => {
+      const $ = cheerio.load(renderComponent('cookies-banner', params));
+
+      expect($('.ons-cookies-banner').attr('aria-label')).toBe('Cookies banner');
+    });
+
+    describe('initial banner', () => {
+      it('has `statementTitle` title text', () => {
+        const $ = cheerio.load(renderComponent('cookies-banner', params));
+
+        const statementTitle = $('.ons-cookies-banner__title')
+          .text()
+          .trim();
+        expect(statementTitle).toBe('Cookies on ons.gov.uk');
+      });
+
+      it('has `statementText` text', () => {
+        const $ = cheerio.load(renderComponent('cookies-banner', params));
+
+        const statementText = $('.ons-cookies-banner__primary .ons-cookies-banner__statement')
+          .html()
+          .trim();
+        expect(statementText).toBe(
+          '<p>Cookies are small files stored on your device when you visit a website. We store some cookies that are essential to make the site work.</p><p>We would like to set <a href="/cookies">additional cookies</a> to remember your settings and understand how you use the site. This helps us to improve our services. </p>',
+        );
+      });
+
+      it('Renders an `accept` button with correct text', () => {
+        const faker = templateFaker();
+        const buttonSpy = faker.spy('button');
+
+        faker.renderComponent('cookies-banner', params);
+
+        expect(buttonSpy.occurrences[0].text).toBe('Accept additional cookies');
+      });
+
+      it('Renders a `reject` button with correct text', () => {
+        const faker = templateFaker();
+        const buttonSpy = faker.spy('button');
+
+        faker.renderComponent('cookies-banner', params);
+
+        expect(buttonSpy.occurrences[1].text).toBe('Reject additional cookies');
+      });
+
+      it('Renders a link with text', () => {
+        const $ = cheerio.load(renderComponent('cookies-banner', params));
+
+        const linkText = $('.ons-cookies-banner__link')
+          .text()
+          .trim();
+        expect(linkText).toBe('Cookie settings');
+      });
+
+      it('Renders a link with url', () => {
+        const $ = cheerio.load(renderComponent('cookies-banner', params));
+
+        const linkText = $('.ons-cookies-banner__link').attr('href');
+        expect(linkText).toBe('/cookies');
+      });
+    });
+
+    describe('confirmation banner', () => {
+      it('has `preferencesText` text', () => {
+        const $ = cheerio.load(renderComponent('cookies-banner', params));
+
+        const preferencesText = $('.ons-cookies-banner__confirmation .ons-cookies-banner__preferences-text')
+          .html()
+          .trim();
+        expect(preferencesText).toBe('You can <a href="/cookies">change your cookie preferences</a> at any time.');
+      });
+
+      it('renders a button with text', () => {
+        const faker = templateFaker();
+        const buttonSpy = faker.spy('button');
+
+        faker.renderComponent('cookies-banner', params);
+
+        expect(buttonSpy.occurrences[2].text).toBe('Hide');
+      });
+
+      it('has the correct `confirmationButtonTextAria` for `buttonContext`', () => {
+        const faker = templateFaker();
+        const buttonSpy = faker.spy('button');
+
+        faker.renderComponent('cookies-banner', params);
+
+        expect(buttonSpy.occurrences[2].buttonContext).toBe('the cookie message');
+      });
+    });
   });
 
-  it('has a `role` of "region"', () => {
-    const $ = cheerio.load(renderComponent('cookies-banner', EXAMPLE_COOKIES_BANNER_MINIMAL));
-
-    expect($('.ons-cookies-banner').attr('role')).toBe('region');
-  });
-
-  it('has a default `aria-label` of "Cookies banner"', () => {
-    const $ = cheerio.load(renderComponent('cookies-banner', EXAMPLE_COOKIES_BANNER_MINIMAL));
-
-    expect($('.ons-cookies-banner').attr('aria-label')).toBe('Cookies banner');
-  });
-
-  it('has the provided `ariaLabel` for `aria-label`', () => {
-    const $ = cheerio.load(renderComponent('cookies-banner', EXAMPLE_COOKIES_BANNER));
-
-    expect($('.ons-cookies-banner').attr('aria-label')).toBe('the cookies banner');
-  });
-
-  describe('content', () => {
-    it('has provided `statementTitle` title text', () => {
-      const $ = cheerio.load(renderComponent('cookies-banner', EXAMPLE_COOKIES_BANNER_MINIMAL));
+  describe('mode: Welsh language', () => {
+    it('has the welsh version of default values', () => {
+      const $ = cheerio.load(renderComponent('cookies-banner', { lang: 'cy' }));
 
       const statementTitle = $('.ons-cookies-banner__title')
         .text()
         .trim();
-      expect(statementTitle).toBe('Tell us whether you accept cookies');
-    });
-
-    it('has provided `statementText` text', () => {
-      const $ = cheerio.load(renderComponent('cookies-banner', EXAMPLE_COOKIES_BANNER_MINIMAL));
-
-      const statementText = $('.ons-cookies-banner__primary .ons-cookies-banner__desc')
-        .html()
-        .trim();
-      expect(statementText).toBe(
-        'We use <a href="#0">cookies to collect information</a> about how you use census.gov.uk. We use this information to make the website work as well as possible and improve our services.',
-      );
-    });
-
-    it('has provided `confirmationText` text', () => {
-      const $ = cheerio.load(renderComponent('cookies-banner', EXAMPLE_COOKIES_BANNER_MINIMAL));
-
-      const statementText = $('.ons-cookies-banner__confirmation .ons-cookies-banner__desc')
-        .html()
-        .trim();
-      expect(statementText).toBe('You’ve accepted all cookies. You can <a href="#0">change your cookie preferences</a> at any time.');
-    });
-  });
-
-  describe('primary button', () => {
-    it('assumes default text "Accept all cookies"', () => {
-      const faker = templateFaker();
-      const buttonSpy = faker.spy('button');
-
-      faker.renderComponent('cookies-banner', EXAMPLE_COOKIES_BANNER_MINIMAL);
-
-      expect(buttonSpy.occurrences[0].text).toBe('Accept all cookies');
-    });
-
-    it('has provided `primaryButtonText` text', () => {
-      const faker = templateFaker();
-      const buttonSpy = faker.spy('button');
-
-      faker.renderComponent('cookies-banner', EXAMPLE_COOKIES_BANNER);
-
-      expect(buttonSpy.occurrences[0].text).toBe('Accept cookies');
-    });
-  });
-
-  describe('secondary button', () => {
-    it('assumes default text "Set cookie preferences"', () => {
-      const faker = templateFaker();
-      const buttonSpy = faker.spy('button');
-
-      faker.renderComponent('cookies-banner', EXAMPLE_COOKIES_BANNER_MINIMAL);
-
-      expect(buttonSpy.occurrences[1].text).toBe('Set cookie preferences');
-    });
-
-    it('has provided `secondaryButtonText` text', () => {
-      const faker = templateFaker();
-      const buttonSpy = faker.spy('button');
-
-      faker.renderComponent('cookies-banner', EXAMPLE_COOKIES_BANNER);
-
-      expect(buttonSpy.occurrences[1].text).toBe('Set preferences');
-    });
-
-    it('has provided `secondaryButtonUrl`', () => {
-      const faker = templateFaker();
-      const buttonSpy = faker.spy('button');
-
-      faker.renderComponent('cookies-banner', EXAMPLE_COOKIES_BANNER);
-
-      expect(buttonSpy.occurrences[1].url).toBe('https://example.com/cookies/set-preferences');
-    });
-  });
-
-  describe('confirmation button', () => {
-    it('assumes default text "Hide"', () => {
-      const faker = templateFaker();
-      const buttonSpy = faker.spy('button');
-
-      faker.renderComponent('cookies-banner', EXAMPLE_COOKIES_BANNER_MINIMAL);
-
-      expect(buttonSpy.occurrences[2].text).toBe('Hide');
-    });
-
-    it('has provided `confirmationButtonText` text', () => {
-      const faker = templateFaker();
-      const buttonSpy = faker.spy('button');
-
-      faker.renderComponent('cookies-banner', EXAMPLE_COOKIES_BANNER);
-
-      expect(buttonSpy.occurrences[2].text).toBe('Close');
-    });
-
-    it('has a default `buttonContext` of "cookie banner"', () => {
-      const faker = templateFaker();
-      const buttonSpy = faker.spy('button');
-
-      faker.renderComponent('cookies-banner', EXAMPLE_COOKIES_BANNER_MINIMAL);
-
-      expect(buttonSpy.occurrences[2].buttonContext).toBe('the cookie message');
-    });
-
-    it('has the provided `confirmationButtonTextAria` for `buttonContext`', () => {
-      const faker = templateFaker();
-      const buttonSpy = faker.spy('button');
-
-      faker.renderComponent('cookies-banner', EXAMPLE_COOKIES_BANNER);
-
-      expect(buttonSpy.occurrences[2].buttonContext).toBe('the cookie banner');
+      expect(statementTitle).toBe('Cwcis ar ons.gov.uk');
     });
   });
 });
