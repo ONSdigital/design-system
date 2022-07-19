1a21cf463b0ced2a5dc593621be09e985ff9e5ab
Matthew Simon <info@boxadesign.co.uk>
2022-07-19 15:02:04 +0100

    Updates for collapsible rework

--- src/components/collapsible/collapsible.js
+++ src/components/collapsible/collapsible.js
@@ -3,13 +3,9 @@ export default class Collapsible {
     this.saveState = collapsibleElement.getAttribute('data-save-state') === 'true';
     this.open = collapsibleElement.getAttribute('data-open') === 'true';
 
-    // State
-    this.group = collapsibleElement.getAttribute('data-group');
-
     // Elements
     this.collapsible = collapsibleElement;
     this.collapsibleHeader = this.collapsible.querySelector('.ons-js-collapsible-heading');
-    this.content = this.collapsible.querySelector('.ons-js-collapsible-content');
 
     // Initialise
     const collapsibleId = collapsibleElement.getAttribute('id');
@@ -19,19 +15,47 @@ export default class Collapsible {
     } else {
       this.setOpen(false);
     }
+
+    this.collapsibleHeader.addEventListener('click', this.toggle.bind(this));
+    this.collapsibleHeader.addEventListener('keydown', this.keyboardInteraction.bind(this));
+  }
+
+  toggle(event) {
+    // Storing previous timestamp and checking against the current event's timestamp prevents event propagation
+    // to collapsibleHeader element when the button is in the collapsibleHeader, but does not prevent event propagation to the body for analytics
+    if (this.previousTimestamp !== event.timeStamp) {
+      this.previousTimestamp = event.timeStamp;
+      this.setOpen(!this.isOpen);
+    }
   }
 
   setOpen(open) {
     if (open !== this.isOpen) {
       const action = open ? 'Open' : 'Close';
+
       this.isOpen = open;
       this.collapsibleHeader.setAttribute('data-ga-action', `${action} panel`);
+      this.collapsible['setAttribute']('open', '');
     }
 
     if (this.saveState === true && open === true) {
       localStorage.setItem(this.collapsible.getAttribute('id'), true);
     } else {
       localStorage.removeItem(this.collapsible.getAttribute('id'));
+    }
+  }
+
+  keyboardInteraction(event) {
+    const keyCode = event.which;
+    switch (keyCode) {
+      // Enter/Space
+      case 13:
+      case 32:
+        event.preventDefault();
+        event.stopPropagation();
+
+        this.toggle(event);
+        break;
     }
   }
 }
