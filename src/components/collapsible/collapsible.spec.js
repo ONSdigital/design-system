243f8b8deaead776039a22443c13d3ab88fbfffc
Matt Simon <info@boxadesign.co.uk>
2022-08-03 12:00:34 +0100

    Collapsible - use details element and update functionality (#2013)
    * Collapsible - use details element and update functionality
    
    * WIP on collapsible
    
    * Updates for collapsible rework
    
    * Update collapsible js
    
    * Update js, tests and docs
    
    * Fix bug with all button when set to open and add more tests
    
    * Revert package.json
    
    * Move collapsible group into accordion and fix ga label bug
    
    * Update tests
    
    * Update tests

--- src/components/collapsible/collapsible.spec.js
+++ src/components/collapsible/collapsible.spec.js
@@ -7,53 +7,6 @@ const EXAMPLE_COLLAPSIBLE_BASIC = {
 };
 
 describe('script: collapsible', () => {
-  it('begins closed', async () => {
-    await setTestPage('/test', renderComponent('collapsible', EXAMPLE_COLLAPSIBLE_BASIC));
-
-    const openItemCount = await page.$$eval('.ons-collapsible--open', elements => elements.length);
-    expect(openItemCount).toBe(0);
-  });
-
-  it('sets the `role` attribute', async () => {
-    await setTestPage('/test', renderComponent('collapsible', EXAMPLE_COLLAPSIBLE_BASIC));
-
-    const role = await page.$eval('.ons-collapsible', element => element.getAttribute('role'));
-    expect(role).toBe('group');
-  });
-
-  it('adds the correct class', async () => {
-    await setTestPage('/test', renderComponent('collapsible', EXAMPLE_COLLAPSIBLE_BASIC));
-
-    const className = await page.$eval('.ons-collapsible', element => element.classList.contains('ons-collapsible--initialised'));
-    expect(className).toBe(true);
-  });
-
-  it('sets attributes on the heading element', async () => {
-    await setTestPage('/test', renderComponent('collapsible', EXAMPLE_COLLAPSIBLE_BASIC));
-
-    const role = await page.$eval('.ons-js-collapsible-heading', element => element.getAttribute('role'));
-    const ariaControls = await page.$eval('.ons-js-collapsible-heading', element => element.getAttribute('aria-controls'));
-    const tabIndex = await page.$eval('.ons-js-collapsible-heading', element => element.getAttribute('tabindex'));
-
-    expect(role).toBe('link');
-    expect(ariaControls).toBe('collapsible-id');
-    expect(tabIndex).toBe('0');
-  });
-
-  it('sets attributes on the button element', async () => {
-    await setTestPage('/test', renderComponent('collapsible', EXAMPLE_COLLAPSIBLE_BASIC));
-
-    const ariaControls = await page.$eval('.ons-js-collapsible-button', element => element.getAttribute('aria-controls'));
-    expect(ariaControls).toBe('collapsible-id');
-  });
-
-  it('sets the button element to be visible', async () => {
-    await setTestPage('/test', renderComponent('collapsible', EXAMPLE_COLLAPSIBLE_BASIC));
-
-    const openClass = await page.$eval('.ons-collapsible', element => element.classList.contains('ons-collapsible--open'));
-    expect(openClass).toBe(false);
-  });
-
   it('begins open when specified', async () => {
     await setTestPage(
       '/test',
@@ -63,8 +16,8 @@ describe('script: collapsible', () => {
       }),
     );
 
-    const openClass = await page.$eval('.ons-collapsible', element => element.classList.contains('ons-collapsible--open'));
-    expect(openClass).toBe(true);
+    const openAttribute = await page.$eval('.ons-js-collapsible', node => node.open !== null);
+    expect(openAttribute).toBe(true);
   });
 
   describe('when the collapsible heading is clicked to open the collapsible', () => {
@@ -78,59 +31,10 @@ describe('script: collapsible', () => {
       expect(openAttribute).toBe(true);
     });
 
-    it('sets the open class', async () => {
-      const openClass = await page.$eval('.ons-collapsible', element => element.classList.contains('ons-collapsible--open'));
-      expect(openClass).toBe(true);
-    });
-
-    it('sets the `aria` attributes', async () => {
-      const ariaExpanded = await page.$eval('.ons-js-collapsible-heading', element => element.getAttribute('aria-expanded'));
-      const ariaHidden = await page.$eval('.ons-js-collapsible-content', element => element.getAttribute('aria-hidden'));
-
-      expect(ariaExpanded).toBe('true');
-      expect(ariaHidden).toBe('false');
-    });
-
     it('sets the `ga` attributes', async () => {
       const gaHeadingAttribute = await page.$eval('.ons-js-collapsible-heading', element => element.getAttribute('data-ga-action'));
-      const gaButtonAttribute = await page.$eval('.ons-js-collapsible-button', element => element.getAttribute('data-ga-action'));
 
       expect(gaHeadingAttribute).toBe('Open panel');
-      expect(gaButtonAttribute).toBe('Open panel');
-    });
-  });
-
-  describe('when the collapsible button is clicked to close the collapsible', () => {
-    beforeEach(async () => {
-      await setTestPage('/test', renderComponent('collapsible', EXAMPLE_COLLAPSIBLE_BASIC));
-      await page.click('.ons-js-collapsible-heading');
-      await page.click('.ons-js-collapsible-button');
-    });
-
-    it('removes the `open` attribute', async () => {
-      const openAttribute = await page.$eval('.ons-js-collapsible', node => node.open === true);
-      expect(openAttribute).toBe(false);
-    });
-
-    it('removes the open class', async () => {
-      const openClass = await page.$eval('.ons-collapsible', element => element.classList.contains('ons-collapsible--open'));
-      expect(openClass).toBe(false);
-    });
-
-    it('sets the `aria` attributes', async () => {
-      const ariaExpanded = await page.$eval('.ons-js-collapsible-heading', element => element.getAttribute('aria-expanded'));
-      const ariaHidden = await page.$eval('.ons-js-collapsible-content', element => element.getAttribute('aria-hidden'));
-
-      expect(ariaExpanded).toBe('false');
-      expect(ariaHidden).toBe('true');
-    });
-
-    it('sets the `ga` attributes', async () => {
-      const gaHeadingAttribute = await page.$eval('.ons-js-collapsible-heading', element => element.getAttribute('data-ga-action'));
-      const gaButtonAttribute = await page.$eval('.ons-js-collapsible-button', element => element.getAttribute('data-ga-action'));
-
-      expect(gaHeadingAttribute).toBe('Close panel');
-      expect(gaButtonAttribute).toBe('Close panel');
     });
   });
 
@@ -146,8 +50,8 @@ describe('script: collapsible', () => {
       });
 
       it('opens the collapsible content', async () => {
-        const openClass = await page.$eval('.ons-collapsible', element => element.classList.contains('ons-collapsible--open'));
-        expect(openClass).toBe(true);
+        const openAttribute = await page.$eval('.ons-js-collapsible', node => node.open !== null);
+        expect(openAttribute).toBe(true);
       });
     });
 
@@ -157,8 +61,8 @@ describe('script: collapsible', () => {
       });
 
       it('opens the collapsible content', async () => {
-        const openClass = await page.$eval('.ons-collapsible', element => element.classList.contains('ons-collapsible--open'));
-        expect(openClass).toBe(true);
+        const openAttribute = await page.$eval('.ons-js-collapsible', node => node.open !== null);
+        expect(openAttribute).toBe(true);
       });
     });
   });
@@ -194,43 +98,6 @@ describe('script: collapsible', () => {
         const localStorage = await page.evaluate(() => localStorage.getItem('collapsible-id'));
         expect(localStorage).toBe(null);
       });
-    });
-  });
-
-  describe('when the collapsible is an accordion', () => {
-    beforeEach(async () => {
-      await setTestPage(
-        '/test',
-        renderComponent('collapsible', {
-          ...EXAMPLE_COLLAPSIBLE_BASIC,
-          isAccordion: true,
-        }),
-      );
-    });
-
-    it('does not set `tabindex` on the heading element', async () => {
-      const tabIndex = await page.$eval('.ons-js-collapsible-heading', element => element.getAttribute('tabindex'));
-
-      expect(tabIndex).toBe(null);
-    });
-  });
-
-  describe('when the collapsible is an accordion and the simple variant', () => {
-    beforeEach(async () => {
-      await setTestPage(
-        '/test',
-        renderComponent('collapsible', {
-          ...EXAMPLE_COLLAPSIBLE_BASIC,
-          isAccordion: true,
-          variants: 'simple',
-        }),
-      );
-    });
-
-    it('does set `tabindex` on the heading element', async () => {
-      const tabIndex = await page.$eval('.ons-js-collapsible-heading', element => element.getAttribute('tabindex'));
-
-      expect(tabIndex).toBe('0');
     });
   });
 });
