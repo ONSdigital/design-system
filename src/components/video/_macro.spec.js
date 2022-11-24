ad2c49c4490862e3a83227b934aeb4a4ca9d4168
Matthew Simon <info@boxadesign.co.uk>
2022-11-24 13:45:18 +0000

    load video only when cookie is accepted

--- src/components/video/_macro.spec.js
+++ src/components/video/_macro.spec.js
@@ -8,53 +8,54 @@ import { renderComponent } from '../../tests/helpers/rendering';
 const EXAMPLE_VIDEO_YOUTUBE = {
   videoEmbedUrl: 'https://www.youtube.com/embed/_EGJlvkgbPo',
   title: 'Census 2021 promotional TV advert',
-};
-
-const EXAMPLE_VIDEO_VIMEO = {
-  videoEmbedUrl: 'https://player.vimeo.com/video/508878572',
-  title: 'A message from National Statistician Ian Diamond for National Apprenticeship Week 2021',
+  linkText: 'Example link text',
 };
 
 describe('macro: video', () => {
-  describe('mode: YouTube', () => {
-    it('passes jest-axe checks', async () => {
-      const $ = cheerio.load(renderComponent('video', EXAMPLE_VIDEO_YOUTUBE));
+  it('passes jest-axe checks', async () => {
+    const $ = cheerio.load(renderComponent('video', EXAMPLE_VIDEO_YOUTUBE));
 
-      const results = await axe($.html());
-      expect(results).toHaveNoViolations();
-    });
-
-    it('outputs an <iframe> element with the provided `title`', () => {
-      const $ = cheerio.load(renderComponent('video', EXAMPLE_VIDEO_YOUTUBE));
-
-      expect($('iframe').attr('title')).toBe('Census 2021 promotional TV advert');
-    });
-
-    it('outputs an <iframe> element with the provided `videoEmbedUrl`', () => {
-      const $ = cheerio.load(renderComponent('video', EXAMPLE_VIDEO_YOUTUBE));
-
-      expect($('iframe').attr('src')).toBe('https://www.youtube.com/embed/_EGJlvkgbPo');
-    });
+    const results = await axe($.html());
+    expect(results).toHaveNoViolations();
   });
 
-  describe('mode: Vimeo', () => {
-    it('passes jest-axe checks', async () => {
-      const $ = cheerio.load(renderComponent('video', EXAMPLE_VIDEO_VIMEO));
+  it('outputs the default cover image', () => {
+    const $ = cheerio.load(renderComponent('video', EXAMPLE_VIDEO_YOUTUBE));
 
-      const results = await axe($.html());
-      expect(results).toHaveNoViolations();
-    });
+    expect($('.ons-video__img').attr('src')).toBe('/img/placeholder-video.png');
+  });
 
-    it('outputs an <iframe> element with the provided `title`', () => {
-      const $ = cheerio.load(renderComponent('video', EXAMPLE_VIDEO_VIMEO));
+  it('outputs the provided cover image', () => {
+    const $ = cheerio.load(renderComponent('video', { ...EXAMPLE_VIDEO_YOUTUBE, coverImage: 'example-cover.png' }));
 
-      expect($('iframe').attr('title')).toBe('A message from National Statistician Ian Diamond for National Apprenticeship Week 2021');
-    });
+    expect($('.ons-video__img').attr('src')).toBe('example-cover.png');
+  });
 
-    it('outputs an <iframe> element with the provided `videoEmbedUrl`', () => {
-      const $ = cheerio.load(renderComponent('video', EXAMPLE_VIDEO_VIMEO));
+  it('outputs the provided link text', () => {
+    const $ = cheerio.load(renderComponent('video', { ...EXAMPLE_VIDEO_YOUTUBE, coverImage: 'example-cover.png' }));
 
-      expect($('iframe').attr('src')).toBe('https://player.vimeo.com/video/508878572');
-    });
+    expect(
+      $('.ons-video__link-text')
+        .text()
+        .trim(),
+    ).toBe('Example link text');
+  });
+
+  it('outputs a hyperlink with the provided `url`', () => {
+    const $ = cheerio.load(renderComponent('video', EXAMPLE_VIDEO_YOUTUBE));
+
+    expect($('.ons-video__link').attr('href')).toBe('https://www.youtube.com/embed/_EGJlvkgbPo');
+  });
+
+  it('outputs an <iframe> element with the provided `title`', () => {
+    const $ = cheerio.load(renderComponent('video', EXAMPLE_VIDEO_YOUTUBE));
+
+    expect($('iframe').attr('title')).toBe('Census 2021 promotional TV advert');
+  });
+
+  it('outputs an <iframe> element with the provided `videoEmbedUrl` data attribute', () => {
+    const $ = cheerio.load(renderComponent('video', EXAMPLE_VIDEO_YOUTUBE));
+
+    expect($('iframe').attr('data-src')).toBe('https://www.youtube.com/embed/_EGJlvkgbPo');
   });
 });
