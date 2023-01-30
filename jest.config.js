f19299afb66d5cdd59d3942a367c1f5c98208059
Matt Simon <info@boxadesign.co.uk>
2023-01-30 15:59:16 +0000

    Add snapshot testing for base page template (#2557)

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
 const config = {
   verbose: true,
   preset: process.env.TEST_WITH_PUPPETEER ? 'jest-puppeteer' : undefined,
+  snapshotResolver: `${process.cwd()}/src/tests/helpers/snapshotResolver.js`,
 };
 
 module.exports = config;
