24580910206ba76964e808c91c734d7f0cd337a7
Matt Simon <info@boxadesign.co.uk>
2023-03-27 14:41:40 +0100

    Fix issue with textarea and update references

--- src/components/textarea/textarea.dom.js
+++ src/components/textarea/textarea.dom.js
@@ -1,10 +1,7 @@
-import autosize from 'autosize';
-
 import domready from '../../js/domready';
 
 async function initialise() {
   const limitedInputs = [...document.querySelectorAll('.ons-js-char-limit-input')];
-  autosize(document.querySelectorAll('textarea'));
 
   if (limitedInputs.length) {
     const CharLimit = (await import('../char-check-limit/character-limit')).default;
