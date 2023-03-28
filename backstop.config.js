fe19dc25c2460db6082fe8b670d11f31eacef5dc
Matt Simon <info@boxadesign.co.uk>
2023-03-28 11:32:57 +0100

    Install backstopjs and git lfs for visual regression testing

---
+++ backstop.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  id: 'ds-vr-test',
+  viewports: [
+    {
+      name: 'desktop',
+      width: 1920,
+      height: 1080,
+    },
+    {
+      name: 'tablet',
+      width: 768,
+      height: 1024,
+    },
+    {
+      name: 'mobile',
+      width: 375,
+      height: 667,
+    },
+  ],
+  scenarios: [],
+  paths: {
+    bitmaps_reference: 'backstop_data/bitmaps_reference',
+    bitmaps_test: 'backstop_data/bitmaps_test',
+    engine_scripts: 'backstop_data/engine_scripts',
+    html_report: 'backstop_data/html_report',
+    ci_report: 'backstop_data/ci_report',
+  },
+  engine: 'puppeteer',
+  engineOptions: {
+    args: ['--no-sandbox'],
+  },
+  report: process.env.RUNNING_IN_CI === 'true' ? [] : ['browser'],
+};
