5aac03fd67eea5ee7aa4543211ee1ce2281fd408
Alessio <alessio.venturini@and.digital>
2023-06-09 14:08:46 +0100

    small test code implemented to see how tooltip will look on click

--- src/components/input/character-check.dom.js
+++ src/components/input/character-check.dom.js
@@ -1,6 +1,39 @@
 import domready from '../../js/domready';
 
 async function initialise() {
+  const abbrInputs = [...document.querySelectorAll('.ons-js-input-abbr')];
+
+  if (abbrInputs.length > 0) {
+    abbrInputs.forEach(abbrInput => {
+      const addTooltip = input => {
+        const target = input.target;
+        target.classList.add('ons-input__tooltip');
+        const title = target.getAttribute('title');
+        const previousTooltip = [...document.getElementsByClassName('ons-input__tooltip__text')];
+        if (title && previousTooltip.length === 0) {
+          // Create the tooltip span element
+          const tooltip = document.createElement('span');
+          tooltip.className = 'ons-input__tooltip__text ons-u-fs-s';
+          tooltip.textContent = title;
+
+          // Append the tooltip to the target element
+          target.appendChild(tooltip);
+
+          // Remove the tooltip after a delay
+          setTimeout(function() {
+            tooltip.remove();
+          }, 2000);
+        } else if (title && previousTooltip.length > 0) {
+          // accessing first element in the array as only one tooltip would be in the dom an any one time
+          previousTooltip[0].remove();
+          addTooltip(input);
+        }
+      };
+      abbrInput.addEventListener('click', input => {
+        addTooltip(input);
+      });
+    });
+  }
   const checkedInputs = [...document.querySelectorAll('.ons-js-char-check-input')];
 
   if (checkedInputs.length) {
