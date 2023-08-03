29cbffc900f819a78fd847bf0f36ae7f3bf09a0b
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2023-08-03 15:54:38 +0100

    test netlify fix

--- prepare.js
+++
@@ -1,6 +0,0 @@
-// This file removes the instalation of husky hooks on CI. It was causing an issue on Netlify
-// https://answers.netlify.com/t/error-when-building-a-react-app-that-has-husky-installed/54344
-const isCi = process.env.CI !== undefined;
-if (!isCi) {
-  require('husky').install();
-}
