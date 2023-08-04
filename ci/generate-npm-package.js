df527ab9818c8cb8384a3b72c33bec6138d73534
Precious Onyenaucheya <precious.onyenaucheya@ons.gov.uk>
2023-08-04 09:56:10 +0100

    modfied generate npm-package file

--- ci/generate-npm-package.js
+++ ci/generate-npm-package.js
@@ -8,12 +8,12 @@ const componentsPath = `${sourcePath}/components`;
 const newComponentsPath = `${cwd}/components`;
 const templatesPath = `${sourcePath}/layout`;
 const newTemplatesPath = `${cwd}/layout`;
-const assetFolders = ['css', 'favicons', 'fonts', 'img', 'scripts', 'js'];
-const builtAssetsFolders = assetFolders.map(folder => `${cwd}/build/${folder}`);
+const assetFolders = ['css', 'favicons', 'img', 'scripts', 'js'];
+const builtAssetsFolders = assetFolders.map((folder) => `${cwd}/build/${folder}`);
 const newSassPath = `${cwd}/scss`;
 
 async function removeExistingFolders() {
-  const folders = [newComponentsPath, newTemplatesPath, ...assetFolders];
+  const folders = [newComponentsPath, newTemplatesPath, 'fonts', ...assetFolders];
   for (let folder of folders) {
     await fs.remove(folder);
   }
@@ -27,7 +27,7 @@ async function copyAssets() {
     await fs.ensureDir(folder);
 
     try {
-      const files = (await fs.readdir(builtPath)).filter(path => !path.includes('patternlib'));
+      const files = (await fs.readdir(builtPath)).filter((path) => !path.includes('patternlib'));
 
       for (let file of files) {
         if (file.match(/(\.\w+)$/)) {
@@ -37,7 +37,7 @@ async function copyAssets() {
           const nestedBuiltPath = `${builtPath}/${file}`;
           await fs.ensureDir(newFolderPath);
 
-          const nestedFiles = (await fs.readdir(nestedBuiltPath)).filter(path => !path.includes('patternlib'));
+          const nestedFiles = (await fs.readdir(nestedBuiltPath)).filter((path) => !path.includes('patternlib'));
 
           for (let nestedFile of nestedFiles) {
             await fs.copy(`${nestedBuiltPath}/${nestedFile}`, `${newFolderPath}/${nestedFile}`);
@@ -53,6 +53,7 @@ async function copyAssets() {
 async function copyBaseSass() {
   await fs.ensureDir(newSassPath);
   await fs.copy(`${sourcePath}/scss`, `${newSassPath}`);
+  await fs.copy(`${sourcePath}/static/fonts`, `${newSassPath}`);
 }
 
 async function run() {
