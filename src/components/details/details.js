0f628878185fd7333b6d6bd3d3bcd4832bfef368
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2023-11-16 16:25:36 +0000

    revert details component to fix print style

--- src/components/details/details.js
+++ src/components/details/details.js
@@ -3,21 +3,31 @@ export default class Details {
     this.saveState = detailsElement.getAttribute('data-save-state') === 'true';
     this.open = detailsElement.getAttribute('data-open') === 'true';
     this.group = detailsElement.getAttribute('data-group');
+    this.isAccordion = detailsElement.classList.contains('ons-details--accordion');
 
     // Elements
     this.details = detailsElement;
     this.detailsHeader = this.details.querySelector('.ons-js-details-heading');
+    this.content = this.details.querySelector('.ons-js-details-content');
 
     // Initialise
     const detailsId = detailsElement.getAttribute('id');
 
+    this.details.setAttribute('role', 'group');
+    this.detailsHeader.setAttribute('role', 'link');
+    this.detailsHeader.setAttribute('aria-controls', detailsId);
+
+    this.detailsHeader.setAttribute('tabindex', 0);
+
     if (localStorage.getItem(detailsId) || this.open) {
       this.setOpen(true);
-      this.details['setAttribute']('open', '');
+    } else {
+      this.setOpen(false);
     }
 
     this.detailsHeader.addEventListener('click', this.toggle.bind(this));
     this.detailsHeader.addEventListener('keydown', this.keyboardInteraction.bind(this));
+    this.details.classList.add('ons-details--initialised');
   }
 
   toggle(event) {
@@ -28,10 +38,14 @@ export default class Details {
   setOpen(open) {
     if (open !== this.isOpen) {
       const action = open ? 'Open' : 'Close';
+      const cls = open ? 'add' : 'remove';
       const openAttribute = open ? 'set' : 'remove';
 
       this.isOpen = open;
       this.details[`${openAttribute}Attribute`]('open', '');
+      this.details.classList[cls]('ons-details--open');
+      this.detailsHeader.setAttribute('aria-expanded', open);
+      this.content.setAttribute('aria-hidden', !open);
       this.detailsHeader.setAttribute('data-ga-action', `${action} panel`);
 
       if (this.onOpen && this.onClose) {
