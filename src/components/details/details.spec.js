a5c4643eb9e04710909c33fac3e9072a29168da1
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2023-11-21 12:05:18 +0000

    add test for multiple

--- src/components/details/details.spec.js
+++ src/components/details/details.spec.js
@@ -1,4 +1,4 @@
-import { renderComponent, setTestPage } from '../../tests/helpers/rendering';
+import { renderComponent, renderTemplate, setTestPage } from '../../tests/helpers/rendering';
 
 const EXAMPLE_DETAILS_BASIC = {
   id: 'details-id',
@@ -6,11 +6,21 @@ const EXAMPLE_DETAILS_BASIC = {
   content: 'Content for details',
 };
 
-const EXAMPLE_DETAILS_BASIC_2 = {
-  id: 'details-id-2',
-  title: 'Title for details',
-  content: 'Content for details',
-};
+const EXAMPLE_PAGE = `
+      ${renderComponent('details', {
+        id: 'details-id',
+        title: 'Title for details',
+        content: 'Content for details',
+      })}
+
+      ${renderComponent('details', {
+        id: 'details-id-2',
+        title: 'Title for details',
+        content: 'Content for details',
+      })}
+  `;
+
+const RENDERED_EXAMPLE_PAGE = renderTemplate(EXAMPLE_PAGE);
 
 describe('script: details', () => {
   it('begins open when specified', async () => {
@@ -22,8 +32,8 @@ describe('script: details', () => {
       }),
     );
 
-    const openAttribute = await page.$eval('.ons-js-details', (node) => node.open !== null);
-    expect(openAttribute).toBe(true);
+    const detailsOpenClass = await page.$eval('.ons-js-details', (node) => node.classList.contains('ons-details--open'));
+    expect(detailsOpenClass).toBe(true);
   });
 
   describe('when the details heading is clicked to open the details', () => {
@@ -32,9 +42,10 @@ describe('script: details', () => {
       await page.click('.ons-js-details-heading');
     });
 
-    it('sets the `open` attribute', async () => {
-      const openAttribute = await page.$eval('.ons-js-details', (node) => node.open !== null);
-      expect(openAttribute).toBe(true);
+    it('sets the `open` attribute and adds the correct class', async () => {
+      const detailsOpenClass = await page.$eval('.ons-js-details', (node) => node.classList.contains('ons-details--open'));
+
+      expect(detailsOpenClass).toBe(true);
     });
 
     it('sets the `ga` attributes', async () => {
@@ -46,15 +57,16 @@ describe('script: details', () => {
 
   describe('when there is more than one details component and a details heading is clicked to open the details', () => {
     beforeEach(async () => {
-      await setTestPage('/test', (renderComponent('details', EXAMPLE_DETAILS_BASIC), renderComponent('details', EXAMPLE_DETAILS_BASIC_2)));
+      await setTestPage('/test', RENDERED_EXAMPLE_PAGE);
       await page.click('#details-id > .ons-js-details-heading');
     });
 
-    it('sets the `open` attribute on the right component', async () => {
-      const openAttribute = await page.$eval('#details-id', (node) => node.open !== null);
-      expect(openAttribute).toBe(true);
-      const openAttribute2 = await page.$eval('#details-id-2', (node) => node.open !== null);
-      expect(openAttribute2).toBe(false);
+    it('sets the `open` attribute and open class on the right component', async () => {
+      const detailsOpenClass = await page.$eval('#details-id', (node) => node.classList.contains('ons-details--open'));
+      const detailsOpenClass2 = await page.$eval('#details-id-2', (node) => node.classList.contains('ons-details--open'));
+
+      expect(detailsOpenClass).toBe(true);
+      expect(detailsOpenClass2).toBe(false);
     });
   });
 
@@ -70,8 +82,8 @@ describe('script: details', () => {
       });
 
       it('opens the details content', async () => {
-        const openAttribute = await page.$eval('.ons-js-details', (node) => node.open !== null);
-        expect(openAttribute).toBe(true);
+        const detailsOpenClass = await page.$eval('.ons-js-details', (node) => node.classList.contains('ons-details--open'));
+        expect(detailsOpenClass).toBe(true);
       });
     });
 
@@ -81,8 +93,8 @@ describe('script: details', () => {
       });
 
       it('opens the details content', async () => {
-        const openAttribute = await page.$eval('.ons-js-details', (node) => node.open !== null);
-        expect(openAttribute).toBe(true);
+        const detailsOpenClass = await page.$eval('.ons-js-details', (node) => node.classList.contains('ons-details--open'));
+        expect(detailsOpenClass).toBe(true);
       });
     });
   });
