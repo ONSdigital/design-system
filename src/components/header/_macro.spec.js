2c949ba4cd9cc9388b2fc5a728db5ae3fe192c2a
Bali Birch-Lee_ONS <149602681+balibirchlee@users.noreply.github.com>
2024-01-29 10:38:09 +0000

    Update src/components/header/_macro.spec.js
    Co-authored-by: rmccar <42928680+rmccar@users.noreply.github.com>

--- src/components/header/_macro.spec.js
+++ src/components/header/_macro.spec.js
@@ -407,7 +407,7 @@ describe('macro: header', () => {
       });
     });
 
-    it('has gutterless tag if there is no button present', () => {
+    it('has gutterless class if there is no button present', () => {
       const $ = cheerio.load(
         renderComponent('header', {
           ...EXAMPLE_HEADER_BASIC,
