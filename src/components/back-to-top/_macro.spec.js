9939ce12baae2862350e25aabc19f6ea9417d2a7
Aditya_Unnithan_ONS <a_unnithan@hotmail.com>
2024-02-01 11:48:57 +0000

    Changes to CSS

---
+++ src/components/back-to-top/_macro.spec.js
@@ -0,0 +1,32 @@
+/** @jest-environment jsdom */
+
+import * as cheerio from 'cheerio';
+
+import axe from '../../tests/helpers/axe';
+import { renderComponent, templateFaker } from '../../tests/helpers/rendering';
+
+describe('macro: back-to-top', () => {
+  it('passes jest-axe checks', async () => {
+    const $ = cheerio.load(
+      renderComponent('back-to-top', {
+        description: 'Back to top',
+        anchor: 'example-target',
+      }),
+    );
+
+    const results = await axe($.html());
+    expect(results).toHaveNoViolations();
+  });
+
+  it('has a target element', async () => {
+    const $ = cheerio.load(
+      renderComponent('back-to-top', {
+        description: 'Back to top',
+        anchor: 'example-target',
+      }),
+    );
+
+    expect($('.ons-back-to-top')[0].tagName).toBe('a');
+  });
+  
+});
