2614e12c003ee0e01b89f7e5cf4dbd2a2d1bdaad
Aditya_Unnithan_ONS <a_unnithan@hotmail.com>
2024-02-01 14:20:00 +0000

    Add more tests

--- src/components/back-to-top/_macro.spec.js
+++ src/components/back-to-top/_macro.spec.js
@@ -28,4 +28,41 @@ describe('macro: back-to-top', () => {
 
     expect($('.ons-back-to-top')[0].tagName).toBe('a');
   });
+  it('has back to top link with the provided description', async () => {
+    const $ = cheerio.load(
+      renderComponent('back-to-top', {
+        description: 'Back to top',
+        anchor: 'example-target',
+      }),
+    );
+    expect($('.ons-back-to-top').text().trim()).toBe('Back to top');
+  });
+  it('has back to top link with the default description if no description provided', async () => {
+    const $ = cheerio.load(
+      renderComponent('back-to-top', {
+        anchor: 'example-target',
+      }),
+    );
+
+    expect($('.ons-back-to-top').text().trim()).toBe('Back to top');
+  });
+  it('has back to top link with the default anchor if no anchor provided', async () => {
+    const $ = cheerio.load(
+      renderComponent('back-to-top', {
+        description: 'Back to top',
+      }),
+    );
+
+    expect($('.ons-back-to-top').attr('href')).toBe('#');
+  });
+  it('has back to top link with the provided anchor', async () => {
+    const $ = cheerio.load(
+      renderComponent('back-to-top', {
+        description: 'Back to top',
+        anchor: 'example-target',
+      }),
+    );
+
+    expect($('.ons-back-to-top').attr('href')).toBe('#example-target');
+  });
 });
