367afbca57108df8ca74b81632bc692372befb4b
Aditya Unnithan_ONS <56112669+adi-unni@users.noreply.github.com>
2024-02-28 14:46:36 +0000

    ⬆️  Back To Top Link  ⬆️  (#2990)
    * Page template for length
    
    * Add macro
    
    * Add component foundations
    
    * Build stylesheet
    
    * Add additional functionality
    
    * Improved functionality
    
    * Fix border and layout
    
    * Fix resize behaviour
    
    * Changes to CSS
    
    * Add more tests
    
    * Update VR Tests
    
    * Change to full width
    
    ---------
    
    Co-authored-by: Alessio Venturini <112873190+alessioventuriniAND@users.noreply.github.com>

---
+++ src/components/back-to-top/back-to-top.dom.js
@@ -0,0 +1,12 @@
+import domready from '../../js/domready';
+
+async function backToTop() {
+  const bttElement = [...document.querySelectorAll('.ons-js-back-to-top')];
+
+  if (bttElement) {
+    const Btt = (await import('./back-to-top')).default;
+    bttElement.forEach((btn) => new Btt(btn));
+  }
+}
+
+domready(backToTop);
---
+++ src/components/back-to-top/back-to-top.js
@@ -0,0 +1,58 @@
+export default class backToTop {
+  constructor(component) {
+    this.component = component;
+    this.content = this.component.previousElementSibling;
+    this.target = document.getElementById(this.component.firstElementChild.href.split('#')[1]);
+    this.contentleft;
+    this.updateContentDetails();
+
+    this.handleScroll = this.handleScroll.bind(this);
+    window.addEventListener('scroll', this.handleScroll);
+
+    window.addEventListener('resize', () => {
+      this.setEnabled();
+      this.updateContentDetails();
+      this.handleScroll();
+    });
+
+    this.component.addEventListener('click', () => {
+      this.component.firstElementChild.blur();
+    });
+  }
+
+  handleScroll() {
+    let scrollPosition = window.scrollY + window.innerHeight;
+
+    if (this.target) {
+      scrollPosition = -this.target.getBoundingClientRect().top + window.innerHeight;
+    }
+
+    const contentRect = this.content.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+    const contentBottom = contentRect.bottom;
+
+    const stickyThreshold = windowHeight * 2;
+    if (scrollPosition > stickyThreshold && windowHeight < contentBottom) {
+      this.setSticky();
+    } else {
+      this.setEnabled();
+    }
+  }
+
+  setSticky() {
+    this.component.classList.remove('ons-back-to-top__enabled');
+    this.component.classList.add('ons-back-to-top__sticky');
+    this.component.firstElementChild.children[0].style.marginLeft = `${this.contentleft}px`;
+  }
+
+  setEnabled() {
+    this.updateContentDetails();
+    this.component.classList.remove('ons-back-to-top__sticky');
+    this.component.classList.add('ons-back-to-top__enabled');
+    this.component.firstElementChild.children[0].style.marginLeft = '';
+  }
+
+  updateContentDetails() {
+    this.contentleft = this.component.getBoundingClientRect().left;
+  }
+}
