9430c772c01fefc372a47df5a1ae42345db2e300
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2024-08-16 14:14:27 +0100

    formatting

--- src/components/video/video.spec.js
+++ src/components/video/video.spec.js
@@ -82,7 +82,7 @@ describe('script: video', () => {
             await setTestPage(
                 '/test',
                 `${renderComponent('video', EXAMPLE_VIDEO_YOUTUBE)}
-        <div class="ons-cookies-banner ons-u-db"><button class="ons-js-accept-cookies">Accept</button></div>`,
+                <div class="ons-cookies-banner ons-u-db"><button class="ons-js-accept-cookies">Accept</button></div>`,
             );
             await page.click('.ons-js-accept-cookies');
         });
@@ -114,7 +114,7 @@ describe('script: video', () => {
             await setTestPage(
                 '/test',
                 `${renderComponent('video', EXAMPLE_VIDEO_VIMEO)}
-        <div class="ons-cookies-banner ons-u-db"><button class="ons-js-accept-cookies">Accept</button></div>`,
+                <div class="ons-cookies-banner ons-u-db"><button class="ons-js-accept-cookies">Accept</button></div>`,
             );
             await page.click('.ons-js-accept-cookies');
 
