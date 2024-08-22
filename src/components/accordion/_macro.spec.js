3f9cb699d858b5344e347797e2aa51d9fc4f4dbb
Bali Birch-Lee <bali.birch-lee@ons.gov.uk>
2024-08-22 17:03:54 +0100

    refactors test document to follow FGWT

--- src/components/accordion/_macro.spec.js
+++ src/components/accordion/_macro.spec.js
@@ -5,7 +5,7 @@ import * as cheerio from 'cheerio';
 import axe from '../../tests/helpers/axe';
 import { renderComponent } from '../../tests/helpers/rendering';
 
-const EXAMPLE_ACCORDION_WITH_TWO_ITEMS = {
+const EXAMPLE_ACCORDION = {
     id: 'accordion-identifier',
     itemsList: [
         {
@@ -19,49 +19,63 @@ const EXAMPLE_ACCORDION_WITH_TWO_ITEMS = {
     ],
 };
 
-const EXAMPLE_ACCORDION = {
-    ...EXAMPLE_ACCORDION_WITH_TWO_ITEMS,
-    allButton: {
-        open: 'Open label',
-        close: 'Close label',
-    },
-};
-
-describe('macro: accordion', () => {
-    it('passes jest-axe checks', async () => {
-        const $ = cheerio.load(renderComponent('accordion', EXAMPLE_ACCORDION));
-
-        const results = await axe($.html());
-        expect(results).toHaveNoViolations();
-    });
-
-    it('has the provided `id` attribute', () => {
-        const $ = cheerio.load(renderComponent('accordion', EXAMPLE_ACCORDION_WITH_TWO_ITEMS));
-
-        expect($('.ons-accordion').attr('id')).toBe('accordion-identifier');
-    });
-
-    it('has additionally provided style classes', () => {
-        const $ = cheerio.load(
-            renderComponent('accordion', {
-                ...EXAMPLE_ACCORDION_WITH_TWO_ITEMS,
-                classes: 'extra-class another-extra-class',
-            }),
-        );
-
-        expect($('.ons-accordion').hasClass('extra-class')).toBe(true);
-        expect($('.ons-accordion').hasClass('another-extra-class')).toBe(true);
-    });
-
-    describe('item', () => {
-        it('has provided title text', () => {
-            const $ = cheerio.load(renderComponent('accordion', EXAMPLE_ACCORDION_WITH_TWO_ITEMS));
-
-            const titleText = $('.ons-details__title').first().text().trim();
-            expect(titleText).toBe('Title for item 1');
+describe('FOR: Accordion', () => {
+    describe('GIVEN: Params: required ', () => {
+        describe('WHEN: all required params are provided', () => {
+            const $ = cheerio.load(renderComponent('accordion', EXAMPLE_ACCORDION));
+            test('THEN: jest-axe checks pass', async () => {
+                const results = await axe($.html());
+                expect(results).toHaveNoViolations();
+            });
         });
-
-        it('has title with provided tag override', () => {
+    });
+    describe('GIVEN: Params: id', () => {
+        describe('WHEN: id is provided', () => {
+            const $ = cheerio.load(renderComponent('accordion', EXAMPLE_ACCORDION));
+            test('THEN: renders with provided id', () => {
+                expect($('.ons-accordion').attr('id')).toBe('accordion-identifier');
+            });
+        });
+    });
+    describe('GIVEN: Params: classes', () => {
+        describe('WHEN: additional style classes are provided', () => {
+            const $ = cheerio.load(
+                renderComponent('accordion', {
+                    ...EXAMPLE_ACCORDION,
+                    classes: 'extra-class another-extra-class',
+                }),
+            );
+            test('THEN: renders with provided classes', () => {
+                expect($('.ons-accordion').hasClass('extra-class')).toBe(true);
+                expect($('.ons-accordion').hasClass('another-extra-class')).toBe(true);
+            });
+        });
+    });
+    describe('GIVEN: Params: itemsList: AccordionItem', () => {
+        describe('WHEN: title is provided', () => {
+            const $ = cheerio.load(renderComponent('accordion', EXAMPLE_ACCORDION));
+            test('THEN: renders title with provided text', () => {
+                const titleText = $('.ons-details__title').first().text().trim();
+                expect(titleText).toBe('Title for item 1');
+            });
+        });
+        describe('WHEN: titleTag is not provided', () => {
+            const $ = cheerio.load(
+                renderComponent('accordion', {
+                    itemsList: [
+                        {
+                            title: 'Title for item 1',
+                            content: 'Content for item 1',
+                        },
+                    ],
+                }),
+            );
+            test('THEN: item title renders with default heading tag', () => {
+                const titleTag = $('.ons-details__title')[0].tagName;
+                expect(titleTag).toBe('h2');
+            });
+        });
+        describe('WHEN: titleTag is provided', () => {
             const $ = cheerio.load(
                 renderComponent('accordion', {
                     itemsList: [
@@ -73,19 +87,19 @@ describe('macro: accordion', () => {
                     ],
                 }),
             );
-
-            const titleTag = $('.ons-details__title')[0].tagName;
-            expect(titleTag).toBe('h5');
+            test('THEN: item title renders with provided heading tag', () => {
+                const titleTag = $('.ons-details__title')[0].tagName;
+                expect(titleTag).toBe('h5');
+            });
         });
-
-        it('has provided content text', () => {
-            const $ = cheerio.load(renderComponent('accordion', EXAMPLE_ACCORDION_WITH_TWO_ITEMS));
-
-            const titleText = $('.ons-details__content').first().text().trim();
-            expect(titleText).toBe('Content for item 1');
+        describe('WHEN: content is provided', () => {
+            const $ = cheerio.load(renderComponent('accordion', EXAMPLE_ACCORDION));
+            test('THEN: item content renders with provided text', () => {
+                const titleText = $('.ons-details__content').first().text().trim();
+                expect(titleText).toBe('Content for item 1');
+            });
         });
-
-        it('has additionally provided `attributes`', () => {
+        describe('WHEN: attributes are provided', () => {
             const $ = cheerio.load(
                 renderComponent('accordion', {
                     itemsList: [
@@ -99,12 +113,12 @@ describe('macro: accordion', () => {
                     ],
                 }),
             );
-
-            expect($('.ons-details').attr('a')).toBe('123');
-            expect($('.ons-details').attr('b')).toBe('456');
+            test('THEN: item renders with provided HTML attributes', () => {
+                expect($('.ons-details').attr('a')).toBe('123');
+                expect($('.ons-details').attr('b')).toBe('456');
+            });
         });
-
-        it('has additionally provided `headingAttributes`', () => {
+        describe('WHEN: headingAttributes are provided', () => {
             const $ = cheerio.load(
                 renderComponent('accordion', {
                     itemsList: [
@@ -118,12 +132,12 @@ describe('macro: accordion', () => {
                     ],
                 }),
             );
-
-            expect($('.ons-details__heading').attr('a')).toBe('123');
-            expect($('.ons-details__heading').attr('b')).toBe('456');
+            test('THEN: item header renders with provided HTML attributes', () => {
+                expect($('.ons-details__heading').attr('a')).toBe('123');
+                expect($('.ons-details__heading').attr('b')).toBe('456');
+            });
         });
-
-        it('has additionally provided `contentAttributes`', () => {
+        describe('WHEN: contentAttributes are provided', () => {
             const $ = cheerio.load(
                 renderComponent('accordion', {
                     itemsList: [
@@ -138,31 +152,37 @@ describe('macro: accordion', () => {
                     ],
                 }),
             );
-
-            expect($('.ons-details__content').attr('a')).toBe('123');
-            expect($('.ons-details__content').attr('b')).toBe('456');
+            test('THEN: item content renders with provided HTML attributes', () => {
+                expect($('.ons-details__content').attr('a')).toBe('123');
+                expect($('.ons-details__content').attr('b')).toBe('456');
+            });
         });
     });
-
-    describe('toggle all button', () => {
-        it('outputs a button with the expected class', () => {
+    describe('GIVEN: Params: allButton: AccordionButton', () => {
+        describe('WHEN: required open/close params are provided', () => {
             const $ = cheerio.load(
                 renderComponent('accordion', {
-                    ...EXAMPLE_ACCORDION_WITH_TWO_ITEMS,
+                    ...EXAMPLE_ACCORDION,
                     allButton: {
                         open: 'Open label',
                         close: 'Close label',
                     },
                 }),
             );
-
-            expect($('button.ons-accordion__toggle-all').length).toBe(1);
+            test('THEN: renders button with expected class', () => {
+                expect($('button.ons-accordion__toggle-all').length).toBe(1);
+            });
+            test('THEN: renders button with provided open text', () => {
+                expect($('.ons-accordion__toggle-all-inner').text()).toBe('Open label');
+            });
+            test('THEN: renders button with provided close text', () => {
+                expect($('button.ons-accordion__toggle-all').attr('data-close-all')).toBe('Close label');
+            });
         });
-
-        it('has additionally provided `attributes`', () => {
+        describe('WHEN: attributes are provided', () => {
             const $ = cheerio.load(
                 renderComponent('accordion', {
-                    ...EXAMPLE_ACCORDION_WITH_TWO_ITEMS,
+                    ...EXAMPLE_ACCORDION,
                     allButton: {
                         open: 'Open label',
                         close: 'Close label',
@@ -173,9 +193,10 @@ describe('macro: accordion', () => {
                     },
                 }),
             );
-
-            expect($('button.ons-accordion__toggle-all').attr('a')).toBe('123');
-            expect($('button.ons-accordion__toggle-all').attr('b')).toBe('456');
+            test('THEN: renders button with additional attributes provided', () => {
+                expect($('button.ons-accordion__toggle-all').attr('a')).toBe('123');
+                expect($('button.ons-accordion__toggle-all').attr('b')).toBe('456');
+            });
         });
     });
 });
