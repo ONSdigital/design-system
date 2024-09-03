4284cfec9091fdc4d2a1189ddd49660b78986aad
SriHV <sri.harsha.v.parupudi@ons.gov.uk>
2024-09-03 15:36:20 +0100

    commiting latest changes

--- src/components/address-output/_macro.spec.js
+++ src/components/address-output/_macro.spec.js
@@ -8,13 +8,12 @@ import { renderComponent } from '../../tests/helpers/rendering';
 import { EXAMPLE_ADDRESS_OUTPUT_FULL, EXAMPLE_ADDRESS_OUTPUT_NONE } from './_test_examples';
 
 describe('FOR: address-output', () => {
-    describe('GIVEN: Params: default', () => {
-        describe('WHEN: All params are at default state', () => {
-            const $ = cheerio.load(renderComponent('address-output', EXAMPLE_ADDRESS_OUTPUT_FULL));
+    describe('GIVEN: Params: none', () => {
+        describe('WHEN: no parameters are provided', () => {
+            const $ = cheerio.load(renderComponent('address-output', EXAMPLE_ADDRESS_OUTPUT_NONE));
 
-            test('THEN: jest-axe tests pass', async () => {
-                const results = await axe($.html());
-                expect(results).toHaveNoViolations();
+            test('THEN: renders no lines', () => {
+                expect($('.ons-address-output__lines *').length).toBe(0);
             });
         });
     });
@@ -35,12 +34,52 @@ describe('FOR: address-output', () => {
         });
     });
 
-    describe('GIVEN: Params: none', () => {
-        describe('WHEN: no parameters are provided', () => {
-            const $ = cheerio.load(renderComponent('address-output', EXAMPLE_ADDRESS_OUTPUT_NONE));
+    describe('GIVEN: Params: All params', () => {
+        describe('WHEN: All address line params are provided', () => {
+            const $ = cheerio.load(renderComponent('address-output', EXAMPLE_ADDRESS_OUTPUT_FULL));
 
-            test('THEN: renders no lines', () => {
-                expect($('.ons-address-output__lines *').length).toBe(0);
+            test('THEN: jest-axe tests pass', async () => {
+                const results = await axe($.html());
+                expect(results).toHaveNoViolations();
+            });
+
+            test('THEN: renders `unit` line correctly', () => {
+                expect($('.ons-address-output__unit').text().trim()).toBe('Unit 5');
+            });
+
+            test('THEN: renders `organisation` line correctly', () => {
+                expect($('.ons-address-output__organisation').text().trim()).toBe('Trescos');
+            });
+
+            test('THEN: renders `line1` line correctly', () => {
+                expect($('.ons-address-output__line1').text().trim()).toBe('Abingdon Road');
+            });
+
+            test('THEN: renders `line2` line correctly', () => {
+                expect($('.ons-address-output__line2').text().trim()).toBe('Goathill');
+            });
+
+            test('THEN: renders the `town` line correctly', () => {
+                expect($('.ons-address-output__town').text().trim()).toBe('Barry');
+            });
+
+            test('THEN: renders the `postcode` line correctly', () => {
+                expect($('.ons-address-output__postcode').text().trim()).toBe('AB12 6UH');
+            });
+        });
+    });
+
+    describe('GIVEN: Params: single param', () => {
+        describe('WHEN: classes are provided', () => {
+            const $ = cheerio.load(
+                renderComponent('address-output', {
+                    unit: 'Unit 5',
+                }),
+            );
+
+            test('THEN: renders with additional classes provided', async () => {
+                expect($('.ons-address-output').hasClass('extra-class')).toBe(true);
+                expect($('.ons-address-output').hasClass('another-extra-class')).toBe(true);
             });
         });
     });
