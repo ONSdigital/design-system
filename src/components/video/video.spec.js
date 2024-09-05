17d5e72b80dfd26fef51199ad62021e932131355
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2024-09-05 15:12:01 +0100

    revert

--- src/components/video/video.spec.js
+++ src/components/video/video.spec.js
@@ -43,38 +43,38 @@ describe('script: video', () => {
             });
 
             await setTestPage('/test', renderComponent('video', EXAMPLE_VIDEO_YOUTUBE));
-        });
+        }, 10000);
 
         it('should hide the placeholder content', async () => {
             const displayStyle = await page.$eval('.ons-js-video-placeholder', (node) =>
                 window.getComputedStyle(node).getPropertyValue('display'),
             );
             expect(displayStyle).toBe('none');
-        });
+        }, 10000);
 
         it('should show the iframe', async () => {
             const displayStyle = await page.$eval('.ons-js-video-iframe', (node) =>
                 window.getComputedStyle(node).getPropertyValue('display'),
             );
             expect(displayStyle).toBe('block');
-        });
+        }, 10000);
 
         it('should add the correct modifier class', async () => {
             const hasClass = await page.$eval('.ons-js-video', (node) => node.classList.contains('ons-video--hasIframe'));
             expect(hasClass).toBe(true);
-        });
+        }, 10000);
 
         it('should not add dnt to YouTube videos', async () => {
             const src = await page.$eval('.ons-js-video-iframe', (node) => node.getAttribute('src'));
             expect(src.includes('?dnt=1')).toBe(false);
-        });
+        }, 10000);
 
         it('should add dnt to Vimeo videos', async () => {
             await setTestPage('/test', renderComponent('video', EXAMPLE_VIDEO_VIMEO));
 
             const src = await page.$eval('.ons-js-video-iframe', (node) => node.getAttribute('src'));
             expect(src.includes('?dnt=1')).toBe(true);
-        });
+        }, 10000);
     });
 
     describe('when cookies are accepted via banner', () => {
@@ -85,30 +85,31 @@ describe('script: video', () => {
                 <div class="ons-cookies-banner ons-u-db"><button class="ons-js-accept-cookies">Accept</button></div>`,
             );
             await page.click('.ons-js-accept-cookies');
-        });
+        }, 10000);
 
         it('should hide the placeholder content', async () => {
             const displayStyle = await page.$eval('.ons-js-video-placeholder', (node) =>
                 window.getComputedStyle(node).getPropertyValue('display'),
             );
             expect(displayStyle).toBe('none');
-        });
+        }, 10000);
 
         it('should show the iframe', async () => {
             const displayStyle = await page.$eval('.ons-js-video-iframe', (node) =>
                 window.getComputedStyle(node).getPropertyValue('display'),
             );
             expect(displayStyle).toBe('block');
-        });
+        }, 10000);
 
         it('should add the correct modifier class', async () => {
             const hasClass = await page.$eval('.ons-js-video', (node) => node.classList.contains('ons-video--hasIframe'));
             expect(hasClass).toBe(true);
-        });
+        }, 10000);
+
         it('should not add dnt to YouTube videos', async () => {
             const src = await page.$eval('.ons-js-video-iframe', (node) => node.getAttribute('src'));
             expect(src.includes('?dnt=1')).toBe(false);
-        });
+        }, 10000);
 
         it('should add dnt to Vimeo videos', async () => {
             await setTestPage(
@@ -120,6 +121,6 @@ describe('script: video', () => {
 
             const src = await page.$eval('.ons-js-video-iframe', (node) => node.getAttribute('src'));
             expect(src.includes('?dnt=1')).toBe(true);
-        });
+        }, 10000);
     });
 });
