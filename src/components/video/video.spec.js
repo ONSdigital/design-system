6cc8ef345627df51ef4358e75a926fcb5ee9051f
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2024-09-06 11:24:55 +0100

    test

--- src/components/video/video.spec.js
+++ src/components/video/video.spec.js
@@ -69,14 +69,14 @@ describe('script: video', () => {
             const src = await page.$eval('.ons-js-video-iframe', (node) => node.getAttribute('src'));
             expect(src.includes('?dnt=1')).toBe(false);
         }, 10000);
-
-        it('should add dnt to Vimeo videos', async () => {
-            await setTestPage('/test', renderComponent('video', EXAMPLE_VIDEO_VIMEO));
-
-            const src = await page.$eval('.ons-js-video-iframe', (node) => node.getAttribute('src'));
-            expect(src.includes('?dnt=1')).toBe(true);
-        }, 10000);
     });
+
+    it('should add dnt to Vimeo videos', async () => {
+        await setTestPage('/test', renderComponent('video', EXAMPLE_VIDEO_VIMEO));
+
+        const src = await page.$eval('.ons-js-video-iframe', (node) => node.getAttribute('src'));
+        expect(src.includes('?dnt=1')).toBe(true);
+    }, 10000);
 
     describe('when cookies are accepted via banner', () => {
         beforeEach(async () => {
