163a16052cc10e975fd60b4eabfd6e22daade8b7
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2024-09-12 14:56:47 +0100

    test

--- backstop.config.js
+++ backstop.config.js
@@ -40,5 +40,5 @@ module.exports = {
     },
     report: process.env.RUNNING_IN_CI === 'true' ? [] : ['browser'],
     dockerCommandTemplate:
-        'docker run --rm -i --user $(id -u):$(id -g) --mount type=bind,source="{cwd}",target=/src backstopjs/backstopjs:{version} {backstopCommand} {args}',
+        'ENV XDG_CONFIG_HOME=/tmp/.chromium ENV XDG_CACHE_HOME=/tmp/.chromium docker run --rm -i --user $(id -u):$(id -g) --mount type=bind,source="{cwd}",target=/src backstopjs/backstopjs:{version} {backstopCommand} {args}',
 };
