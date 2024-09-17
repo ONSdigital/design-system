c5578b298cb45e949c1092a4c6e0c62fbb139e87
Precious Onyenaucheya <86783201+precious-onyenaucheya-ons@users.noreply.github.com>
2024-09-17 10:59:05 +0100

    Update src/components/autosuggest/_macro.spec.js
    Co-authored-by: rmccar <42928680+rmccar@users.noreply.github.com>

--- src/components/autosuggest/_macro.spec.js
+++ src/components/autosuggest/_macro.spec.js
@@ -94,7 +94,7 @@ describe('FOR: autosuggest', () => {
                     allowMultiple: false,
                 }),
             );
-            test('THEN: it does not have the data-allow-multiple attribute', () => {
+            test('THEN: it does not have the data-allow-multiple attribute on the container element', () => {
                 expect($('.ons-autosuggest').attr('data-allow-multiple')).toBeUndefined();
             });
         });
