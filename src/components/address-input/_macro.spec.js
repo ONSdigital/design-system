5fe61f1ef59480e1d454c77de6dd8ded80f9c100
Precious Onyenaucheya <86783201+precious-onyenaucheya-ons@users.noreply.github.com>
2024-09-19 11:11:23 +0100

    Update src/components/address-input/_macro.spec.js
    Co-authored-by: rmccar <42928680+rmccar@users.noreply.github.com>

--- src/components/address-input/_macro.spec.js
+++ src/components/address-input/_macro.spec.js
@@ -6,7 +6,7 @@ import axe from '../../tests/helpers/axe';
 import { renderComponent, templateFaker } from '../../tests/helpers/rendering';
 import { EXAMPLE_AUTOSUGGEST_ADDRESS_MINIMAL, EXAMPLE_MANUAL_INPUT_FIELDS } from './_test-examples';
 
-describe('FOR: address-input', () => {
+describe('FOR: Macro: Address-input', () => {
     describe('GIVEN: Params: required', () => {
         describe('WHEN: all required params are provided', () => {
             const $ = cheerio.load(renderComponent('address-input', EXAMPLE_AUTOSUGGEST_ADDRESS_MINIMAL));
