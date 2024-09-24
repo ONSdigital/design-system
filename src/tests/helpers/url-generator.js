69baf73573a44f885e18e9ffb9e105574a5f4336
Alessio <alessio.venturini@and.digital>
2024-09-24 15:34:37 +0100

    testing new url

--- src/tests/helpers/url-generator.js
+++ src/tests/helpers/url-generator.js
@@ -3,7 +3,7 @@ const util = require('util');
 const { glob } = require('glob');
 const readdir = util.promisify(fs.readdir);
 
-const testURL = `http://0.0.0.0:3010`;
+const testURL = `http://localhost:3010`;
 
 export default async () => {
     let urls = [];
