28f266ea804bf84e957ad559591b860e889a0521
Precious Onyenaucheya <precious.onyenaucheya@ons.gov.uk>
2024-09-30 20:08:13 +0100

    address PR comments

--- src/components/hero/_macro.spec.js
+++ src/components/hero/_macro.spec.js
@@ -37,7 +37,7 @@ describe('macro: hero', () => {
     it('has expected `title`', () => {
         const $ = cheerio.load(renderComponent('hero', EXAMPLE_HERO));
 
-        const title = $('h1').html().trim();
+        const title = $('.ons-hero__title').html().trim();
         expect(title).toBe('Hero title');
     });
 
