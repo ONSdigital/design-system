5598fd76ae5e955214284775bc4bc0e9ade17112
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2024-11-15 14:49:24 +0000

    rename table of contents files

--- src/components/table-of-contents/table-of-contents.dom.js
+++ src/components/table-of-contents/table-of-contents.dom.js
@@ -4,7 +4,7 @@ async function toc() {
     const toc = [...document.querySelectorAll('.ons-js-toc-container')];
 
     if (toc.length) {
-        const Toc = (await import('./toc')).default;
+        const Toc = (await import('./table-of-contents')).default;
 
         toc.forEach((component) => new Toc(component));
     }
