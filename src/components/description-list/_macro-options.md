22c7b5f5f3bb5bb5ad57a3814ca23b4c6afae2bb
Precious Onyenaucheya <precious.onyenaucheya@ons.gov.uk>
2024-11-28 13:47:59 +0000

    update as per PR comments

--- src/components/description-list/_macro-options.md
+++ src/components/description-list/_macro-options.md
@@ -1,12 +1,12 @@
-| Name                 | Type          | Required | Description                                                                                                    |
-| -------------------- | ------------- | -------- | -------------------------------------------------------------------------------------------------------------- |
-| id                   | string        | false    | The HTML `id` for the description list (`<dl>`)                                                                |
-| classes              | string        | false    | Classes to apply to description list (`<dl>`)                                                                  |
-| descriptionListLabel | string        | false    | Sets the HTML `title` and `aria-label` attributes to provide additional information about the description list |
-| termCol              | number        | true     | The number of grid columns used for the `<dt>` elements above medium breakpoint                                |
-| descriptionCol       | number        | true     | The number of grid columns used for the `<dd>` elements above medium breakpoint                                |
-| itemsList            | array`<Item>` | true     | Settings for the terms and descriptions of the [description list items](#item)                                 |
-| horizontal           | boolean       | false    | Set to "true" to align the description list horizontally                                                       |
+| Name                 | Type            | Required | Description                                                                                                    |
+| -------------------- | --------------- | -------- | -------------------------------------------------------------------------------------------------------------- |
+| id                   | string          | false    | The HTML `id` for the description list (`<dl>`)                                                                |
+| classes              | string          | false    | Classes to apply to description list (`<dl>`)                                                                  |
+| descriptionListLabel | string          | false    | Sets the HTML `title` and `aria-label` attributes to provide additional information about the description list |
+| termCol              | number          | true     | The number of grid columns used for the `<dt>` elements above medium breakpoint                                |
+| descriptionCol       | number          | true     | The number of grid columns used for the `<dd>` elements above medium breakpoint                                |
+| itemsList            | array`<Item>`   | true     | Settings for the terms and descriptions of the [description list items](#item)                                 |
+| variants             | array or string | false    | An array of values or single value (string) to adjust the component using available variants:“inline”          |
 
 ## Item
 
--- src/components/description-list/_macro.spec.js
+++ src/components/description-list/_macro.spec.js
@@ -3,144 +3,175 @@
 import * as cheerio from 'cheerio';
 import axe from '../../tests/helpers/axe';
 import { renderComponent } from '../../tests/helpers/rendering';
-import { EXAMPLE_DESCRIPTION_LIST_FULL, EXAMPLE_DESCRIPTION_LIST_MINIMAL } from './_test-examples';
 
-describe('FOR: Macro: Description List', () => {
-    describe('GIVEN: Params: required', () => {
-        describe('WHEN: all required params are provided', () => {
-            const $ = cheerio.load(renderComponent('description-list', EXAMPLE_DESCRIPTION_LIST_MINIMAL));
-            test('THEN: jest-axe checks pass', async () => {
-                const results = await axe($.html());
-                expect(results).toHaveNoViolations();
-            });
-        });
+const EXAMPLE_DESCRIPTION_LIST_FULL = {
+    id: 'example-id',
+    classes: 'ons-u-mb-no',
+    descriptionListLabel: 'This is an example of the description list component',
+    termCol: 2,
+    descriptionCol: 10,
+    itemsList: [
+        {
+            term: 'Survey:',
+            descriptions: [
+                {
+                    id: 'description-1',
+                    description: 'Bricks & Blocks',
+                },
+            ],
+        },
+        {
+            term: 'RU Refs:',
+            descriptions: [
+                {
+                    id: 'description-2',
+                    description: '49900000118',
+                },
+                {
+                    id: 'description-3',
+                    description: '49300005832',
+                },
+            ],
+        },
+    ],
+};
+
+const EXAMPLE_DESCRIPTION_LIST_MINIMAL = {
+    itemsList: [
+        {
+            term: 'Survey:',
+            descriptions: [
+                {
+                    description: 'Bricks & Blocks',
+                },
+            ],
+        },
+        {
+            term: 'RU Refs:',
+            descriptions: [
+                {
+                    description: '49900000118',
+                },
+                {
+                    description: '49300005832',
+                },
+            ],
+        },
+    ],
+};
+
+describe('macro: description-list', () => {
+    it('passes jest-axe checks when all parameters are provided', async () => {
+        const $ = cheerio.load(renderComponent('description-list', EXAMPLE_DESCRIPTION_LIST_FULL));
+
+        const results = await axe($.html());
+        expect(results).toHaveNoViolations();
     });
 
-    describe('GIVEN: Params: all', () => {
-        describe('WHEN: all params are provided', () => {
-            const $ = cheerio.load(renderComponent('description-list', EXAMPLE_DESCRIPTION_LIST_FULL));
-            test('THEN: jest-axe checks pass', async () => {
-                const results = await axe($.html());
-                expect(results).toHaveNoViolations();
-            });
-        });
+    it('passes jest-axe checks when minimal parameters are provided', async () => {
+        const $ = cheerio.load(renderComponent('description-list', EXAMPLE_DESCRIPTION_LIST_MINIMAL));
+
+        const results = await axe($.html());
+        expect(results).toHaveNoViolations();
     });
 
-    describe('GIVEN: Params: id', () => {
-        describe('WHEN: id is provided', () => {
-            const $ = cheerio.load(
-                renderComponent('description-list', {
-                    ...EXAMPLE_DESCRIPTION_LIST_MINIMAL,
-                    id: 'example-id',
-                }),
-            );
-            test('THEN: renders with provided id', () => {
-                expect($('#example-id').length).toBe(1);
-            });
-        });
+    it('has the provided `id` attribute', () => {
+        const $ = cheerio.load(
+            renderComponent('description-list', {
+                ...EXAMPLE_DESCRIPTION_LIST_MINIMAL,
+                id: 'example-id',
+            }),
+        );
+
+        expect($('#example-id').length).toBe(1);
     });
 
-    describe('GIVEN: Params: classes', () => {
-        describe('WHEN: additional style classes are provided', () => {
-            const $ = cheerio.load(
-                renderComponent('description-list', {
-                    ...EXAMPLE_DESCRIPTION_LIST_MINIMAL,
-                    classes: 'extra-class another-extra-class',
-                }),
-            );
-            test('THEN: renders with provided classes', () => {
-                expect($('.ons-description-list').hasClass('extra-class')).toBe(true);
-                expect($('.ons-description-list').hasClass('another-extra-class')).toBe(true);
-            });
-        });
+    it('has provided variant style class when variant is provided', () => {
+        const $ = cheerio.load(
+            renderComponent('list', {
+                ...EXAMPLE_DESCRIPTION_LIST_MINIMAL,
+                variants: 'inline',
+            }),
+        );
+
+        expect($('.ons-description-list').hasClass('ons-description-list--horizontal')).toBe(true);
     });
 
-    describe('GIVEN: Params: descriptionListLabel', () => {
-        describe('WHEN: descriptionListLabel is provided', () => {
-            const $ = cheerio.load(
-                renderComponent('description-list', {
-                    ...EXAMPLE_DESCRIPTION_LIST_MINIMAL,
-                    descriptionListLabel: 'This is an example of the description list component',
-                }),
-            );
-            test('THEN: renders with title and aria-label attributes', () => {
-                expect($('.ons-description-list').attr('title')).toBe('This is an example of the description list component');
-                expect($('.ons-description-list').attr('aria-label')).toBe('This is an example of the description list component');
-            });
-        });
+    it('has additionally provided style classes', () => {
+        const $ = cheerio.load(
+            renderComponent('description-list', {
+                ...EXAMPLE_DESCRIPTION_LIST_MINIMAL,
+                classes: 'extra-class another-extra-class',
+            }),
+        );
+
+        expect($('.ons-description-list').hasClass('extra-class')).toBe(true);
+        expect($('.ons-description-list').hasClass('another-extra-class')).toBe(true);
     });
 
-    describe('GIVEN: Params: horizontal', () => {
-        describe('WHEN: horizontal is true', () => {
-            const $ = cheerio.load(
-                renderComponent('description-list', {
-                    ...EXAMPLE_DESCRIPTION_LIST_MINIMAL,
-                    horizontal: true,
-                }),
-            );
-            test('THEN: renders with the horizontal class', () => {
-                expect($('.ons-description-list').hasClass('ons-description-list--horizontal')).toBe(true);
-            });
-        });
+    it('outputs `title` and `aria-label` attributes when `descriptionListLabel` is provided', () => {
+        const $ = cheerio.load(
+            renderComponent('description-list', {
+                ...EXAMPLE_DESCRIPTION_LIST_MINIMAL,
+                descriptionListLabel: 'This is an example of the description list component',
+            }),
+        );
+
+        expect($('.ons-description-list').attr('title')).toBe('This is an example of the description list component');
+        expect($('.ons-description-list').attr('aria-label')).toBe('This is an example of the description list component');
     });
 
-    describe('GIVEN: Params: termCol', () => {
-        describe('WHEN: termCol is provided', () => {
-            test.each([
-                [1, 'ons-col-1\\@m'],
-                [4, 'ons-col-4\\@m'],
-            ])('THEN: applies the correct termCol class (%i -> %s)', (termCol, expectedClass) => {
-                const $ = cheerio.load(
-                    renderComponent('description-list', {
-                        ...EXAMPLE_DESCRIPTION_LIST_MINIMAL,
-                        termCol,
-                    }),
-                );
-                expect($(`.ons-description-list__term.${expectedClass}`).length).toBe(2);
-            });
-        });
+    it('outputs list items as expected', () => {
+        const $ = cheerio.load(renderComponent('description-list', EXAMPLE_DESCRIPTION_LIST_FULL));
+
+        const $listElements = $('.ons-description-list__term, .ons-description-list__value');
+
+        expect($listElements[0].tagName).toBe('dt');
+        expect($($listElements[0]).text()).toBe('Survey:');
+
+        expect($listElements[1].tagName).toBe('dd');
+        expect($($listElements[1]).attr('id')).toBe('description-1');
+        expect($($listElements[1]).text()).toBe('Bricks & Blocks');
+
+        expect($listElements[2].tagName).toBe('dt');
+        expect($($listElements[2]).text()).toBe('RU Refs:');
+
+        expect($listElements[3].tagName).toBe('dd');
+        expect($($listElements[3]).attr('id')).toBe('description-2');
+        expect($($listElements[3]).text()).toBe('49900000118');
+
+        expect($listElements[4].tagName).toBe('dd');
+        expect($($listElements[4]).attr('id')).toBe('description-3');
+        expect($($listElements[4]).text()).toBe('49300005832');
     });
 
-    describe('GIVEN: Params: descriptionCol', () => {
-        describe('WHEN: descriptionCol is provided', () => {
-            test.each([
-                [1, 'ons-col-1\\@m'],
-                [4, 'ons-col-4\\@m'],
-            ])('THEN: applies the correct descriptionCol class (%i -> %s)', (descriptionCol, expectedClass) => {
-                const $ = cheerio.load(
-                    renderComponent('description-list', {
-                        ...EXAMPLE_DESCRIPTION_LIST_MINIMAL,
-                        descriptionCol,
-                    }),
-                );
-                expect($(`.ons-description-list__value.${expectedClass}`).length).toBe(3);
-            });
-        });
+    it.each([
+        [1, 'ons-col-1\\@m'],
+        [4, 'ons-col-4\\@m'],
+    ])('applies class for the provided `termCol` (%i -> %s)', (termCol, expectedClass) => {
+        const $ = cheerio.load(
+            renderComponent('description-list', {
+                ...EXAMPLE_DESCRIPTION_LIST_MINIMAL,
+                termCol,
+            }),
+        );
+
+        const $termElements = $(`.ons-description-list__term.${expectedClass}`);
+        expect($termElements.length).toBe(2);
     });
 
-    describe('GIVEN: Params: itemsList', () => {
-        describe('WHEN: itemsList is provided', () => {
-            const $ = cheerio.load(renderComponent('description-list', EXAMPLE_DESCRIPTION_LIST_FULL));
-            test('THEN: renders list items correctly', () => {
-                const $listElements = $('.ons-description-list__term, .ons-description-list__value');
-                expect($listElements[0].tagName).toBe('dt');
-                expect($($listElements[0]).text().trim()).toBe('Survey:');
+    it.each([
+        [1, 'ons-col-1\\@m'],
+        [4, 'ons-col-4\\@m'],
+    ])('applies class for the provided `descriptionCol` (%i -> %s)', (descriptionCol, expectedClass) => {
+        const $ = cheerio.load(
+            renderComponent('description-list', {
+                ...EXAMPLE_DESCRIPTION_LIST_MINIMAL,
+                descriptionCol,
+            }),
+        );
 
-                expect($listElements[1].tagName).toBe('dd');
-                expect($($listElements[1]).attr('id')).toBe('description-1');
-                expect($($listElements[1]).text().trim()).toBe('Bricks & Blocks');
-
-                expect($listElements[2].tagName).toBe('dt');
-                expect($($listElements[2]).text().trim()).toBe('RU Refs:');
-
-                expect($listElements[3].tagName).toBe('dd');
-                expect($($listElements[3]).attr('id')).toBe('description-2');
-                expect($($listElements[3]).text().trim()).toBe('49900000118');
-
-                expect($listElements[4].tagName).toBe('dd');
-                expect($($listElements[4]).attr('id')).toBe('description-3');
-                expect($($listElements[4]).text().trim()).toBe('49300005832');
-            });
-        });
+        const $valueElements = $(`.ons-description-list__value.${expectedClass}`);
+        expect($valueElements.length).toBe(3);
     });
 });
