0cfcf9a00f3f2b5472134e79f4179c788dd88847
admilne <andrew.milne@ext.ons.gov.uk>
2024-12-13 17:14:46 +0000

    Ensure nested radios and checkboxes work as expected and improve code readability

--- src/components/radios/radio-with-fieldset.js
+++ src/components/radios/radio-with-fieldset.js
@@ -1,28 +1,32 @@
 export default class RadioWithFieldset {
     constructor(context) {
         this.context = context;
-        this.radios = [...context.closest('.ons-radios__items').querySelectorAll('.ons-js-radio')];
-        this.childInputs = [...this.context.querySelectorAll('input')];
-        this.selectAllChildrenInput = this.context.querySelector('.ons-js-select-all-children');
+        this.outerInputs = [
+            ...context.closest('.ons-radios__items').querySelectorAll('input:not(.ons-js-other-fieldset-radio input):not(.ons-js-other)'),
+        ];
+        this.parentInput = this.context.querySelector('.ons-js-other');
+        this.childInputs = [...this.context.querySelectorAll('.ons-js-other-fieldset-radio input')];
+        this.shouldSelectAllChildren = this.context.querySelector('.ons-js-select-all-children') !== null;
 
-        if (this.selectAllChildrenInput) {
-            this.selectAllChildrenInput.addEventListener('change', this.checkChildInputsOnSelect.bind(this));
-        } else {
-            this.radios.forEach((radio) => radio.addEventListener('change', this.uncheckChildInputsOnDeselect.bind(this)));
-        }
+        this.outerInputs.forEach((radio) => radio.addEventListener('change', this.uncheckAllChildInputs.bind(this)));
+        this.childInputs.forEach((radio) => radio.addEventListener('change', this.checkParentRadioInput.bind(this)));
+        this.parentInput.addEventListener('change', this.handleParentInputChange.bind(this));
     }
 
-    checkChildInputsOnSelect() {
+    uncheckAllChildInputs() {
         this.childInputs.forEach((input) => {
-            input.checked = this.selectAllChildrenInput.checked === true ? true : false;
+            input.checked = false;
         });
     }
 
-    uncheckChildInputsOnDeselect() {
-        const isOther = this.radios.find((radio) => radio.classList.contains('ons-js-other'));
-        if (isOther && isOther.checked === false) {
+    checkParentRadioInput() {
+        this.parentInput.checked = true;
+    }
+
+    handleParentInputChange() {
+        if (this.shouldSelectAllChildren) {
             this.childInputs.forEach((input) => {
-                input.checked = false;
+                input.checked = true;
             });
         }
     }
