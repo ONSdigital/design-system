8a997524c594cda25120eca4648c59c7a912bd0c
SriHV <sri.harsha.v.parupudi@ons.gov.uk>
2025-02-12 12:31:54 +0000

    changes as per Joe's commnets

--- src/components/hero/_macro.spec.js
+++ src/components/hero/_macro.spec.js
@@ -112,6 +112,13 @@ describe('macro: hero', () => {
         expect($('.ons-hero__badge svg title').text().trim()).toBe('Offical Statistics Badge');
     });
 
+    it('outputs the Census 2021 Logo when censusLogo is set to true', () => {
+        const $ = cheerio.load(renderComponent('hero', { ...EXAMPLE_HERO, censusLogo: true }));
+
+        expect($('.ons-hero__census-logo').length).toBe(1);
+        expect($('.ons-hero__census-logo svg title').text().trim()).toBe('Census 2021 Logo');
+    });
+
     it('renders curved gradient when variant is `grey`', () => {
         const $ = cheerio.load(renderComponent('hero', { ...EXAMPLE_HERO, variants: 'grey' }));
         expect($('.ons-hero--grey').length).toBe(1);
