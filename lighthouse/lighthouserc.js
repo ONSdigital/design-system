246871335cc7764faeb0212714afeaec9c7b8974
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2025-02-14 15:36:20 +0000

    test lighthouse

--- lighthouse/lighthouserc.js
+++ lighthouse/lighthouserc.js
@@ -9,9 +9,33 @@ module.exports = {
             },
         },
         assert: {
-            assertions: {
-                'categories:accessibility': ['warn', { minScore: 1 }],
-            },
+            assertMatrix: [
+                {
+                    matchingUrlPattern: 'components/radios/example-radios-with-revealed.*',
+                    assertions: {
+                        'categories:accessibility': ['error', { minScore: 0.91 }],
+                    },
+                },
+                {
+                    matchingUrlPattern: 'components/radios/example-radios-with-clear-button.*',
+                    assertions: {
+                        'categories:accessibility': ['error', { minScore: 0.94 }],
+                    },
+                },
+                {
+                    matchingUrlPattern: 'patterns/feedback/example-feedback-form.*|patterns/correct-errors/example-errors-proto.*',
+                    assertions: {
+                        'categories:accessibility': ['error', { minScore: 0.94 }],
+                    },
+                },
+                {
+                    matchingUrlPattern:
+                        '^(?!.*components/radios/example-radios-with-revealed.*|.*components/radios/example-radios-with-clear-button.*|.*patterns/correct-errors/example-errors-proto.*|.*patterns/feedback/example-feedback-form.*).*',
+                    assertions: {
+                        'categories:accessibility': ['error', { minScore: 1 }],
+                    },
+                },
+            ],
         },
         upload: {
             target: 'temporary-public-storage',
