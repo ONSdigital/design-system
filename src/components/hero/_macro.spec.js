a972fdd5657300e03dcca2b533a9b9b165db4506
SriHV <sri.harsha.v.parupudi@ons.gov.uk>
2025-02-20 12:59:23 +0000

    changes as per comments

--- src/components/hero/_macro.spec.js
+++ src/components/hero/_macro.spec.js
@@ -105,18 +105,30 @@ describe('macro: hero', () => {
         expect(content).toBe('Topic Text');
     });
 
-    it('outputs the official statistics badge when officialStatisticsBadge is set to true and variants is `grey`', () => {
+    it('outputs the official statistics badge when officialStatisticsBadge is set to true for `grey` variant', () => {
         const $ = cheerio.load(renderComponent('hero', { ...EXAMPLE_HERO, variants: 'grey', officialStatisticsBadge: true }));
 
         expect($('.ons-hero__badge').length).toBe(1);
         expect($('.ons-hero__badge svg title').text().trim()).toBe('Offical Statistics Badge');
     });
 
-    it('outputs the Census 2021 Logo when censusLogo is set to true and variants is `grey`', () => {
+    it('outputs the Census 2021 Logo when censusLogo is set to true for `grey` variant', () => {
         const $ = cheerio.load(renderComponent('hero', { ...EXAMPLE_HERO, variants: 'grey', censusLogo: true }));
 
         expect($('.ons-hero__census-logo').length).toBe(1);
         expect($('.ons-hero__census-logo svg title').text().trim()).toBe('Census 2021 Logo');
+    });
+
+    it('does not render the official statistics badge when officialStatisticsBadge is set to true for all variants except `grey`', () => {
+        const $ = cheerio.load(renderComponent('hero', { ...EXAMPLE_HERO, censusLogo: true }));
+
+        expect($('.ons-hero__badge').length).toBe(0);
+    });
+
+    it('does not render the Census 2021 Logo when censusLogo is set to true for all variants except `grey`', () => {
+        const $ = cheerio.load(renderComponent('hero', { ...EXAMPLE_HERO, censusLogo: true }));
+
+        expect($('.ons-hero__census-logo').length).toBe(0);
     });
 
     it('renders curved gradient when variant is `grey`', () => {
