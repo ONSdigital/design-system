c705b641e2345058ef100e7221897cf82e7f71fc
Helen C <helen.chapman@torchbox.com>
2025-02-24 15:00:29 +0000

    Remove any values in the chart options constants variables that aren't used

--- src/components/charts/specific-chart-options.js
+++ src/components/charts/specific-chart-options.js
@@ -6,11 +6,7 @@ class SpecificChartOptions {
             // Alternate theme colours from https://service-manual.ons.gov.uk/data-visualisation/colours/using-colours-in-charts
             alternateTheme: ['#206095', '#27A0CC', '#871A5B', '#A8BD3A', '#F66068'],
             labelColor: '#414042',
-            axisLabelColor: '#707071',
-            gridLineColor: '#d9d9d9',
-            zeroLineColor: '#b3b3b3',
             // Responsive font sizes
-            mobileFontSize: '0.875rem',
             desktopFontSize: '1rem',
         };
 
