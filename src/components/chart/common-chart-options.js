e1677829b45e874510dccdb2bd931ed1f9df039c
Precious Onyenaucheya <precious.onyenaucheya@ons.gov.uk>
2025-03-14 12:45:32 +0000

    update fonts to match data viz guide

--- src/components/chart/common-chart-options.js
+++ src/components/chart/common-chart-options.js
@@ -5,8 +5,8 @@ class CommonChartOptions {
             gridLineColor: '#d9d9d9',
             zeroLineColor: '#b3b3b3',
             // Responsive font sizes
-            mobileFontSize: '0.875rem',
-            desktopFontSize: '1.05rem',
+            mobileFontSize: '0.75rem',
+            desktopFontSize: '0.875rem',
         };
 
         this.options = {
