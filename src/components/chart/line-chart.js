98105df974a86ade88a5e74fce3c97ae58abff1d
Helen C <helen.chapman@torchbox.com>
2025-03-20 14:19:45 +0000

    A few fixes after the merge

--- src/components/chart/line-chart.js
+++ src/components/chart/line-chart.js
@@ -1,6 +1,11 @@
 class LineChart {
     getLineChartOptions = () => {
         return {
+            legend: {
+                // Specific legend symbol width and height for line charts
+                symbolWidth: 20,
+                symbolHeight: 3,
+            },
             plotOptions: {
                 line: {
                     lineWidth: 3,
@@ -19,22 +24,21 @@ class LineChart {
 
     updateLastPointMarker = (currentChart) => {
         currentChart.series.forEach((series) => {
-            const points = series.points;
-            if (points && points.length > 0) {
-                // Show only the last point marker
-                const lastPoint = points[points.length - 1];
-                lastPoint.update(
-                    {
-                        marker: {
-                            enabled: true,
-                            radius: 4,
-                            symbol: 'circle',
-                            fillColor: series.color,
-                            lineWidth: 0,
+            // If markers are disabled, hide them all but the last point marker
+            if (series.options.marker.enabled.enabled === undefined) {
+                const points = series.points;
+                if (points && points.length > 0) {
+                    // Show only the last point marker
+                    const lastPoint = points[points.length - 1];
+                    lastPoint.update(
+                        {
+                            marker: {
+                                enabled: true,
+                            },
                         },
-                    },
-                    false,
-                );
+                        false,
+                    );
+                }
             }
         });
     };
