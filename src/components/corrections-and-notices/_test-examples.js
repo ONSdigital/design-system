dd1de8bacc14e5f9aeab20dac29649975f5faa06
SriHV <123635670+SriHV@users.noreply.github.com>
2025-03-26 16:11:55 +0000

    Update src/components/corrections-and-notices/_test-examples.js
    Co-authored-by: janehughes <8680557+janehughes@users.noreply.github.com>

--- src/components/corrections-and-notices/_test-examples.js
+++ src/components/corrections-and-notices/_test-examples.js
@@ -1,7 +1,7 @@
 export const EXAMPLE_CORRECTIONS = {
     corrections: [
         {
-            date: '22nd Janauary 2025,4:30PM',
+            date: '22nd January 2025, 4:30PM',
             text: 'description1',
             url: '#0',
         },
