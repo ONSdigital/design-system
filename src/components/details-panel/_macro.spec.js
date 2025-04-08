e9e603e0ce33445eaacdf6f4db26a267c592ab52
SriHV <sri.harsha.v.parupudi@ons.gov.uk>
2025-04-08 15:30:27 +0100

    changed test names

--- src/components/details-panel/_macro.spec.js
+++ src/components/details-panel/_macro.spec.js
@@ -7,7 +7,7 @@ import { EXAMPLE_DETAILS_PANEL } from './_test-examples';
 describe('FOR: Macro: Details Panel', () => {
     describe('GIVEN: Params: id', () => {
         describe('WHEN: id is provided', () => {
-            test('THEN: has the provided id attribute', () => {
+            test('THEN: the id attribute has the provided id', () => {
                 const $ = cheerio.load(renderComponent('details-panel', EXAMPLE_DETAILS_PANEL));
                 expect($('.ons-details-panel').attr('id')).toBe('example-id');
             });
@@ -17,7 +17,7 @@ describe('FOR: Macro: Details Panel', () => {
     describe('GIVEN: Params: title', () => {
         describe('WHEN: title param is provided', () => {
             const $ = cheerio.load(renderComponent('details-panel', EXAMPLE_DETAILS_PANEL));
-            test('THEN: banner has the correct titles', () => {
+            test('THEN: banner has the correct title', () => {
                 const contentTitle = $('.ons-details-panel__banner-title').text().replace(/\s+/g, ' ').trim();
                 expect(contentTitle).toBe('Correction and Notice');
             });
@@ -31,27 +31,27 @@ describe('FOR: Macro: Details Panel', () => {
                 const results = await axe($.html());
                 expect(results).toHaveNoViolations();
             });
-            test('THEN: has correct titles in content', () => {
+            test('THEN: items have correct titles', () => {
                 const contentTitle0 = $('.ons-details-panel__content-heading').eq(0).text().trim();
                 const contentTitle1 = $('.ons-details-panel__content-heading').eq(1).text().trim();
                 expect(contentTitle0).toBe('Correction');
                 expect(contentTitle1).toBe('Notice');
             });
-            test('THEN: has correct dates', () => {
+            test('THEN: items have correct dates', () => {
                 const dateText0 = $('.ons-details-panel__content-date').eq(0).text().trim();
                 const dateText1 = $('.ons-details-panel__content-date').eq(1).text().trim();
 
                 expect(dateText0).toBe('22nd January 2025, 4:30PM');
                 expect(dateText1).toBe('22nd January 2025');
             });
-            test('THEN: has correct description text', () => {
+            test('THEN: items have correct description text', () => {
                 const contentdescriptionText0 = $('.ons-details-panel__content-text').eq(0).text().trim();
                 const contentdescriptionText1 = $('.ons-details-panel__content-text').eq(1).text().trim();
 
                 expect(contentdescriptionText0).toBe('description1');
                 expect(contentdescriptionText1).toBe('description2');
             });
-            test('THEN: correction has a valid link and text', () => {
+            test('THEN: correction item has a valid link and text', () => {
                 const correctionLinkText = $('.ons-details-panel__content-url').eq(0).text().trim();
                 const correctionLinkUrl = $('.ons-details-panel__content-url').eq(0).attr('href');
 
