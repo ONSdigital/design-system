cf39c3f67413a95a2f18d6a451a8ffdbcaaac670
Precious Onyenaucheya <precious.onyenaucheya@ons.gov.uk>
2025-04-22 14:50:42 +0100

    remove duplicate method

--- src/components/chart/column-chart.js
+++ src/components/chart/column-chart.js
@@ -15,40 +15,6 @@ class ColumnChart {
         };
     };
 
-    // Set the point padding between each bar to be 3% (an overall gap of 6%)
-    // For charts with fewer than 5 categories, we use a wider point padding of 4% (8% gap between bars)
-    // For cluster charts we use 0 for the point padding and a group padding of 4% (8% gap between bars)
-    updatePointPadding = (config, currentChart, stackedLayout, numberOfExtraLines) => {
-        const numberOfCategories = config.xAxis.categories.length;
-        const numberOfSeries = currentChart.series.length - numberOfExtraLines; // Get number of column series
-        let pointPadding = 0;
-        let groupPadding = 0;
-        // non-clustered charts or stacked charts
-        if (numberOfSeries === 1 || stackedLayout === true) {
-            if (numberOfCategories >= 20) {
-                pointPadding = 0.1;
-            } else {
-                pointPadding = 0.2;
-            }
-        } else {
-            // clustered charts
-            if (numberOfCategories >= 20) {
-                groupPadding = 0.1;
-            } else {
-                groupPadding = 0.2;
-            }
-        }
-
-        // update the point width and padding
-        currentChart.series.forEach((series) => {
-            series.update({
-                pointPadding: pointPadding,
-                groupPadding: groupPadding,
-                maxPointWidth: 75,
-            });
-        });
-    };
-
     // Set the point padding between each bar
     // For charts with fewer than 5 categories, we use a wider point padding of 2%
     // For cluster charts we use 0 for the point padding and a group padding of 2%
