e3d8b6925ffdfdbb9535bc4e731138e828b8cd48
SriHV <sri.harsha.v.parupudi@ons.gov.uk>
2025-04-30 16:28:09 +0100

    added word-limit-component

--- src/components/textarea/textarea.dom.js
+++ src/components/textarea/textarea.dom.js
@@ -2,10 +2,16 @@ import domready from '../../js/domready';
 
 async function initialise() {
     const limitedInputs = [...document.querySelectorAll('.ons-js-char-limit-input')];
+    const wordlimitedInputs = [...document.querySelectorAll('.ons-js-word-limit-input')];
     if (limitedInputs.length) {
         const CharLimit = (await import('../char-check-limit/character-limit')).default;
 
         limitedInputs.forEach((input) => new CharLimit(input));
+    }
+    if (wordlimitedInputs.length) {
+        const WordLimit = (await import('../word-check-limit/word-limit')).default;
+
+        wordlimitedInputs.forEach((input) => new WordLimit(input));
     }
 }
 
---
+++ src/components/word-check-limit/word-limit.js
@@ -0,0 +1,55 @@
+import { trackEvent } from '../../js/analytics';
+
+const inputClassLimitReached = 'ons-input--limit-reached';
+const remainingClassLimitReached = 'ons-input__limit--reached';
+const attrCharLimitRef = 'data-word-limit-ref';
+
+export default class WordLimit {
+    constructor(input) {
+        this.input = input;
+        this.maxLength = input.maxLength;
+        this.limitElement = document.getElementById(input.getAttribute(attrCharLimitRef));
+        this.singularMessage = this.limitElement.getAttribute('data-wordcount-singular');
+        this.pluralMessage = this.limitElement.getAttribute('data-wordcount-plural');
+
+        this.updateLimitReadout(null, true);
+        this.limitElement.classList.remove('ons-u-d-no');
+
+        input.addEventListener('input', this.updateLimitReadout.bind(this));
+    }
+
+    updateLimitReadout(event, firstRun) {
+        const value = this.input.value.trim().split(/\s+/);
+        const remaining = this.maxLength - value.length;
+        const message = remaining === 1 ? this.singularMessage : this.pluralMessage;
+        // Prevent aria live announcement when component initialises
+        if (!firstRun && event.inputType) {
+            this.limitElement.setAttribute('aria-live', 'polite');
+            this.limitElement.setAttribute('aria-live', [remaining > 0 ? 'polite' : 'assertive']);
+        } else {
+            this.limitElement.removeAttribute('aria-live');
+        }
+
+        this.limitElement.innerText = message.replace('{x}', remaining);
+
+        this.setLimitClass(remaining, this.input, inputClassLimitReached);
+        this.setLimitClass(remaining, this.limitElement, remainingClassLimitReached);
+
+        this.track(remaining);
+    }
+
+    setLimitClass(remaining, element, limitClass) {
+        element.classList[remaining > 0 ? 'remove' : 'add'](limitClass);
+    }
+
+    track(remaining) {
+        if (remaining < 1) {
+            trackEvent({
+                event_type: 'event',
+                event_category: 'Error',
+                event_action: 'Textarea limit reached',
+                event_label: `Limit of ${this.maxLength} reached/exceeded`,
+            });
+        }
+    }
+}
