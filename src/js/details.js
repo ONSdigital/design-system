85c8179d907b6ecb7536b61c938abfec84e2e165
Jane <8680557+janehughes@users.noreply.github.com>
2025-06-09 14:26:19 +0100

    update details panel to use html details component

---
+++ src/js/details.js
@@ -0,0 +1,35 @@
+import domready from './domready';
+
+export default function toggleDetails() {
+    const allDetails = document.querySelectorAll('details');
+
+    // Collapse all <detail> elements by default
+    allDetails.forEach((detail) => detail.removeAttribute('open'));
+
+    // Expand all <details> before printing
+    window.addEventListener('beforeprint', () => {
+        allDetails.forEach((detail) => {
+            // Set as visible if already open
+            if (detail.hasAttribute('open')) {
+                detail.dataset.open = 'true';
+            } else {
+                detail.setAttribute('open', '');
+            }
+        });
+    });
+
+    // Collapse all <details> after printing
+    window.addEventListener('afterprint', () => {
+        allDetails.forEach((detail) => {
+            // Re-open all <details> which were previously visible
+            if (detail.dataset.open === 'true') {
+                detail.setAttribute('open', '');
+                delete detail.dataset.open;
+            } else {
+                detail.removeAttribute('open');
+            }
+        });
+    });
+}
+
+domready(toggleDetails);
