2b90645a16a0153e4caebcb3a5367196bf6fc409
SriHV <sri.harsha.v.parupudi@ons.gov.uk>
2025-06-19 11:25:33 +0100

    improved lighthouse-ci

--- lighthouse/lighthouserc.js
+++ lighthouse/lighthouserc.js
@@ -1,5 +1,5 @@
 const mode = process.env.LHCI_MODE;
-const urls = mode === 'allAudits' ? require('./urls.json').urls : require('./urls.json').skipurls; // if env var is 'all' it retrieves urls from url array, else it retrieves urls from skipurls array.
+const urls = mode === 'allAudits' ? require('./urls.json').urls : require('./urls.json').skipurls; // if env var is 'allAudits' it retrieves urls from url array, else it retrieves urls from skipurls array.
 module.exports = {
     ci: {
         collect: {
