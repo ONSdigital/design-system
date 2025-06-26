5fbb4c35986e328dfcbe7f7b64028dbaa3f092a9
SriHV <sri.harsha.v.parupudi@ons.gov.uk>
2025-06-26 16:10:42 +0100

    added few more tests

--- src/components/char-check-limit/_macro.spec.js
+++ src/components/char-check-limit/_macro.spec.js
@@ -5,7 +5,7 @@ import * as cheerio from 'cheerio';
 import axe from '../../tests/helpers/axe';
 import { renderComponent } from '../../tests/helpers/rendering';
 
-import { EXAMPLE_CHAR_CHECK_LIMIT, EXAMPLE_WORD_LIMIT } from './_test-examples';
+import { EXAMPLE_CHAR_CHECK_LIMIT, EXAMPLE_WORD_CHECK_LIMIT, EXAMPLE_CHAR_WORD_CHECK_LIMIT_MESSAGES } from './_test-examples';
 
 describe('FOR: Macro: CharCheckLimit', () => {
     describe('GIVEN: Params: Required', () => {
@@ -44,8 +44,34 @@ describe('FOR: Macro: CharCheckLimit', () => {
     });
 
     describe('GIVEN: Params: variant', () => {
+        describe('WHEN: variant is provided', () => {
+            const $ = cheerio.load(
+                renderComponent(
+                    'char-check-limit',
+                    {
+                        ...EXAMPLE_CHAR_CHECK_LIMIT,
+                        variant: 'check',
+                    },
+                    ['<p>Test content.</p>'],
+                ),
+            );
+
+            test('THEN: passes jest-axe checks with variant set to check', async () => {
+                const results = await axe($.html());
+                expect(results).toHaveNoViolations();
+            });
+
+            test('THEN: renders the passed content', () => {
+                expect($('.ons-js-char-check-input').text()).toBe('Test content.');
+            });
+
+            test('THEN: sets char as the count type to count number of characters', () => {
+                expect($('.ons-input__limit').attr('data-count-type')).toBe('char');
+            });
+        });
+
         describe('WHEN: variant is set to word', () => {
-            const $ = cheerio.load(renderComponent('char-check-limit', EXAMPLE_WORD_LIMIT));
+            const $ = cheerio.load(renderComponent('char-check-limit', EXAMPLE_WORD_CHECK_LIMIT));
 
             test('THEN: passes jest-axe checks', async () => {
                 const results = await axe($.html());
@@ -57,23 +83,105 @@ describe('FOR: Macro: CharCheckLimit', () => {
             });
 
             test('THEN: has the data attribute which defines wordCountPlural', () => {
-                const $ = cheerio.load(renderComponent('char-check-limit', EXAMPLE_WORD_LIMIT));
+                const $ = cheerio.load(renderComponent('char-check-limit', EXAMPLE_WORD_CHECK_LIMIT));
                 expect($('.ons-input__limit').attr('data-message-plural')).toBe('You have {x} words remaining');
             });
 
             test('THEN: has the data attribute which defines wordCountSingular', () => {
-                const $ = cheerio.load(renderComponent('char-check-limit', EXAMPLE_WORD_LIMIT));
+                const $ = cheerio.load(renderComponent('char-check-limit', EXAMPLE_WORD_CHECK_LIMIT));
                 expect($('.ons-input__limit').attr('data-message-singular')).toBe('You have {x} word remaining');
             });
 
             test('THEN: has the data attribute which defines wordCountOverLimitPlural', () => {
-                const $ = cheerio.load(renderComponent('char-check-limit', EXAMPLE_WORD_LIMIT));
+                const $ = cheerio.load(renderComponent('char-check-limit', EXAMPLE_WORD_CHECK_LIMIT));
                 expect($('.ons-input__limit').attr('data-message-over-limit-plural')).toBe('You have {x} words too many');
             });
 
             test('THEN: has the data attribute which defines wordCountOverLimitSingular', () => {
-                const $ = cheerio.load(renderComponent('char-check-limit', EXAMPLE_WORD_LIMIT));
+                const $ = cheerio.load(renderComponent('char-check-limit', EXAMPLE_WORD_CHECK_LIMIT));
                 expect($('.ons-input__limit').attr('data-message-over-limit-singular')).toBe('You have {x} word too many');
+            });
+        });
+    });
+
+    describe('GIVEN: Params: Character and Word count messaages', () => {
+        describe('WHEN: character and word count messages are set and variant is set to check', () => {
+            const $ = cheerio.load(
+                renderComponent(
+                    'char-check-limit',
+                    {
+                        ...EXAMPLE_CHAR_WORD_CHECK_LIMIT_MESSAGES,
+                        variant: 'check',
+                    },
+                    ['<p>Test content.</p>'],
+                ),
+            );
+
+            test('THEN: passes jest-axe checks', async () => {
+                const results = await axe($.html());
+
+                expect(results).toHaveNoViolations();
+            });
+
+            test('THEN: sets char as the count type to count number of characters', () => {
+                expect($('.ons-input__limit').attr('data-count-type')).toBe('char');
+            });
+
+            test('THEN: has the provided id attribute', () => {
+                expect($('.ons-input__limit').attr('id')).toBe('example-char-word-check-limit');
+            });
+
+            test('THEN: has the data attribute which defines charCountPlural', () => {
+                expect($('.ons-input__limit').attr('data-message-plural')).toBe('You have {x} characters remaining');
+            });
+
+            test('THEN: has the data attribute which defines charCountSingular', () => {
+                expect($('.ons-input__limit').attr('data-message-singular')).toBe('You have {x} character remaining');
+            });
+            test('THEN: has the data attribute which defines charCountOverLimitSingular', () => {
+                expect($('.ons-input__limit').attr('data-message-over-limit-singular')).toBe('You have {x} character too many');
+            });
+
+            test('THEN: has the data attribute which defines charCountOverLimitPlural', () => {
+                expect($('.ons-input__limit').attr('data-message-over-limit-plural')).toBe('You have {x} characters too many');
+            });
+        });
+
+        describe('WHEN: character and word count messages are set and variant is set to word', () => {
+            const $ = cheerio.load(
+                renderComponent('char-check-limit', {
+                    ...EXAMPLE_CHAR_WORD_CHECK_LIMIT_MESSAGES,
+                    variant: 'word',
+                }),
+            );
+
+            test('THEN: passes jest-axe checks', async () => {
+                const results = await axe($.html());
+
+                expect(results).toHaveNoViolations();
+            });
+
+            test('THEN: sets char as the count type to count number of words', () => {
+                expect($('.ons-input__limit').attr('data-count-type')).toBe('word');
+            });
+
+            test('THEN: has the provided id attribute', () => {
+                expect($('.ons-input__limit').attr('id')).toBe('example-char-word-check-limit');
+            });
+
+            test('THEN: has the data attribute which defines wordCountPlural', () => {
+                expect($('.ons-input__limit').attr('data-message-plural')).toBe('You have {x} words remaining');
+            });
+
+            test('THEN: has the data attribute which defines wordCountSingular', () => {
+                expect($('.ons-input__limit').attr('data-message-singular')).toBe('You have {x} word remaining');
+            });
+            test('THEN: has the data attribute which defines wordCountOverLimitSingular', () => {
+                expect($('.ons-input__limit').attr('data-message-over-limit-singular')).toBe('You have {x} word too many');
+            });
+
+            test('THEN: has the data attribute which defines wordCountOverLimitPlural', () => {
+                expect($('.ons-input__limit').attr('data-message-over-limit-plural')).toBe('You have {x} words too many');
             });
         });
     });
--- src/components/char-check-limit/_test-examples.js
+++ src/components/char-check-limit/_test-examples.js
@@ -6,9 +6,21 @@ export const EXAMPLE_CHAR_CHECK_LIMIT = {
     charCountOverLimitPlural: '{x} characters too many',
 };
 
-export const EXAMPLE_WORD_LIMIT = {
+export const EXAMPLE_WORD_CHECK_LIMIT = {
     id: 'example-word-limit',
     variant: 'word',
+    wordCountSingular: 'You have {x} word remaining',
+    wordCountPlural: 'You have {x} words remaining',
+    wordCountOverLimitSingular: 'You have {x} word too many',
+    wordCountOverLimitPlural: 'You have {x} words too many',
+};
+
+export const EXAMPLE_CHAR_WORD_CHECK_LIMIT_MESSAGES = {
+    id: 'example-char-word-check-limit',
+    charCountSingular: 'You have {x} character remaining',
+    charCountPlural: 'You have {x} characters remaining',
+    charCountOverLimitSingular: 'You have {x} character too many',
+    charCountOverLimitPlural: 'You have {x} characters too many',
     wordCountSingular: 'You have {x} word remaining',
     wordCountPlural: 'You have {x} words remaining',
     wordCountOverLimitSingular: 'You have {x} word too many',
