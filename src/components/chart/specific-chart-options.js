652ea0c3c8011ea7e62a852583c0af036a68e669
Helen C <helen.chapman@torchbox.com>
2025-07-08 09:51:33 +0100

    Add a timeout to the legend symbol update

--- src/components/chart/specific-chart-options.js
+++ src/components/chart/specific-chart-options.js
@@ -134,82 +134,88 @@ class SpecificChartOptions {
     };
 
     updateLegendSymbols = (chart) => {
-        if (chart.legend.options.enabled) {
-            chart.legend.allItems.forEach((item, index) => {
-                const { legendItem, userOptions } = item;
-                const seriesType = userOptions?.type;
-                const { label, symbol, line } = legendItem || {};
+        if (this.symbolUpdateTimeout) {
+            clearTimeout(this.symbolUpdateTimeout);
+        }
 
-                if (seriesType === 'line') {
-                    // This is the case for the column plus line chart - the series type is
-                    // line, but the chart type is column. In this case we show a simple
-                    // line symbol in the legend, but we need to move the label to the right
-                    // to account for the longer line symbol
-                    if (chart.userOptions.chart.type !== 'line') {
-                        label?.attr({
-                            x: 30, // Adjust label position to account for longer line
-                        });
-                    }
+        this.symbolUpdateTimeout = setTimeout(() => {
+            if (chart.legend.options.enabled) {
+                chart.legend.allItems.forEach((item, index) => {
+                    const { legendItem, userOptions } = item;
+                    const seriesType = userOptions?.type;
+                    const { label, symbol, line } = legendItem || {};
 
-                    // This is the scenario for a line chart with markers disabled
-                    // We have custom code in line-chart.js to update the last point to
-                    // display as a symbol. This code checks if there is no symbol in the legend
-                    // (which means it is a line chart with markers disabled)
-                    // and if so, it updates the legend to display as a symbol rather than as a line
-                    // We only to this for chart types that are explicitly line charts - i.e. not column with line
-                    if (!symbol && label && label.element && chart.userOptions.chart.type === 'line') {
-                        // Hide the line in the legend
-                        if (line) {
-                            line.hide();
+                    if (seriesType === 'line') {
+                        // This is the case for the column plus line chart - the series type is
+                        // line, but the chart type is column. In this case we show a simple
+                        // line symbol in the legend, but we need to move the label to the right
+                        // to account for the longer line symbol
+                        if (chart.userOptions.chart.type !== 'line') {
+                            label?.attr({
+                                x: 30, // Adjust label position to account for longer line
+                            });
                         }
 
-                        // Remove any existing custom symbols for this legend item
-                        const existingSymbols = label.parentGroup.element.querySelectorAll('[data-custom-legend-symbol]');
-                        existingSymbols.forEach((symbol) => symbol.remove());
+                        // This is the scenario for a line chart with markers disabled
+                        // We have custom code in line-chart.js to update the last point to
+                        // display as a symbol. This code checks if there is no symbol in the legend
+                        // (which means it is a line chart with markers disabled)
+                        // and if so, it updates the legend to display as a symbol rather than as a line
+                        // We only to this for chart types that are explicitly line charts - i.e. not column with line
+                        if (!symbol && label && label.element && chart.userOptions.chart.type === 'line') {
+                            // Hide the line in the legend
+                            if (line) {
+                                line.hide();
+                            }
 
-                        // Create a custom symbol for the legend using the line marker symbol options
-                        const renderer = chart.renderer;
-                        const symbolX = -5;
-                        const symbolY = 8;
-                        const markerStyle = this.constants.lineMarkerStyles[index % this.constants.lineMarkerStyles.length];
+                            // Remove any existing custom symbols for this legend item
+                            const existingSymbols = label.parentGroup.element.querySelectorAll('[data-custom-legend-symbol]');
+                            existingSymbols.forEach((symbol) => symbol.remove());
 
-                        const legendSymbol = renderer
-                            .symbol(markerStyle.symbol, symbolX, symbolY, 12, markerStyle.radius, markerStyle.radius)
-                            .attr({
-                                fill: item.color,
-                                stroke: item.color,
-                                'stroke-width': 1,
-                                // 10px accounts for the stroke width to end up at 12px overall
-                                width: 10,
-                                height: 10,
-                                'data-custom-legend-symbol': true,
+                            // Create a custom symbol for the legend using the line marker symbol options
+                            const renderer = chart.renderer;
+                            const symbolX = -5;
+                            const symbolY = 8;
+                            const markerStyle = this.constants.lineMarkerStyles[index % this.constants.lineMarkerStyles.length];
+
+                            const legendSymbol = renderer
+                                .symbol(markerStyle.symbol, symbolX, symbolY, 12, markerStyle.radius, markerStyle.radius)
+                                .attr({
+                                    fill: item.color,
+                                    stroke: item.color,
+                                    'stroke-width': 1,
+                                    // 10px accounts for the stroke width to end up at 12px overall
+                                    width: 10,
+                                    height: 10,
+                                    'data-custom-legend-symbol': true,
+                                });
+                            legendSymbol.add(label.parentGroup);
+                            label?.attr({
+                                x: 15, // Adjust label position to account for shorter space that the symbol takes up
                             });
-                        legendSymbol.add(label.parentGroup);
-                        label?.attr({
-                            x: 15, // Adjust label position to account for shorter space that the symbol takes up
+                        }
+                    } else if (seriesType === 'scatter') {
+                        // Because the scatter icons have a white border, we have to render them
+                        // slightly larger to make them appear at 12 x 12 px
+                        // Also the text looks better aligned if the y value is tweaked slightly for the scatter icons
+                        symbol.attr({
+                            width: 13,
+                            height: 13,
+                            y: 7,
+                        });
+                    } else {
+                        if (!symbol) return;
+                        // Update the symbol width and height
+                        // For column, bar and other chart types
+                        symbol.attr({
+                            width: 12,
+                            height: 12,
+                            y: 8,
                         });
                     }
-                } else if (seriesType === 'scatter') {
-                    // Because the scatter icons have a white border, we have to render them
-                    // slightly larger to make them appear at 12 x 12 px
-                    // Also the text looks better aligned if the y value is tweaked slightly for the scatter icons
-                    symbol.attr({
-                        width: 13,
-                        height: 13,
-                        y: 7,
-                    });
-                } else {
-                    if (!symbol) return;
-                    // Update the symbol width and height
-                    // For column, bar and other chart types
-                    symbol.attr({
-                        width: 12,
-                        height: 12,
-                        y: 8,
-                    });
-                }
-            });
-        }
+                });
+            }
+        }, 10);
     };
 }
 
