1b5859ae9dfe2ac6ce6b815427af3e158addc1e8
Precious Onyenaucheya <86783201+precious-onyenaucheya-ons@users.noreply.github.com>
2025-07-10 10:47:12 +0100

    Fix inconsistent font issue in chart rendering (#3675)

--- src/components/chart/chart.dom.js
+++ src/components/chart/chart.dom.js
@@ -1,8 +1,10 @@
 import HighchartsBaseChart from './chart';
 import domready from '../../js/domready';
 
-domready(async () => {
-    [HighchartsBaseChart].forEach((Component) => {
-        document.querySelectorAll(Component.selector()).forEach((el) => new Component(el));
+document.fonts.ready.then(() => {
+    domready(async () => {
+        [HighchartsBaseChart].forEach((Component) => {
+            document.querySelectorAll(Component.selector()).forEach((el) => new Component(el));
+        });
     });
 });
