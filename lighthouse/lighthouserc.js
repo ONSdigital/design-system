f20b6dc724f2ce965e2127a09193194c175e1e0f
SriHV <sri.harsha.v.parupudi@ons.gov.uk>
2025-07-14 16:03:52 +0100

    testing

--- lighthouse/lighthouserc.js
+++ lighthouse/lighthouserc.js
@@ -11,6 +11,18 @@ module.exports = {
         assert: {
             assertMatrix: [
                 {
+                    matchingUrlPattern: 'components/radios/example-radios-with-revealed.*',
+                    assertions: {
+                        'categories:accessibility': ['error', { minScore: 0.91 }],
+                    },
+                },
+                {
+                    matchingUrlPattern: 'components/radios/example-radios-with-clear-button.*',
+                    assertions: {
+                        'categories:accessibility': ['error', { minScore: 0.93 }],
+                    },
+                },
+                {
                     matchingUrlPattern: 'patterns/feedback/example-feedback-form.*|patterns/correct-errors/example-errors-proto.*',
                     assertions: {
                         'categories:accessibility': ['error', { minScore: 0.94 }],
@@ -18,7 +30,7 @@ module.exports = {
                 },
                 {
                     matchingUrlPattern:
-                        '^(?!.*patterns/correct-errors/example-errors-proto.*|.*patterns/feedback/example-feedback-form.*).*',
+                        '^(?!.*components/radios/example-radios-with-revealed.*|.*components/radios/example-radios-with-clear-button.*|.*patterns/correct-errors/example-errors-proto.*|.*patterns/feedback/example-feedback-form.*).*',
                     assertions: {
                         'categories:accessibility': ['error', { minScore: 1 }],
                     },
