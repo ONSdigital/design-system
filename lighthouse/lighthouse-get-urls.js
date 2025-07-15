675c4afd4ca00aa171524b70bc5df6aff144d143
SriHV <sri.harsha.v.parupudi@ons.gov.uk>
2025-07-15 12:37:41 +0100

    added back all the URLs

--- lighthouse/lighthouse-get-urls.js
+++ lighthouse/lighthouse-get-urls.js
@@ -32,6 +32,11 @@ async function getUrls() {
     const knownIssueFiles = [
         'example-errors-proto.html',
         'example-errors-proto-errors.html',
+        'example-feedback-form.html',
+        'example-feedback-form-errors.html',
+        'example-radios-with-revealed-text-input.html',
+        'example-radios-with-revealed-text-input-expanded.html',
+        'example-radios-with-revealed-text-area.html',
         'example-radios-with-revealed-text-area-expanded.html',
         'example-radios-with-revealed-select.html',
         'example-radios-with-revealed-select-expanded.html',
