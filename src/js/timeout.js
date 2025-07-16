a83de4505c09dbc8db36bc84f2d2398e13614804
SriHV <sri.harsha.v.parupudi@ons.gov.uk>
2025-07-16 17:13:41 +0100

    added DOMPurify

--- src/js/timeout.js
+++ src/js/timeout.js
@@ -1,3 +1,5 @@
+import purify from '../../lib/purify';
+
 export default class Timeout {
     constructor(context, sessionExpiryEndpoint, initialExpiryTime, enableTimeoutReset, startOnLoad) {
         this.context = context;
@@ -77,23 +79,23 @@ export default class Timeout {
                 ($this.endWithFullStop ? '.' : '');
 
             if (timerExpired) {
-                $this.countdown.innerHTML = '<span class="ons-u-fw-b">' + $this.countdownExpiredText + '</span>';
-                $this.accessibleCountdown.innerHTML = $this.countdownExpiredText;
+                $this.countdown.innerHTML = purify.sanitize('<span class="ons-u-fw-b">' + $this.countdownExpiredText + '</span>');
+                $this.accessibleCountdown.innerHTML = purify.sanitize($this.countdownExpiredText);
                 setTimeout($this.redirect.bind($this), 2000);
             } else {
                 seconds--;
                 $this.expiryTimeInMilliseconds = seconds * 1000;
-                $this.countdown.innerHTML = timeLeftText;
+                $this.countdown.innerHTML = purify.sanitize(timeLeftText);
 
                 if (minutesLeft < 1 && secondsLeft < 20) {
                     $this.accessibleCountdown.setAttribute('aria-live', 'assertive');
                 }
 
                 if (!$this.timerRunOnce) {
-                    $this.accessibleCountdown.innerHTML = timeLeftText;
+                    $this.accessibleCountdown.innerHTML = purify.sanitize(timeLeftText);
                     $this.timerRunOnce = true;
                 } else if (secondsLeft % 15 === 0) {
-                    $this.accessibleCountdown.innerHTML = timeLeftText;
+                    $this.accessibleCountdown.innerHTML = purify.sanitize(timeLeftText);
                 }
 
                 timers.push(setTimeout(runTimer.bind($this), 1000));
@@ -165,11 +167,11 @@ export default class Timeout {
     }
 
     redirect() {
-        if (this.isValidUrl(this.timeOutRedirectUrl)) {
-            const sanitizedUrl = encodeURI(this.timeOutRedirectUrl);
+        const sanitizedUrl = purify.sanitize(this.timeOutRedirectUrl);
+        if (this.isValidUrl(this.sanitizedUrl)) {
             window.location.replace(sanitizedUrl);
         } else {
-            console.error('Invalid redirect URL:', this.timeOutRedirectUrl);
+            console.error('Invalid redirect URL:', sanitizedUrl);
             // Optionally redirect to a default safe URL
             window.location.replace('/default-safe-url');
         }
@@ -180,7 +182,7 @@ export default class Timeout {
             const parsedUrl = new URL(url, window.location.origin);
             // Ensure the URL uses a safe protocol (e.g., https) and belongs to a trusted domain
             const trustedDomains = ['example.com', 'trusted-site.com'];
-            const isTrustedDomain = trustedDomains.some(domain => parsedUrl.hostname.endsWith(domain));
+            const isTrustedDomain = trustedDomains.some((domain) => parsedUrl.hostname.endsWith(domain));
             return (parsedUrl.protocol === 'https:' || parsedUrl.protocol === 'http:') && isTrustedDomain;
         } catch (e) {
             return false;
