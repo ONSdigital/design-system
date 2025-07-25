23a0f0cf7ffcb53686e5f1fd17a38aa5c60e86a4
SriHV <sri.harsha.v.parupudi@ons.gov.uk>
2025-07-25 09:25:06 +0100

    testing DOMPurify

--- src/components/video/video.js
+++ src/components/video/video.js
@@ -1,3 +1,4 @@
+import DOMPurify from 'dompurify';
 export default class Video {
     constructor(component) {
         this.component = component;
@@ -30,24 +31,12 @@ export default class Video {
     }
     addDNTtoVimeoVideos() {
         let src = this.iframe.getAttribute('data-src');
-        src = this.sanitizeSrc(src);
+        src = DOMPurify.sanitize(src);
         if (src.includes('player.vimeo.com/video') && src.includes('?dnt=1') === false) {
             src += '?dnt=1';
             return src;
         } else {
             return src;
         }
-    }
-
-    sanitizeSrc(src) {
-        try {
-            const url = new URL(src);
-            if (url.hostname === 'player.vimeo.com') {
-                return url.toString();
-            }
-        } catch (e) {
-            console.error('Invalid src URL:', src);
-        }
-        return '';
     }
 }
