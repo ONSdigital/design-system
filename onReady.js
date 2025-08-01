08bd08e6aeadbf3d82fe86e84afc79c9dd6e1d94
Precious Onyenaucheya <precious.onyenaucheya@ons.gov.uk>
2025-08-01 15:32:32 +0100

    revert change

--- onReady.js
+++ onReady.js
@@ -1,4 +1,5 @@
-module.exports = async (page) => {
+module.exports = async (page, scenario) => {
+    await require('./clickAndHoverHelper')(page, scenario);
     const selector = '.ons-chart__iframe';
     await page.waitFor(selector);
     await page.evaluate((selector) => {
