84f209971e96936174865d82d6e9c41328bfc9b2
Precious Onyenaucheya <precious.onyenaucheya@ons.gov.uk>
2025-08-01 14:58:23 +0100

    update script

--- onReady.js
+++ onReady.js
@@ -1,7 +1,9 @@
-module.exports = async (page) => {
-    page.evaluate(async () => {
-        document.querySelectorAll('[loading="lazy"]').forEach((element) => {
-            element.loading = 'eager';
-        });
-    });
+module.exports = async (page, scenario) => {
+    console.log('SCENARIO > ' + scenario.label);
+    await require('./clickAndHoverHelper')(page, scenario);
+    const selector = '.ons-chart__iframe';
+    await page.waitFor(selector);
+    await page.evaluate((selector) => {
+        document.querySelector(selector).scrollIntoView();
+    }, selector);
 };
