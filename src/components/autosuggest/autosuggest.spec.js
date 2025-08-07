1ca6b56b37552616a4fddd368ab8899ee3101a8f
SriHV <sri.harsha.v.parupudi@ons.gov.uk>
2025-08-07 18:16:20 +0100

    upgraded to 23.0.0

--- src/components/autosuggest/autosuggest.spec.js
+++ src/components/autosuggest/autosuggest.spec.js
@@ -389,6 +389,7 @@ describe('script: autosuggest', () => {
 
             await page.type('.ons-js-autosuggest-input', 'state', { delay: 20 });
             await page.keyboard.press('ArrowDown');
+
             await page.hover('.ons-autosuggest__option:nth-child(1)');
 
             const focusedClassCount = await page.$$eval('.ons-autosuggest__option--focused', (nodes) => nodes.length);
