311e6585d01c6646f8acc31f8ec4d923928b4310
SriHV <sri.harsha.v.parupudi@ons.gov.uk>
2025-08-11 18:15:08 +0100

    added tests

--- src/components/breadcrumbs/_macro.spec.js
+++ src/components/breadcrumbs/_macro.spec.js
@@ -132,4 +132,30 @@ describe('FOR: Macro: Breadcrumbs', () => {
             });
         });
     });
+
+    describe('GIVEN: Params: variant', () => {
+        describe('WHEN: variant is in hero variants', () => {
+            const $ = cheerio.load(
+                renderComponent('breadcrumbs', {
+                    ...EXAMPLE_BREADCRUMBS_REQUIRED_PARAMS,
+                    variant: 'hero-grey',
+                }),
+            );
+            test('THEN: wraps breadcrumbs in hero wrapper with correct modifier class', () => {
+                expect($('.ons-hero-wrapper').hasClass('ons-hero-wrapper--hero-grey')).toBe(true);
+            });
+        });
+
+        describe('WHEN: variant is not in hero variants', () => {
+            const $ = cheerio.load(
+                renderComponent('breadcrumbs', {
+                    ...EXAMPLE_BREADCRUMBS_REQUIRED_PARAMS,
+                    variant: 'test-variant',
+                }),
+            );
+            test('THEN: does not render hero-wrapper', () => {
+                expect($('.ons-hero-wrapper').length).toBe(0);
+            });
+        });
+    });
 });
