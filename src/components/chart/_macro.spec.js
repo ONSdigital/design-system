8a39a32e12ed03219fec5039d8bdffd42e684719
Precious Onyenaucheya <86783201+precious-onyenaucheya-ons@users.noreply.github.com>
2025-08-29 14:00:26 +0100

    Update src/components/chart/_macro.spec.js
    Co-authored-by: rmccar <42928680+rmccar@users.noreply.github.com>

--- src/components/chart/_macro.spec.js
+++ src/components/chart/_macro.spec.js
@@ -1042,7 +1042,7 @@ describe('Macro: Chart', () => {
             });
         });
 
-        describe('GIVEN: Params: YAxis', () => {
+        describe('GIVEN: Params: yAxis', () => {
             describe('WHEN: yAxis options are provided', () => {
                 const $ = cheerio.load(renderComponent('chart', EXAMPLE_COLUMN_CHART_PARAMS));
                 const configScript = $(`script[data-highcharts-config--column-chart-123]`).html();
