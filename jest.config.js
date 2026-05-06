5ef36d552ba2d9b6877cc5cdc0e6cf1060e1409a
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2026-05-06 16:37:28 +0100

    fix tests from node 24 upgrade

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,28 @@ const config = {
     preset: process.env.TEST_WITH_PUPPETEER ? 'jest-puppeteer' : undefined,
     snapshotResolver: `${process.cwd()}/src/tests/helpers/snapshotResolver.js`,
     modulePathIgnorePatterns: ['templates/components', 'build'],
+    transform: {
+        '^.+\\.m?js$': [
+            'babel-jest',
+            {
+                babelrc: false,
+                configFile: false,
+                sourceType: 'unambiguous',
+                presets: [
+                    [
+                        '@babel/preset-env',
+                        {
+                            bugfixes: true,
+                            modules: 'commonjs',
+                            targets: {
+                                node: 'current',
+                            },
+                        },
+                    ],
+                ],
+            },
+        ],
+    },
 };
 
 module.exports = config;
